32575e34f1065ed80b04b8c2923c72826fdb27ef
Sean Leas <phoenix_24leas.com>
2020-05-12 19:15:50 -0600

    Change randomize function to randomizeCover to follow srp and create randomizeImage function

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 // Create variables targetting the relevant DOM elements here 👇
 var coverImage = document.querySelector('.cover-image')
 var coverTitle = document.querySelector('.cover-title')
+var tagLine = document.querySelector('.tagline')
+var tagLineOne = document.querySelector('.tagline-1')
+var tagLineTwo = document.querySelector('.tagline-2')
 
 // We've provided a few variables below
 var savedCovers = [
@@ -9,14 +12,19 @@ var savedCovers = [
 var currentCover;
 
 // Add your event listeners here 👇
-window.addEventListener('load', randomize)
+window.addEventListener('load', randomizeCover)
 
 // Create your event handlers and other functions here 👇
 
-function randomize() {
-  coverImage.src = covers[getRandomIndex(covers)]
+function randomizeCover() {
+  randomizeImage()
   coverTitle.innerText = titles[getRandomIndex(titles)]
 }
+
+function randomizeImage() {
+  coverImage.src = covers[getRandomIndex(covers)]
+}
+
 
 // We've provided one function to get you started
 function getRandomIndex(array) {
