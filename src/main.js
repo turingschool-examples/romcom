e7508c9351c74c7bb0fe5d8df1d1a5c2940141d4
Joshua Sevy <joshuasevy@outlook.com>
2020-05-14 12:43:51 -0600

    Iteration 0 functional

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 // Create variables targetting the relevant DOM elements here 👇
 var loadCover = document.querySelector('.cover-image');
 var loadTitle = document.querySelector('.cover-title');
-var tag1 = document.querySelector('.tagline1');
-var tag2 = document.querySelector('.tagline2');
+var tagDiscriptors = document.querySelector('h3');
+
+
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
@@ -12,7 +13,7 @@ var currentCover;
 // Add your event listeners here 👇
 loadTitle.innerText = `${titles[getRandomIndex(titles)]}`;
 loadCover.src = `${covers[getRandomIndex(covers)]}`;
-tag1.innerText =`${descriptors[getRandomIndex(descriptors)]}`;
+tagDiscriptors.innerText =`A tale of ${descriptors[getRandomIndex(descriptors)]} and ${descriptors[getRandomIndex(descriptors)]}`;
 
 
 // Create your event handlers and other functions here 👇
