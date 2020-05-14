984c563ba77247d931f0942ea0c3fbe2ef96154d
Naomi Ware <nware1066@yahoo.com>
2020-05-14 08:28:24 -0600

    complete iteration0

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ var tagline2 = document.querySelector(".tagline-2");
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
+
 var currentCover;
 
 // Add your event listeners here 👇
