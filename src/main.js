fe3026a27cbd81a230e14e02f91a0ad7a33b4d8b
Stephanie Sohns <ssohns@gmail.com>
2020-05-13 19:23:20 -0600

    Randomization on pageload

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 // Create variables targetting the relevant DOM elements here 👇
-
+var loadCover = document.querySelector('.cover-image');
+var loadTitle = document.querySelector('.cover-title');
+var tag1 = document.querySelector('h3');
+// var tag2 = document.querySelector('.tagline');
 
 // We've provided a few variables below
 var savedCovers = [
@@ -11,8 +14,11 @@ var currentCover;
 
 
 // Create your eent handlers and other functions here 👇
-
-
+// var loadTitle = titles[getRandomIndex(titles)];
+loadCover.src = `${covers[getRandomIndex(covers)]}`
+loadTitle.innerText = `${titles[getRandomIndex(titles)]}`
+tag1.innerText = `${descriptors[getRandomIndex(descriptors)]}`
+// tag2.innerText = `${descriptors[getRandomIndex(descriptors)]}`
 // We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
