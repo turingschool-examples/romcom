518d7357a765321a9e59752b88e8bce0606a04e7
Joshua Sevy <joshuasevy@outlook.com>
2020-05-13 19:24:44 -0600

    Randomization progress on iteration0

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 // Create variables targetting the relevant DOM elements here 👇
-
-
+var loadCover = document.querySelector('.cover-image');
+var loadTitle = document.querySelector('.cover-title');
+var tag1 = document.querySelector('.tagline1');
+var tag2 = document.querySelector('.tagline2');
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
@@ -8,9 +10,12 @@ var savedCovers = [
 var currentCover;
 
 // Add your event listeners here 👇
+loadTitle.innerText = `${titles[getRandomIndex(titles)]}`;
+loadCover.src = `${covers[getRandomIndex(covers)]}`;
+tag1.innerText =`${descriptors[getRandomIndex(descriptors)]}`;
 
 
-// Create your eent handlers and other functions here 👇
+// Create your event handlers and other functions here 👇
 
 
 // We've provided one function to get you started
