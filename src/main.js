3e7b99cc2b7f2de0d6d23183fd3b68112afc86f8
Katy St Sauveur <katyrogowski@yahoo.com>
2020-05-14 15:06:02 -0700

    Randomize button working

--- src/main.js
+++ src/main.js
@@ -4,15 +4,16 @@ var coverImg = document.querySelector(".cover-image");
 var coverTagline = document.querySelector(".tagline");
 var coverTaglineOne = document.querySelector(".tagline-1");
 var coverTaglineTwo = document.querySelector(".tagline-2");
+var randomCoverBtn = document.querySelector(".random-cover-button");
 
 // We've provided a few variables below
-var savedCovers = [
-  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
-];
-var currentCover;
+// var savedCovers = [
+//   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
+// ];
+var currentCover = new Cover();
 
 // Add your event listeners here 👇
-
+randomCoverBtn.addEventListener('click', newRandomBook);
 
 // Create your event handlers and other functions here 👇
 
@@ -24,7 +25,6 @@ function getRandomIndex(array) {
 
 function newRandomBook() {
   coverTitle.innerText = titles[getRandomIndex(titles)];
-  // coverTagline.innerText = descriptors[getRandomIndex(descriptors)];
   coverTaglineOne.innerText = descriptors[getRandomIndex(descriptors)];
   coverTaglineTwo.innerText = descriptors[getRandomIndex(descriptors)];
   coverImg.src = covers[getRandomIndex(covers)];
