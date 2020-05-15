b0bd78c8e184310336d3c387c80377ac4415b413
chad-dickerson <chad.alan.dickerson@gmail.com>
2020-05-14 19:32:06 -0600

    Add random cover button variable to target DOM element

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ var coverImage = document.querySelector('.cover-image');
 var coverTitle = document.querySelector('.cover-title');
 var tagline1 = document.querySelector('.tagline-1');
 var tagline2 = document.querySelector('.tagline-2');
-
+var randomCoverButton = document.querySelector('.random-cover-button')
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
