efa6a180e17b472f459b512d8fec1c4872e41939
Joshua Sevy <joshuasevy@outlook.com>
2020-05-14 19:38:58 -0600

    Added variables and eventlistener for form page

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
 // Create variables targetting the relevant DOM elements here 👇
+// cover title and tagline variables
 var loadCover = document.querySelector('.cover-image');
 var loadTitle = document.querySelector('.cover-title');
 var tag1 = document.querySelector('.tagline-1');
 var tag2 = document.querySelector('.tagline-2');
+//btn variables
 var randomCoverBtn = document.querySelector('.random-cover-button');
 var loadingRandomCovers = document.querySelector('.home-view');
-
-
+var makeNewBtn = document.querySelector('.make-new-button');
+var homeBtn = document.querySelector('.home-button');
+var saveCoverBtn = document.querySelector('.save-cover-button');
+var viewSavedBtn = document.querySelector('.view-saved-button');
+//page variables
+var homeView = document.querySelector('.home-view');
+var savedView = document.querySelector('.saved-view');
+var formView = document.querySelector('.form-view');
 
 // We've provided a few variables below
 var savedCovers = [
@@ -16,6 +24,13 @@ var currentCover;
 
 // Add your event listeners here 👇
 randomCoverBtn.addEventListener('click', showNewRandomCvr);
+
+makeNewBtn.addEventListener('click' , displayMakeOwnCover);
+
+
+
+
+
 
 window.onload = function randomOnloadCvr() {
   showNewRandomCvr();
@@ -28,6 +43,13 @@ function showNewRandomCvr() {
   tag1.innerText = `${descriptors[getRandomIndex(descriptors)]}`;
   tag2.innerText = `${descriptors[getRandomIndex(descriptors)]}`;
 }
+// function displayMakeOwnCover() {
+//   homeBtn.classList.remove('hidden');
+//   homeView.classList.add('hidden');
+//   formView.classList.remove('hidden');
+//   randomCoverBtn.classList.add('hidden');
+//   saveCoverBtn.classList.add('hidden');
+// }
 
 
 // We've provided one function to get you started
