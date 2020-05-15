e8e419393b8d5510a65539e1d301ec8e57301169
chad-dickerson <chad.alan.dickerson@gmail.com>
2020-05-14 20:26:17 -0600

    Move randomRomCom function to Cover class

--- src/main.js
+++ src/main.js
@@ -3,29 +3,23 @@ var coverImage = document.querySelector('.cover-image');
 var coverTitle = document.querySelector('.cover-title');
 var tagline1 = document.querySelector('.tagline-1');
 var tagline2 = document.querySelector('.tagline-2');
-var randomCoverButton = document.querySelector('.random-cover-button')
+var randomCoverButton = document.querySelector('.random-cover-button');
+
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-var currentCover;
+var currentCover= new Cover(coverImage, coverTitle, tagline1, tagline2);
 
 // Add your event listeners here 👇
 randomCoverButton.addEventListener("click", function() {
-  randomRomCom();
+  currentCover.randomRomCom();
 });
 
 // Create your event handlers and other functions here 👇
-randomRomCom();
+currentCover.randomRomCom();
 
 // We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
-};
-
-function randomRomCom() {
-  coverImage.src = covers[getRandomIndex(covers)];
-  coverTitle.innerText = titles[getRandomIndex(titles)];
-  tagline1.innerText = descriptors[getRandomIndex(descriptors)];
-  tagline2.innerText = descriptors[getRandomIndex(descriptors)];
 };
