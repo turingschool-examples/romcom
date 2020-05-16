52e3379e5e44270e189a6e66d716d1b38709ed9c
Tyler Haglund <relyt4me@gmail.com>
2020-05-16 15:20:21 -0600

    Clear extra parentheses in Cover.js

--- src/Cover.js
+++ src/Cover.js
@@ -6,7 +6,7 @@ class Cover {
     this.tagline1 = descriptor1;
     this.tagline2 = descriptor2;
   }
-  randomizeCover()) {
+  randomizeCover() {
     this.cover = covers[getRandomIndex(covers)];
     this.title = titles[getRandomIndex(titles)];
     this.tagline1 = descriptors[getRandomIndex(descriptors)];
