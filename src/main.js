6392038cf9d0094ed485f271de6da1512f515153
chad-dickerson <chad.alan.dickerson@gmail.com>
2020-05-17 16:11:39 -0600

    Assign current cover to equal created cover after make new cover

--- src/main.js
+++ src/main.js
@@ -21,7 +21,7 @@ var makeMyBookButton = document.querySelector('.create-new-book-button');
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-var currentCover = new Cover(coverImage, coverTitle, tagline1, tagline2);
+var currentCover = new Cover(coverImage.src, coverTitle.innerText, tagline1.innerText, tagline2.innerText);
 
 // Add your event listeners here 👇
 randomCoverButton.addEventListener("click", function() {
@@ -35,6 +35,7 @@ makeMyBookButton.addEventListener('click', function(){
   addInputToArrays();
   var createdCover = new Cover(coverInput.value, titleInput.value, userDesc1.value, userDesc2.value);
   displayCover(createdCover);
+  currentCover = createdCover;
 })
 
 homeButton.addEventListener("click", function(){
