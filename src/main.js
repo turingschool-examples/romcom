ae7795a0924622606596d59fd665fd2469994b13
luxlewis <51483735+luxlewis@users.noreply.github.com>
2020-07-11 14:01:09 +0100

    Added switch view functionality

--- src/main.js
+++ src/main.js
@@ -4,19 +4,45 @@ var coverImageMain = document.querySelector('.cover-image');
 var coverTitleMain = document.querySelector('.cover-title');
 var tagline1Main = document.querySelector('.tagline-1');
 var tagline2Main = document.querySelector('.tagline-2');
+var newCover = document.querySelector('.user-cover');
+var newTitle = document.querySelector('.user-title');
+var newTagline1 = document.querySelector('.user-desc1');
+var newTagline2 = document.querySelector('.user-desc2');
 var randomCoverButton = document.querySelector('.random-cover-button');
 var savedView = document.querySelector('.saved-view');
 var makeNewButton = document.querySelector('.make-new-button');
 var homeButton = document.querySelector('.home-button');
 var makeNewForm = document.querySelector('.form-view');
 var homeView = document.querySelector('.home-view');
+var createNewButton = document.querySelector('.create-new-book-button');
 var saveButton = document.querySelector('.save-cover-button');
 var viewSavedButton = document.querySelector('.view-saved-button');
 
 // We've provided a few variables below
-var savedCovers = [
-  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
-];
+
+var mySavedTitles = JSON.parse(localStorage.getItem("titles"));
+if (localStorage.getItem('titles') == null) {
+  localStorage.setItem('titles', JSON.stringify(titles));
+};
+
+var mySavedDescriptors = JSON.parse(localStorage.getItem('descriptors'));
+if (localStorage.getItem('descriptors') == null) {
+  localStorage.setItem('descriptors', JSON.stringify(descriptors))
+};
+
+var mySavedCovers = JSON.parse(localStorage.getItem('covers'));
+if (localStorage.getItem('covers') == null) {
+  localStorage.setItem('covers', JSON.stringify(covers));
+};
+
+// var mySavedBooks = JSON.parse(localStorage.getItem('books') || '[]');
+//
+// localStorage.setItem('books', JSON.stringify(mySavedBooks));
+
+// var savedCovers = [
+//   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
+// ];
+
 var currentCover;
 
 // Add your event listeners here 👇
@@ -25,6 +51,8 @@ randomCoverButton.addEventListener('click', randomPoster);
 makeNewButton.addEventListener('click', showMakeForm);
 homeButton.addEventListener('click', showMain);
 viewSavedButton.addEventListener('click', showSaved);
+createNewButton.addEventListener('click', createPoster);
+createNewButton.addEventListener('click', showNew)
 // Create your eent handlers and other functions here 👇
 
 
@@ -35,19 +63,17 @@ function getRandomIndex(array) {
 
 function randomPoster() {
   coverImageMain.src = covers[getRandomIndex(covers)];
-  coverTitleMain.innerHTML = titles[getRandomIndex(titles)];
+  coverTitleMain.innerHTML= titles[getRandomIndex(titles)];
   tagline1Main.innerHTML = descriptors[getRandomIndex(descriptors)];
-  tagline2Main.innerHtml = descriptors[getRandomIndex(descriptors)];
-
+  tagline2Main.innerHTML= descriptors[getRandomIndex(descriptors)];
 };
 
 function showMakeForm() {
-  debugger;
   makeNewForm.classList.remove('hidden');
-  homeView.classList.add('hidden');
-  saveButton.classList.remove('hidden');
-  randomCoverButton.classList.add('hidden');
   homeButton.classList.remove('hidden');
+  saveButton.classList.remove('hidden');
+  homeView.classList.add('hidden');
+  randomCoverButton.classList.add('hidden');
   makeNewButton.classList.add('hidden');
   savedView.classList.add('hidden');
 
@@ -55,13 +81,13 @@ function showMakeForm() {
 
 function showSaved() {
   savedView.classList.remove('hidden');
+  homeButton.classList.remove('hidden');
+  makeNewButton.classList.remove('hidden');
+  makeNewForm.classList.add('hidden');
   homeView.classList.add('hidden');
   saveButton.classList.add('hidden');
   randomCoverButton.classList.add('hidden');
-  homeButton.classList.remove('hidden');
-  makeNewForm.classList.add('hidden');
-  makeNewButton.classList.remove('hidden');
-
+  viewSavedButton.classList.add('hidden');
 };
 
 function showMain() {
@@ -72,5 +98,28 @@ function showMain() {
   randomCoverButton.classList.remove('hidden');
   savedView.classList.add('hidden');
   makeNewButton.classList.remove('hidden');
+  viewSavedButton.classList.remove('hidden');
+
+};
+
+function createPoster() {
+  debugger;
+  coverImageMain.src = newCover.value;
+  coverTitleMain.innerHTML= newTitle.value;
+  tagline1Main.innerHTML = newTagline1.value;
+  tagline2Main.innerHTML= newTagline2.value;
+  let myCover = new Cover(newCover, newTitle, newTagline1, newTagline2);
+  // mySavedBooks.push(myCover);
+  // localStorage.setItem('books', JSON.stringify(mySavedBooks));
+  covers.push(newCover);
+  localStorage.setItem('covers', JSON.stringify(covers));
+  titles.push(newTitle);
+  localStorage.setItem('titles', JSON.stringify(titles));
+  descriptors.push(newTagline1);
+  descriptors.push(newTagline2);
+  localStorage.setItem('descriptors', JSON.stringify(descriptors))
+
+};
+function showNew() {
 
 }
