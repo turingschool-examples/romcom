8e99c50b845d2047bbf884b0f036ccf6db7ed62c
Alyssa Bull <alyssabull@gmail.com>
2020-08-27 18:01:24 -0600

    Add variables for saveCoversPage

--- src/main.js
+++ src/main.js
@@ -1,26 +1,24 @@
 // Create variables targetting the relevant DOM (document object model) elements here 👇
-var newCoverImg = document.querySelector(".cover-image");
-var newTitle = document.querySelector(".cover-title");
-var newDescriptor1 = document.querySelector(".tagline-1");
-var newDescriptor2 = document.querySelector(".tagline-2");
+var newCoverImg = document.querySelector('.cover-image');
+var newTitle = document.querySelector('.cover-title');
+var newDescriptor1 = document.querySelector('.tagline-1');
+var newDescriptor2 = document.querySelector('.tagline-2');
 
 var randomCoverButton = document.querySelector('.random-cover-button');
+var makeNewButton = document.querySelector('.make-new-button');
+var homeButton = document.querySelector('.home-button');
+var saveCoverButton = document.querySelector('.save-cover-button');
+var viewSavedCoversButton = document.querySelector('view-saved-button');
 
-var makeNewButton = document.querySelector('.make-new-button')
+var formPage = document.querySelector('.form-view');
+var homeView = document.querySelector('.home-view');
+var savedCoversView = document.querySelector('saved-view');
 
-var homeButton = document.querySelector('.home-button')
-
-var formPage = document.querySelector('.form-view')
-
-var homeView = document.querySelector('.home-view')
-
-var saveCoverButton = document.querySelector('.save-cover-button')
 
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-
 
 var currentCover;
 
@@ -28,13 +26,12 @@ var currentCover;
 randomCoverButton.addEventListener("click", generateRandomCover);
 
 makeNewButton.addEventListener("click", viewForm);
+
+
 // Create your event handlers and other functions here 👇
-
-
-// We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
-}
+};
 
 function generateRandomCover(){
   var randomCoverImg = covers[getRandomIndex(covers)];
@@ -48,11 +45,12 @@ function generateRandomCover(){
   newDescriptor2.innerText = randomDescriptor2;
 
   currentCover = new Cover (newCoverImg, newTitle, newDescriptor1, newDescriptor2);
-}
+};
+
 function viewForm() {
-  formPage.classList.remove('hidden')
-  homeView.classList.add('hidden')
-  randomCoverButton.classList.add('hidden')
-  saveCoverButton.classList.add('hidden')
-  homeButton.classList.remove('hidden')
-}
+  formPage.classList.remove('hidden');
+  homeButton.classList.remove('hidden');
+  homeView.classList.add('hidden');
+  randomCoverButton.classList.add('hidden');
+  saveCoverButton.classList.add('hidden');
+};
