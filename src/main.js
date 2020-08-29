1f007cc021a6b98c2fde0951150a077b6e5cf574
Bryce Mara Jarrett <brycej98@gmail.com>
2020-08-29 12:53:11 -0500

    Add iteration 3 first attempt with pseudocoding comments

--- src/main.js
+++ src/main.js
@@ -11,39 +11,40 @@ var saveCoverButton = document.querySelector('.save-cover-button');
 var homeButton = document.querySelector('.home-button');
 var savedCoversButton = document.querySelector('.view-saved-button');
 var savedView = document.querySelector('.saved-view');
-<<<<<<< Updated upstream
-=======
 var saveCoverButton = document.querySelector('.save-cover-button');
+var makeMyBookButton = document.querySelector('.create-new-book-button');
 
->>>>>>> Stashed changes
+
+var coverInput = document.getElementById('cover');
+/// This is gonna be a url though... ^^
+var titleInput = document.getElementById('title');
+var tagline1Input = document.getElementById('descriptor1');
+var tagline2Input = document.getElementById('descriptor2');
+
 
 // Here I am declaring these variables and linking them to the HTML element.
 
 // We've provided a few variables below
 var savedCovers = [
-  // new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
+  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg",
+   "Sunsets and Sorrows",
+   "sunsets",
+   "sorrows")
 ];
 
 var currentCover;
 
+
 // // Add your event listeners here 👇
-window.onload = useRandomIndex();
-// // Every time the window reloads (window.onload) we are gonna invoke our useRandomIndex
-// // function to get new variables.
-// // Below is another way to do it.
-// window.addEventListener('onload', useRandomIndex());
+window.addEventListener('onload', useRandomIndex());
 randomCoverButton.addEventListener('click', randomCover);
 makeNewOne.addEventListener('click', makeNewCoverByUser);
 savedCoversButton.addEventListener('click', viewSavedCovers);
-<<<<<<< Updated upstream
-homeButton.addEventListener('click', viewHomeScreen)
-=======
 homeButton.addEventListener('click', viewHomeScreen);
-saveCoverButton.addEventListener('click', saveACover);
+makeMyBookButton.addEventListener('click', saveCoverMadeByUser)
 
 
 
->>>>>>> Stashed changes
 // Create your event handlers and other functions here 👇
 
 
@@ -58,7 +59,6 @@ function useRandomIndex() {
   var randomTagline1 = descriptors[getRandomIndex(descriptors)];
   var randomTagline2 = descriptors[getRandomIndex(descriptors)];
   coverImage.src = randomImage;
-  // coverImage.setAttribute('src', randomImage);
   coverTitle.innerHTML = randomTitle;
   tagline1.innerHTML = randomTagline1;
   tagline2.innerHTML = randomTagline2;
@@ -93,6 +93,51 @@ function viewHomeScreen() {
   saveCoverButton.classList.remove('hidden');
 }
 
-function saveACover() {
-  console.log('hello');
+function saveCoverMadeByUser() {
+  // saving the user data into the arrays
+  covers.push(coverInput);
+  titles.push(titleInput);
+  descriptors.push(tagline1Input);
+  descriptors.push(tagline2Input);
+  // these arrays aren't in this main.js file though, will they be pushed then?
+
+  // then hide the form view and show the home view
+formView.classList.add('hidden')
+homeView.classList.remove('hidden')
+
+// also need to display the new image in the home view
+coverImage.src = coverInput;
+coverTitle.innerHTML = titleInput;
+tagline1.innerHTML = tagline1Input;
+tagline2.innerHTML = tagline2Input;
+};
+
+
+class Cover {
+  constructor(cover, title, tagline1, tagline2) {
+    this.cover = cover;
+    this.title = title;
+    this.tagline1 = tagline1;
+    this.tagline2 = tagline2
+  }
 }
+
+var coverByUser = new Cover(coverInput, titleInput, tagline1Input, tagline2Input);
+savedCovers.push(coverByUser);
+// saving the cover made by the user into the savedCovers array
+
+
+//  iteration 3 pseudocoding:
+// We're in the new cover form. The user will input the fields and click make my cover button.
+// We want to save that data into arrays. (cover, title, tagline1. tagline2 )
+//
+// Need to create new instance of the cover class. (First we need a cover class)
+//
+// After the user hits the make my cover we'll be brought back to the main home view.
+// that home view will display the cover they just created. (the instance of our Cover class
+//
+//
+//
+//
+
+// ..
