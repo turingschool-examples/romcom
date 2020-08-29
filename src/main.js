885267c531de0ba76e17923fca71231b00b9805d
Lola Dolinsky <dolinsky.lola@gmail.com>
2020-08-29 15:33:35 -0600

    Add function and variables for random cover on load - complete

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 // Create variables targetting the relevant DOM elements here 👇
+var cover =  document.querySelector('.cover-image');
+var title = document.querySelector('.cover-title');
+var descriptor1 = document.querySelector('.tagline-1');
+var descriptor2 = document.querySelector('.tagline-2');
 
 
 // We've provided a few variables below
@@ -9,11 +13,34 @@ var currentCover;
 
 // Add your event listeners here 👇
 
+window.addEventListener("load", function(){
+  getRandomBook();
+});
+
 
 // Create your event handlers and other functions here 👇
+function getRandomBook() {
+  var randomCover = getRandomIndex(covers);
+  var randomTitle = getRandomIndex(titles);
+  var randomDescriptor = getRandomIndex(descriptors);
+  var randomDescriptor2 = getRandomIndex(descriptors);
 
+  var currentCover = new Cover(randomCover, randomTitle, randomDescriptor, randomDescriptor2);
+
+  cover.src = currentCover.cover
+  title.innerText = currentCover.title
+  descriptor1.innerText = currentCover.tagline1
+  descriptor2.innerText = currentCover.tagline2
+
+  console.log(currentCover);
+}
 
 // We've provided one function to get you started
-function getRandomIndex(array) {
-  return Math.floor(Math.random() * array.length);
+function getRandomIndex(arr) {
+  // return Math.floor(Math.random() * arr.length);
+   return arr[Math.floor(Math.random() * arr.length)]
 }
+//
+// getRandomIndex(covers)
+// getRandomIndex(descriptors)
+// getRandomIndex(titles)
