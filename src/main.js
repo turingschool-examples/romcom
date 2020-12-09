7e90ab96a5b915e605abbb80aea2f29b5f5d2c49
Nikki <piknik@me.com>
2020-12-09 05:30:43 -0700

    Created variables for random cover

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
-// Create variables targetting the relevant DOM elements here 👇
-
-
+// Create variables targeting the relevant DOM elements here 👇
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-var currentCover;
+
+var coverImgSrc = covers[getRandomIndex(covers)]
+var title = titles[getRandomIndex(titles)]
+var descriptor1 = descriptors[getRandomIndex(descriptors)]
+var descriptor2 = descriptors[getRandomIndex(descriptors)]
+
+var currentCover = new Cover(coverImgSrc, title, descriptor1, descriptor2);
 
 // Add your event listeners here 👇
 
