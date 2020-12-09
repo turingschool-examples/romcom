018a6b3452ad3cee39949add3426d88744e014c8
Josh Aragon <josharagon99@gmail.com>
2020-12-08 21:22:19 -0700

    Add random title function

--- src/main.js
+++ src/main.js
@@ -6,9 +6,9 @@ var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 var currentCover;
-
+var title = document.getElementsByClassName('cover-title')[0];
 // Add your event listeners here 👇
-
+window.addEventListener('load', getRandomTitle)
 
 // Create your event handlers and other functions here 👇
 
@@ -16,4 +16,9 @@ var currentCover;
 // We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
+}
+
+function getRandomTitle(title) {
+  var index = getRandomIndex(titles);
+  this.title.innerText = titles[index];
 }
