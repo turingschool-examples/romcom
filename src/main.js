73343d0bb43a3a0c3742c2dedfbc5a02e09eaa2e
Matt Umland <mattumland@gmail.com>
2020-12-10 08:32:20 -0500

    add random cover on load feature

--- src/main.js
+++ src/main.js
@@ -4,17 +4,32 @@ var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 
-var coverImgSrc = covers[getRandomIndex(covers)]
-var title = titles[getRandomIndex(titles)]
-var descriptor1 = descriptors[getRandomIndex(descriptors)]
-var descriptor2 = descriptors[getRandomIndex(descriptors)]
+window.addEventListener('load', updateCover);
 
-var currentCover = new Cover(coverImgSrc, title, descriptor1, descriptor2);
+function buildNewCover() {
+  var coverImgSrc = covers[getRandomIndex(covers)];
+  var title = titles[getRandomIndex(titles)];
+  var descriptor1 = descriptors[getRandomIndex(descriptors)];
+  var descriptor2 = descriptors[getRandomIndex(descriptors)];
+  return new Cover(coverImgSrc, title, descriptor1, descriptor2);
+}
 
-// Add your event listeners here 👇
+var coverImage = document.querySelector('.cover-image');
+var coverTitle = document.querySelector('.cover-title');
+var firstDescriptor = document.querySelector('.tagline-1');
+var secondDescriptor = document.querySelector('.tagline-2');
+var button = document.querySelector('.random-cover-button');
+
+button.addEventListener('click', updateCover);
 
 
-// Create your event handlers and other functions here 👇
+function updateCover() {
+  var currentCover = buildNewCover();
+  coverImage.src = currentCover.cover;
+  coverTitle.innerHTML = currentCover.title;
+  firstDescriptor.innerHTML = currentCover.tagline1;
+  secondDescriptor.innerHTML = currentCover.tagline2;
+}
 
 
 // We've provided one function to get you started
