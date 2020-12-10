20ee5ed112e351fb505889bceffa8b971afcbf49
Caroline Eubanks <cmeubanks@gmail.com>
2020-12-10 16:13:44 -0700

    created pageLoad variable, event listener, and random cover function

--- src/main.js
+++ src/main.js
@@ -2,19 +2,16 @@
 //Iteration 0: Main Page
 //  Function to randomly generate new images and title
 //  names sourced from cover.js & data.js
-var mainPage = document.querySelector('.cover-image');
+var mainPage = document.querySelector('.cover-image'); //main page
 console.log(mainPage);
-
-var title = document.querySelector(`.cover-title`);
+var title = document.querySelector(`.cover-title`); // main page
 console.log(title);
-
-var tagline1 = document.querySelector(`.tagline-1`);
+var tagline1 = document.querySelector(`.tagline-1`); //main page
 console.log(tagLine1);
-
-var tagline2 = document.querySelector(`.tagline-2`);
+var tagline2 = document.querySelector(`.tagline-2`); // main page
 console.log(tagLine2);
-
-
+var pageLoad = document.querySelector('html'); //main page
+console.log(pageLoad);
 
 
 
@@ -25,12 +22,21 @@ var savedCovers = [
 var currentCover;
 
 // Add your event listeners here 👇
-
+pageLoad.addEventListener('load', getRandomCover);
 
 // Create your event handlers and other functions here 👇
 
 
 // We've provided one function to get you started
-function getRandomIndex(array) {
-  return Math.floor(Math.random() * array.length);
+function getRandomCover(covers) {
+  for ( var i = 0; i < covers.length; i++) {
+  var randomCover = covers[Math.floor(Math.random() * covers.length);];
+  }
+  return randomCover;
 }
+//function getRandomIndex(array) {
+//  return Math.floor(Math.random() * array.length);
+//}
+//function getRandomIndex(array) {
+  //return Math.floor(Math.random() * array.length);
+//}
