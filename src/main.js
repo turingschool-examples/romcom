dc2886c9194899c1f51365fd937a7271fbe09df6
Nikki <piknik@me.com>
2020-12-09 19:37:08 -0700

    added functions to create and display new cover on button click

--- src/main.js
+++ src/main.js
@@ -1,16 +1,37 @@
 // Create variables targetting the relevant DOM elements here 👇
-
-
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-var currentCover;
 
-// Add your event listeners here 👇
+// look at SRP and window.onload
+// function into buildnewcover
 
+function buildNewCover() {
+  var coverImgSrc = covers[getRandomIndex(covers)];
+  var title = titles[getRandomIndex(titles)];
+  var descriptor1 = descriptors[getRandomIndex(descriptors)];
+  var descriptor2 = descriptors[getRandomIndex(descriptors)];
 
-// Create your event handlers and other functions here 👇
+  return new Cover(coverImgSrc, title, descriptor1, descriptor2);
+}
+
+var coverImage = document.querySelector('.cover-image');
+var coverTitle = document.querySelector('.cover-title');
+var firstDescriptor = document.querySelector('.tagline-1');
+var secondDescriptor = document.querySelector('.tagline-2');
+
+var button = document.querySelector('.random-cover-button');
+button.addEventListener('click', updateCover);
+
+function updateCover() {
+  var currentCover = buildNewCover();
+
+  coverImage.src = currentCover.cover;
+  coverTitle.innerHTML = currentCover.title;
+  firstDescriptor.innerHTML = currentCover.tagline1;
+  secondDescriptor.innerHTML = currentCover.tagline2;
+}
 
 
 // We've provided one function to get you started
