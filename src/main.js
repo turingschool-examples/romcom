e1f2391f655ead1ac37d0a1c3d2f044f858fe790
Owen Hallgren <owenhallgren@gmail.com>
2020-12-10 19:17:41 -0700

    fixed iteration 1

--- src/main.js
+++ src/main.js
@@ -2,24 +2,37 @@
 
 
 // We've provided a few variables below
-var savedCovers = [
-  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
-];
-var currentCover;
-
+// var savedCovers = [
+//   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
+// ];
+// var currentCover;
+var mainImage = document.querySelector(`.cover-image`);
+var title = document.querySelector(`.cover-title`);
+var descriptor1 = document.querySelector(`.tagline-1`);
+var descriptor2 = document.querySelector(`.tagline-2`);
 // Add your event listeners here 👇
 document.querySelector('.random-cover-button').addEventListener('click', function(){
-  mainImage.src = covers[getRandomIndex(covers)];
+var randomCover = createRandomCover();
+mainImage.src = randomCovor.cover;
+title.innerText = randomCovor.title;
+descriptor1.innerText = randomCover.descriptor1;
+descriptor2.innerText = randomCover.descriptor2;
 });
 
+
 // Create your event handlers and other functions here 👇
-var mainImage = document.querySelector(`.cover-image`);
+
 function getRandomIndex(covers) {
   return Math.floor(Math.random() * covers.length);
 }
-mainImage.src = covers[getRandomIndex(covers)];
 
-//
+function createRandomCover(){
+  var randomImage = covers[getRandomIndex(covers)];
+  var randomTitle = titles[getRandomIndex(titles)];
+  var randomDescriptor = descriptor[getRandomIndex(descriptor)];
+  var randomCover = new Cover(randomImage, randomTitle, randomDescriptor, randomDescriptor)
+  return randomCover
+}
 // // We've provided one function to get you started
 // function getRandomIndex(array) {
 //   return Math.floor(Math.random() * array.length);
