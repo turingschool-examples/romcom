650d695a367dc5664e85eb50489af5d39e6f6176
Charles Morris <percworld@gmail.com>
2020-12-11 18:13:11 -0700

    add button functionality and save cover view

--- src/main.js
+++ src/main.js
@@ -21,22 +21,37 @@ var tagline1 = document.querySelector('.tagline-1');
 var tagline2 = document.querySelector('.tagline-2');
 // tagline2.innerText = descriptor2;
 
-var randomCoverButton = document.querySelector('.random-cover-button')
-var makeCoverButton = document.querySelector('.make-new-button')
-var formViewButton = document.querySelector('.form-view')
-makeCoverButton.addEventListener('click', unhideForm)
-var saveCoverButton = document.querySelector('.save-cover-button')
-var homeButton = document.querySelector('.home-button')
-var homeView = document.querySelector('.home-view')
+var randomCoverButton = document.querySelector('.random-cover-button');
+var makeCoverButton = document.querySelector('.make-new-button');
+var formViewButton = document.querySelector('.form-view');
+
+var saveCoverButton = document.querySelector('.save-cover-button');
+var homeButton = document.querySelector('.home-button');
+var homeView = document.querySelector('.home-view');
+var savedCoversView = document.querySelector('.saved-view');
+var viewSavedButton = document.querySelector('.view-saved-button');
 
 function unhideForm() {
-formViewButton.classList.toggle('hidden');
-randomCoverButton.classList.toggle('hidden');
-saveCoverButton.classList.toggle('hidden');
-homeButton.classList.toggle('hidden');
-homeView.classList.toggle('hidden');
+  formViewButton.classList.toggle('hidden');
+  randomCoverButton.classList.toggle('hidden');
+  saveCoverButton.classList.toggle('hidden');
+  homeButton.classList.toggle('hidden');
+  homeView.classList.toggle('hidden');
 }
+function viewSavedCovers() {
+  homeView.classList.toggle('hidden');
+  savedCoversView.classList.toggle('hidden');
+  saveCoverButton.classList.toggle('hidden');
+  randomCoverButton.classList.toggle('hidden');
+  homeButton.classList.toggle('hidden');
+}
+function viewHome() {
+  homeView.classList.toggle('hidden');
+  homeButton.classList.toggle('hidden');
+  saveCoverButton.classList.toggle('hidden');
+  randomCoverButton.classList.toggle('hidden');
 
+}
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
@@ -45,7 +60,10 @@ var currentCover = new Cover(image, title, tagline1, tagline2)
 
 // Add your event listeners here 👇
 randomCoverButton.addEventListener('click', createCover)
-
+makeCoverButton.addEventListener('click', unhideForm)
+homeButton.addEventListener('click', viewHome)
+//saveCoverButton.addEventListener('click', unhideSaveCover)
+viewSavedButton.addEventListener('click', viewSavedCovers)
 // Create your event handlers and other functions here 👇
 function createCover() {
   var descriptor1 = descriptors[getRandomIndex(descriptors)];
