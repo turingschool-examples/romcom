3ad46962a08ac03b960a39cc0940a477a1e62011
Nichele Dunn <the7yearitch@hotmail.com>
2020-12-11 17:17:09 -0700

    Update with makeNewButton var & listener, deleted empty spacees

--- src/main.js
+++ src/main.js
@@ -1,28 +1,20 @@
-
 var tagline1 = document.querySelector('.tagline-1');
-
 var tagline2 = document.querySelector('.tagline-2');
-
 var body = document.querySelector('body');
-
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-
 var currentCover;
-
 var pageLoad = document.querySelector('html');
-
 var coverImage = document.querySelector('.cover-image');
-
 var title = document.querySelector(`.cover-title`);
-
 var randomCoverButton = document.querySelector('.random-cover-button');
+var makeNewButton = document.querySelector('.make-new-button');
 
 // Add your event listeners here 👇
 window.addEventListener('load', generateRandomCover);
-
 randomCoverButton.addEventListener('click', generateRandomCover);
+makeNewButton.addEventListener('click', //swich pages to Make New Cover page//)
 
 // Create your event handlers and other functions here 👇
 
@@ -32,7 +24,6 @@ function getRandomIndex(array) {
   }
   return randomIndex;
 };
-
 function generateRandomCover() {
   title.innerText = getRandomIndex(titles);
   coverImage.src = getRandomIndex(covers);
