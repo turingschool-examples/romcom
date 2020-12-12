31d76afd91eb1f80a7ad4a32a6ae237a55bafedb
julia <juilaiwinski@gmail.com>
2020-12-12 11:28:44 -0700

    Display all saved covers

--- src/main.js
+++ src/main.js
@@ -1,19 +1,22 @@
 
 // variables
 var mainPage = document.querySelector('.main-cover');
+var coverForm = document.querySelector('.form-view');
+var homeButton = document.querySelector('.home-button');
 var randomCoverButton = document.querySelector('.random-cover-button');
 var makeOwnCoverButton = document.querySelector('.make-new-button');
-var coverForm = document.querySelector('.form-view');
 var saveCoverButton = document.querySelector('.save-cover-button');
-var homeButton = document.querySelector('.home-button');
 var viewSavedCoversButton = document.querySelector('.view-saved-button');
 
+var form = document.querySelector('form');
 var userCover = document.querySelector('.user-cover');
 var userTitle = document.querySelector('.user-title');
 var userDescriptor1 = document.querySelector('#descriptor1');
 var userDescriptor2 = document.querySelector('#descriptor2');
 var createNewBookButton = document.querySelector('.create-new-book-button');
-var form = document.querySelector('form');
+var savedCoversSection = document.querySelector('.saved-covers-section');
+var viewSavedSection = document.querySelector('.saved-view');
+var homeView = document.querySelector('.home-view');
 
 
 //event listeners
@@ -23,6 +26,7 @@ makeOwnCoverButton.addEventListener('click', showMakeOwnCover);
 homeButton.addEventListener('click', goHome);
 viewSavedCoversButton.addEventListener('click', viewSavedCovers);
 createNewBookButton.addEventListener('click', saveMakeOwnCover);
+saveCoverButton.addEventListener('click', saveCover);
 
 var savedCovers = [];
 var currentCover;
@@ -39,9 +43,6 @@ function getRandomCover() {
   var randomDescriptor1 = descriptors[getRandomIndex(descriptors)];
   var randomDescriptor2 = descriptors[getRandomIndex(descriptors)];;;
   currentCover = new Cover(randomCover, randomTitle, randomDescriptor1, randomDescriptor2);
-  //this is assigning the data model to 'a new cover' below
-
-
 mainPage.innerHTML = `
   <section class="main-cover">
     <img class="cover-image" src="${currentCover.cover}">
@@ -50,14 +51,12 @@ mainPage.innerHTML = `
     <img class="price-tag" src="">
     <img class="overlay" src="./assets/overlay.png">
   </section>
-  `
+  `;
 };
 
 function saveMakeOwnCover() {
   event.preventDefault()
-
   currentCover = new Cover(userCover.value, userTitle.value, userDescriptor1.value, userDescriptor2.value)
-
   mainPage.innerHTML = `
     <section class="main-cover">
       <img class="cover-image" src="${currentCover.cover}">
@@ -66,17 +65,16 @@ function saveMakeOwnCover() {
       <img class="price-tag" src="">
       <img class="overlay" src="./assets/overlay.png">
     </section>
-    `
+    `;
     form.classList.add('hidden');
     mainPage.classList.remove('hidden');
-
 };
 
 function showMakeOwnCover() {
   mainPage.classList.add('hidden');
   coverForm.classList.remove('hidden');
   randomCoverButton.classList.add('hidden');
-  saveCoverButton.classList.add('hidden');
+  saveCoverButton.classList.remove('hidden');
   homeButton.classList.remove('hidden');
 };
 
@@ -95,4 +93,26 @@ function viewSavedCovers() {
   saveCoverButton.classList.add('hidden');
   homeButton.classList.remove('hidden');
   viewSavedCoversButton.classList.remove('hidden');
+  displayAllSavedCovers()
+}
+
+function saveCover() {
+  savedCovers.push(currentCover);
+}
+
+function displayAllSavedCovers() {
+  viewSavedSection.classList.remove('hidden');
+  homeView.classList.add('hidden');
+
+  for (var i = 0; i < savedCovers.length; i++){
+    savedCoversSection.innerHTML += `
+      <section class="main-cover">
+        <img class="cover-image" src="${savedCovers[i].cover}">
+        <h2 class="cover-title">${savedCovers[i].title}</h2>
+        <h3 class="tagline">A tale of <span class="tagline-1">${savedCovers[i].tagline1}</span> and <span class="tagline-2">${savedCovers[i].tagline2}</span></h3>
+        <img class="price-tag" src="">
+        <img class="overlay" src="./assets/overlay.png">
+      </section>
+      `
+  }
 }
