2e8a31a8069c97f42252f1353d9df0e60b43f76b
Whitney Wilkes <wilkes.whit@gmail.com>
2020-12-12 21:10:58 -0700

    Add lrandom image on load

--- src/main.js
+++ src/main.js
@@ -1,19 +1,33 @@
-// Create variables targetting the relevant DOM elements here 👇
+var mainCover = document.querySelector('.main-cover')
+var title = document.querySelector('.cover-title') 
+var cover = document.querySelector('.cover-image')
+var firstDescriptor = document.querySelector('.tagline-1')
+var secondDescriptor = document.querySelector('.tagline-2')
 
 
-// We've provided a few variables below
-var savedCovers = [
-  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
-];
+var savedCovers =[];
 var currentCover;
 
-// Add your event listeners here 👇
+window.onload = displayRandom()
 
-
-// Create your event handlers and other functions here 👇
-
-
-// We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
-}
+  };
+  
+function displayRandom() {
+  var randomCover = covers[getRandomIndex(covers)];
+  var randomTitle = titles[getRandomIndex(titles)];
+  var firstDescriptor = descriptors[getRandomIndex(descriptors)];
+  var secondDescriptor = descriptors[getRandomIndex(descriptors)];
+  currentCover = new Cover(randomCover, randomTitle, firstDescriptor, secondDescriptor);
+
+  mainCover.innerHTML = `
+  <section class="main-cover">
+  <img class="cover-image" src="${currentCover.cover}">
+  <h2 class="cover-title">${currentCover.title}</h2>
+  <h3 class="tagline">A tale of <span class="tagline-1">${currentCover.tagline1}</span> and <span class="tagline-2">${currentCover.tagline2}</span></h3>
+  <img class="price-tag" src="./assets/price.png">
+  <img class="overlay" src="./assets/overlay.png">
+</section>
+`
+};
