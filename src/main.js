7f55bb27a8e6edb89fb12bcacb515ee5cb39d21a
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-03-31 18:57:53 -0400

    Create variables targeting DOM elements

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
 // Create variables targetting the relevant DOM elements here 👇
+var coverImage = document.querySelector('cover-image');
+var coverTitle = document.querySelector('cover-title');
+var tagline = document.querySelector('tagline');
 
+// Global Variables //
 
-// We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 var currentCover;
+
+////////////////////////////
+
+
+
+
 
 // Add your event listeners here 👇
 
@@ -14,6 +23,8 @@ var currentCover;
 
 
 // We've provided one function to get you started
+
+
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
