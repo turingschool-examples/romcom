daeee4a66e2dcf34cc8100e58859b63aff72eaa5
Shauna Myers <shaunamyers26@gmail.com>
2021-04-01 18:10:19 -0400

    Refactor code

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 // Create variables targetting the relevant DOM elements here 👇
+
 var coverImage = document.querySelector('.cover-image');
 var title = document.querySelector('.cover-title');
 var descriptor1 = document.querySelector('.tagline-1');
@@ -8,39 +9,30 @@ var saveCoverButton = document.querySelector('.save-cover-button');
 var saveCoverView = document.querySelector('.saved-view');
 var homeView = document.querySelector('.home-view');
 
-var savedCovers = [
-  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
-];
 var currentCover;
 
-coverImage.src = covers[getRandomIndex(covers)];
-
-title.innerText = titles[getRandomIndex(titles)];
-
-descriptor1.innerText = descriptors[getRandomIndex(descriptors)];
-descriptor2.innerText = descriptors[getRandomIndex(descriptors)];
-
-
 // Add your event listeners here 👇
-
-randomCoverButton.addEventListener('click', function() {
+//google add event listener on page load
+//Event listener should be 1-3 lines, only if you have an anonymous function...no logic...only FIRE/INVOKE functions
+randomCoverButton.addEventListener('click', makeRandomCover);
   // coverImage.src = covers[getRandomIndex(covers)];
   // title.innerText = titles[getRandomIndex(titles)];
   // descriptor1.innerText = descriptors[getRandomIndex(descriptors)];
   // descriptor2.innerText = descriptors[getRandomIndex(descriptors)];
+
+function makeRandomCover() {
   currentCover = new Cover(covers[getRandomIndex(covers)], titles[getRandomIndex(titles)], descriptors[getRandomIndex(descriptors)], descriptors[getRandomIndex(descriptors)]);
-  savedCovers.unshift(currentCover);
   coverImage.src = currentCover.cover;
   title.innerText = currentCover.title;
   descriptor1.innerText = currentCover.tagline1;
   descriptor2.innerText = currentCover.tagline2;
-});
+}
 
+//Reflect changes from above
 saveCoverButton.addEventListener('click', function() {
   randomCoverButton.style.display = "none";
   homeView.classList.add('hidden');
   saveCoverView.classList.remove('hidden');
-
 
 })
 
