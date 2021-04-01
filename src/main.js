3ff0e392b90e2d5090428c73d4573b894b4f063d
Jani Palomino <palomino.jani@gmail.com>
2021-03-31 22:23:46 -0700

    Add Random Cover Button Code

--- src/main.js
+++ src/main.js
@@ -7,6 +7,30 @@ var savedCovers = [
 ];
 var currentCover;
 
+//ITERATION 1 - "SHOW NEW RANDOM COVER BUTTON"
+/* THIS WORKS DON'T CHANGE IT */
+var randomCoverButton = document.querySelector('.random-cover-button');
+  randomCoverButton.addEventListener("click", displayRandomImage => {
+    var htmlImage = document.querySelector('.cover-image');
+    htmlImage.src = getRandomImage();
+  });
+
+//ITERATION 2 - "MAKE OWN COVER BUTTON SWITCHES TO FORM"
+/* STILL WORKING ON THIS */
+// var makeCoverButton = document.querySelector('.make-new-button');
+//   makeCoverButton.addEventListener("click", function() {
+//     .view-form-view-hidden.innerHTML
+//   }
+
+
+//working on merging variables from below
+// var getRandomIndex = {
+//   cover: covers,
+//   title: titles,
+//   tagline1: descripters,
+//   tagline2: descripters,
+// }
+
 // Add your event listeners here 👇
 window.addEventListener("load", displayRandomHomePage);
 window.addEventListener("load", displayRandomImage);
@@ -17,8 +41,17 @@ window.addEventListener("load", displayRandomTagline2);
 // Create your event handlers and other functions here 👇
 
 
+//ITERATION 0 - RANDOM MAIN PAGE IMAGE, TITLE, TAGLINE & 2 DESCRIPTORS
 // We've provided one function to get you started
-function getRandomIndex(array) {
+function getRandomIndex(cover, htmlImage, title, htmlTitle, tagline1, htmlTagline1, tagline2, htmlTagline2) {
+  // var htmlImage = document.querySelector('.cover-image');
+  // htmlImage.src = getRandomImage();
+  // var htmlTitle = document.querySelector('.cover-title');
+  // htmlTitle.innerText = getRandomTitle();
+  // var htmlTagline1 = document.querySelector('.tagline-1');
+  // htmlTagline1.innerText = getRandomTagline1();
+  // var htmlTagline2 = document.querySelector('.tagline-2');
+  // htmlTagline2.innerText = getRandomTagline2();
   return Math.floor(Math.random() * array.length);
 }
 
@@ -28,8 +61,8 @@ function getRandomImage() {
 }
 function displayRandomImage() {
   var htmlImage = document.querySelector('.cover-image');
-  console.dir(htmlImage);
-  console.log(htmlImage);
+  // console.dir(htmlImage);
+  // console.log(htmlImage);
   htmlImage.src = getRandomImage();
 }
 
