ea9d683017242073594e958d466aacb7234c915b
Fara Akhatova <faraakhatova@gmail.com>
2021-04-01 14:08:53 -0600

    Invoke function on currentCover variable, and place class Cover to a right function

--- src/main.js
+++ src/main.js
@@ -1,37 +1,35 @@
 //Create variables targetting the relevant DOM elements here 👇
-
-
 var coverImage = document.querySelector('.cover-image');
 var coverTitle = document.querySelector('.cover-title');
 var tagline1 = document.querySelector('.tagline-1');
 var tagline2 = document.querySelector('.tagline-2');
-var newRandomCoverBtn = document.querySelector('.random-cover-button');
 var mainCover = document.querySelector('.main-cover');
+var newRandomCoverBtn = document.querySelector('.random-cover-button');
 var makeOwnCoverBtn = document.querySelector('.make-new-button');
 var formView = document.querySelector('.form-view');
 var homeBtn = document.querySelector('.home-button');
 var saveCoverBtn = document.querySelector('.save-cover-button')
-// var savedView = document.querySelector('.view .saved-view .hidden');
 
 
 
-///// VARIABLE FOR INPUT FIELDS///////
+
 
 
 /////////// GLOBAL VARIABLES //////////
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-var currentCover = new Cover()
 
-/////////////////////////////////////
+var currentCover = getRandomCover()
+
+
 
 
 /////// EVENT LISTENERS👇//////
 
 newRandomCoverBtn.addEventListener('click', showRandomCover)
 makeOwnCoverBtn.addEventListener('click', showForm)
-///////////////////////////////
+
 
 ////// EVENT HANDLERS AND OTHER FUNCTIONS👇///////////
 
@@ -45,12 +43,12 @@ function getRandomCover() {
   coverTitle.innerText = titles[getRandomIndex(titles)];
   tagline1.innerText = descriptors[getRandomIndex(descriptors)];
   tagline2.innerText = descriptors[getRandomIndex(descriptors)];
+
+  currentCover = new Cover(coverImage.src, coverTitle.innerText, tagline1.innerText, tagline2.innerText);
 }
-getRandomCover()
 
 function showRandomCover() {
   getRandomCover()
-
 }
 
 function showForm() {
