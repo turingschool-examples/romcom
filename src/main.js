1f9697114775966625bbca6b2db5a29c3c48fbd7
Lindsay Schnell <lschnell8@gmail.com>
2021-03-31 22:00:46 -0600

    Debug

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ var coverTitle = document.querySelector('.cover-title');
 var tagline1 = document.querySelector('.tagline-1');
 var tagline2 = document.querySelector('.tagline-2');
 
-// We've provided a few variables below
+// We've provided a few variables below this
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
