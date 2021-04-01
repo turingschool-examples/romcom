d61c7b673c525e8fdc772a120d35e8e37767396b
Lauren Kessell <lkessell1@gmail.com>
2021-03-31 21:41:06 -0400

    Create new random cover when random cover button is clicked

--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
 // Create variables targetting the relevant DOM elements here 👇
 var coverImage = document.querySelector('.cover-image');
-
 var title = document.querySelector('.cover-title');
-
 var descriptor1 = document.querySelector('.tagline-1');
 var descriptor2 = document.querySelector('.tagline-2');
+var randomCoverButton = document.querySelector('.random-cover-button');
 
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-var randomCoverButton = document.querySelector('.random-cover-button');
+var currentCover;
 
 coverImage.src = covers[getRandomIndex(covers)];
 
@@ -22,10 +21,12 @@ descriptor2.innerText = descriptors[getRandomIndex(descriptors)];
 // Add your event listeners here 👇
 
 randomCoverButton.addEventListener('click', function() {
-  coverImage.src = covers[getRandomIndex(covers)];
-  title.innerText = titles[getRandomIndex(titles)];
-  descriptor1.innerText = descriptors[getRandomIndex(descriptors)];
-  descriptor2.innerText = descriptors[getRandomIndex(descriptors)];
+  // coverImage.src = covers[getRandomIndex(covers)];
+  // title.innerText = titles[getRandomIndex(titles)];
+  // descriptor1.innerText = descriptors[getRandomIndex(descriptors)];
+  // descriptor2.innerText = descriptors[getRandomIndex(descriptors)];
+  currentCover = new Cover(covers[getRandomIndex(covers)], titles[getRandomIndex(titles)], descriptors[getRandomIndex(descriptors)], descriptors[getRandomIndex(descriptors)]);
+  console.log(currentCover);
 });
 
 
