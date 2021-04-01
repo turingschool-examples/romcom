311090ee506630777e26d25783d566af901e9dbe
Shauna Myers <shaunamyers26@gmail.com>
2021-04-01 16:55:36 -0400

    Remove random cover button when save cover button clicked

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ var title = document.querySelector('.cover-title');
 var descriptor1 = document.querySelector('.tagline-1');
 var descriptor2 = document.querySelector('.tagline-2');
 var randomCoverButton = document.querySelector('.random-cover-button');
+var saveCoverButton = document.querySelector('.save-cover-button');
 
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
@@ -33,6 +34,10 @@ randomCoverButton.addEventListener('click', function() {
   descriptor2.innerText = currentCover.tagline2;
 });
 
+saveCoverButton.addEventListener('click', function() {
+  randomCoverButton.style.display = "none";
+
+})
 
 // Create your event handlers and other functions here 👇
 
