c28b8283e5cdd2a9225f043cd7c046421da48304
Shauna Myers <shaunamyers26@gmail.com>
2021-04-01 17:24:55 -0400

    Create save cover view when is clicked

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ var descriptor1 = document.querySelector('.tagline-1');
 var descriptor2 = document.querySelector('.tagline-2');
 var randomCoverButton = document.querySelector('.random-cover-button');
 var saveCoverButton = document.querySelector('.save-cover-button');
+var saveCoverView = document.querySelector('.saved-view');
+var homeView = document.querySelector('.home-view');
 
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
@@ -36,6 +38,9 @@ randomCoverButton.addEventListener('click', function() {
 
 saveCoverButton.addEventListener('click', function() {
   randomCoverButton.style.display = "none";
+  homeView.classList.add('hidden');
+  saveCoverView.classList.remove('hidden');
+
 
 })
 
