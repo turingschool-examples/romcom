2f20d08f90a0d286f79ffe11f23ee2c4e65a1a5b
Jani Palomino <palomino.jani@gmail.com>
2021-03-31 20:07:05 -0700

    Add Functions and Event Listeners

--- src/main.js
+++ src/main.js
@@ -8,7 +8,11 @@ var savedCovers = [
 var currentCover;
 
 // Add your event listeners here 👇
-
+window.addEventListener("load", displayRandomHomePage);
+window.addEventListener("load", displayRandomImage);
+window.addEventListener("load", displayRandomTitle);
+window.addEventListener("load", displayRandomTagline1);
+window.addEventListener("load", displayRandomTagline2);
 
 // Create your event handlers and other functions here 👇
 
@@ -16,4 +20,49 @@ var currentCover;
 // We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
+}
+
+function getRandomImage() {
+  var cover = covers[Math.floor(Math.random() * covers.length)]
+  return cover;
+}
+function displayRandomImage() {
+  var htmlImage = document.querySelector('.cover-image');
+  console.dir(htmlImage);
+  console.log(htmlImage);
+  htmlImage.src = getRandomImage();
+}
+
+function getRandomTitle() {
+  var title = titles[Math.floor(Math.random() * titles.length)]
+  return title;
+}
+function displayRandomTitle() {
+  var htmlTitle = document.querySelector('.cover-title');
+  htmlTitle.innerText = getRandomTitle();
+}
+
+function getRandomTagline1() {
+  var tagline1 = descriptors[Math.round(Math.random() * descriptors.length)]
+  return tagline1;
+}
+function displayRandomTagline1() {
+  var htmlTagline1 = document.querySelector('.tagline-1');
+  htmlTagline1.innerText = getRandomTagline1();
+}
+
+function getRandomTagline2() {
+  var tagline2 = descriptors[Math.round(Math.random() * descriptors.length)]
+  return tagline2;
+}
+function displayRandomTagline2() {
+  var htmlTagline2 = document.querySelector('.tagline-2');
+  htmlTagline2.innerText = getRandomTagline2();
+}
+
+function displayRandomHomePage() {
+  displayRandomImage();
+  displayRandomTitle();
+  displayRandomTagline1();
+  displayRandomTagline2();
 }
