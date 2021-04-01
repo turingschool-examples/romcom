111c3d0dd2ab995555d778c9b9c65d36c7d53b6c
Fara Akhatova <faraakhatova@gmail.com>
2021-03-31 19:21:02 -0600

    Clean up code

--- src/main.js
+++ src/main.js
@@ -1,26 +1,30 @@
 //Create variables targetting the relevant DOM elements here 👇
+
+
 var coverImage = document.querySelector('.cover-image');
 var coverTitle = document.querySelector('.cover-title');
 var tagline1 = document.querySelector('.tagline-1');
 var tagline2 = document.querySelector('.tagline-2');
 var newRandomCoverBtn = document.querySelector('.random-cover-button');
-// Global Variables //
 
 
+
+/////////// GLOBAL VARIABLES //////////
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-var currentCover;
+var currentCover = new Cover()
 
-////////////////////////////
-
-
+/////////////////////////////////////
 
 
-// Add your event listeners here 👇
+/////// EVENT LISTENERS👇//////
+
 newRandomCoverBtn.addEventListener('click', showRandomCover)
 
-// Create your event handlers and other functions here 👇
+///////////////////////////////
+
+////// EVENT HANDLERS AND OTHER FUNCTIONS👇///////////
 
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
@@ -35,11 +39,8 @@ function getRandomCover() {
 }
 getRandomCover()
 
+
 function showRandomCover() {
   getRandomCover()
 
 }
-
-
-
-// We've provided one function to get you started
