4eb92d569a29934e0e14229e505b168efc1c056c
Natalia Buchmiller <nataliambuchmiller@gmail.com>
2021-04-02 07:48:08 -0700

    updated variable names

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ var coverImage = document.querySelector(".cover-image");
 var coverTitle = document.querySelector(".cover-title");
 var firstTagline = document.querySelector(".tagline-1");
 var secondTagline = document.querySelector(".tagline-2");
-var makeCover = document.querySelector('.make-new-button')
+
+var randomCoverButton = document.querySelector('.random-cover-button')
+var makeNewButton = document.querySelector('.make-new-button')
 
 
 
@@ -11,12 +13,13 @@ var makeCover = document.querySelector('.make-new-button')
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-var currentCover = new Cover(coverImage, coverTitle, firstTagline, secondTagline);
+var currentCover = new Cover();
 
 // Add your event listeners here 👇
 
 window.addEventListener('load', getRandomCover);
-makeCover.addEventListener('click', )
+randomCoverButton.addEventListener('click', createRandomCover);
+
 
 
 // Create your event handlers and other functions here 👇
@@ -28,32 +31,37 @@ makeCover.addEventListener('click', )
 
 // The form to make a new cover should be visable
 
-function getRandomCover() {
+function createRandomCover() {
   changeCover();
   changeTitle();
   changeFirstDescriptor();
   changeSecondDescriptor();
+
 }
 
 function changeCover() {
   var index = getRandomIndex(covers);
   coverImage.src = covers[index];
-  // console.log(covers[index]);
+  currentCover.cover = covers[index];
+  console.log(currentCover);
 }
 
 function changeTitle() {
   var index = getRandomIndex(titles);
   coverTitle.innerHTML = titles[index];
+  currentCover.title = titles[index];
 }
 
 function changeFirstDescriptor() {
   var index = getRandomIndex(descriptors);
   firstTagline.innerHTML = descriptors[index];
+  currentCover.tagline1 = descriptors[index];
 }
 
 function changeSecondDescriptor() {
   var index = getRandomIndex(descriptors);
   secondTagline.innerHTML = descriptors[index];
+  currentCover.tagline2 = descriptors[index];
 }
 
 
