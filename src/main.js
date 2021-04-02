ecf9c08bf3200f67b64af31fca8624ac04e9d381
Shauna Myers <shaunamyers26@gmail.com>
2021-04-02 17:58:43 -0400

    Add user input from form, push to arrays

--- src/main.js
+++ src/main.js
@@ -4,17 +4,24 @@ var coverImage = document.querySelector('.cover-image');
 var title = document.querySelector('.cover-title');
 var descriptor1 = document.querySelector('.tagline-1');
 var descriptor2 = document.querySelector('.tagline-2');
-var randomCoverButton = document.querySelector('.random-cover-button');
 
+var randomCoverButton = document.querySelector('.random-cover-button');
 var makeCoverButton = document.querySelector('.make-new-button');
 var saveCoverButton = document.querySelector('.save-cover-button');
 var homeButton = document.querySelector('.home-button');
+var createNewBookButton = document.querySelector('.create-new-book-button')
+var viewSavedButton = document.querySelector('.view-saved-button');
 
 var formView = document.querySelector('.form-view');
 var homeView = document.querySelector('.home-view');
-// var saveCoverView = document.querySelector('.saved-view');
 var viewSavedView = document.querySelector('.saved-view');
 var viewSavedButton = document.querySelector('.view-saved-button');
+
+var inputCover = document.querySelector('.user-cover');
+var inputTitle = document.querySelector('.user-title');
+var inputFirstDescriptor = document.querySelector('.user-desc1');
+var inputSecondDescriptor = document.querySelector('.user-desc2');
+// var saveCoverView = document.querySelector('.saved-view');
 
 var currentCover;
 
@@ -22,6 +29,11 @@ var currentCover;
 //google add event listener on page load
 //Event listener should be 1-3 lines, only if you have an anonymous function...no logic...only FIRE/INVOKE functions
 randomCoverButton.addEventListener('click', makeRandomCover);
+createNewBookButton.addEventListener('click', function() {
+  event.preventDefault();
+  createNewBook;
+  showNewBookHome;
+});
   // coverImage.src = covers[getRandomIndex(covers)];
   // title.innerText = titles[getRandomIndex(titles)];
   // descriptor1.innerText = descriptors[getRandomIndex(descriptors)];
@@ -55,6 +67,13 @@ function changeViewSavedView() {
   viewSavedView.classList.remove('hidden');
   homeView.classList.add('hidden');
   formView.classList.add('hidden');
+}
+
+function createNewBook() {
+  covers.push(inputCover.value);
+  titles.push(inputTitle.value);
+  descriptors.push(inputFirstDescriptor.value);
+  descriptors.push(inputSecondDescriptor.value);
 }
 
 // //Reflect changes from above
