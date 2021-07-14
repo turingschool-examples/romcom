6234fddef3a06e6a77630545397129ab739800cb
Joshua Benefield <benefield.joshua@gmail.com>
2021-07-14 17:25:39 -0600

    Complete iteration 0+1

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 // Create variables targetting the relevant DOM elements here 👇
-
+var coverImage = document.querySelector('.cover-image');
+var coverTitle = document.querySelector('.cover-title');
+var coverTagline1 = document.querySelector('.tagline-1');
+var coverTagline2 = document.querySelector('.tagline-2');
+var randomButton = document.querySelector('.random-cover-button');
+var form = document.querySelector('.form-view');
+var home = document.querySelector('.home-view');
+var newCoverButton = document.querySelector('.make-new-button');
+var saveCover = document.querySelector('.save-cover-button');
+var homeButton = document.querySelector('.home-button');
 
 // We've provided a few variables below
 var savedCovers = [
@@ -8,11 +17,30 @@ var savedCovers = [
 var currentCover;
 
 // Add your event listeners here 👇
-
+randomButton.onclick = newCover;
+newCoverButton.onclick = customCover;
 
 // Create your event handlers and other functions here 👇
+function newCover() {
+ currentCover = new Cover(covers[getRandomIndex(covers)], titles[getRandomIndex(titles)], descriptors[getRandomIndex(descriptors)], descriptors[getRandomIndex(descriptors)]);
+ coverImage.src = currentCover.cover;
+ coverTitle.innerText = currentCover.title;
+ coverTagline1.innerText = currentCover.tagline1;
+ coverTagline2.innerText = currentCover.tagline2;
+}
 
+function customCover() {
+  form.style.display = "block";
+  home.style.display = "none";
+  saveCover.style.display = "none";
+  randomButton.style.display = "none";
+  homeButton.style.display = "block";
+}
 
+coverImage.src = covers[getRandomIndex(covers)];
+coverTitle.innerText = titles[getRandomIndex(titles)];
+coverTagline1.innerText = descriptors[getRandomIndex(descriptors)];
+coverTagline2.innerText = descriptors[getRandomIndex(descriptors)];
 // We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
