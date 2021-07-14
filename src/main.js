165d47cb8ca053b65bde2018b0544925c670331c
Joshua Horner <lesliewbusiness@gmail.com>
2021-07-14 13:53:42 -0600

    Initial commit with pseudocode

--- src/main.js
+++ src/main.js
@@ -17,3 +17,75 @@ var currentCover;
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
+
+
+// ITERATION 0: MAIN PAGE
+//  - Write JS function to randomize image, title, and text
+//    - Function should be run using window.onload event
+//        - (Make this function a helper function so it can
+//          be run using a button later)
+//        - Function should create new class instance
+//    - Use getRandomIndex to generate a random number
+//    - Use innerText to target and change assets according to
+//      generated index position (see above)
+//      - New poster is assigned to the currentCover variable for saving
+//
+//  (Use preventDefault()? Be sure to research this)
+//
+// ITERATION 1: SHOW RANDOM COVER
+//  - Function should generate new randomized poster on button click
+//      --> ('.random-cover-button')
+//    - Function is run as a new class instance
+//    - Randomized poster updates currentCover variable
+//      - Function should render changes to the DOM
+//
+// ITERATION 2: SWITCHING VIEWS
+//  - Function should switch between main view and form view
+//    - (Use 'hidden' class to change visibility)
+//    - Function should toggle visibility for 'Show New Random Cover'
+//      and 'Save Cover'
+//        - When form view is visible, 'Home' button should be visible
+//          - (Use 'hidden' class in CSS just like above)
+//  - Saved Covers view should be visible on button click
+//    - After click, 'Show New Random Cover' and 'Save Cover' buttons
+//      should be hidden
+//        - 'Home' button should be visible
+//
+//    NOTE: Saved posters do not need to be displayed in this iteration.
+//          Only the saved posters view needs to be displayed.
+//
+// ITERATION 3: CREATING A NEW COVER
+//  - Should allow users to create a cover using input fields in form view
+//    - 'Save' button should be accessed using query selector
+//      - On click (event handler), a helper function should be run which:
+//        - Saves submitted data into respective arrays for future randomization
+//      - As well as a helper function which:
+//        - Generates new instance of Cover class
+//        - Updates currentCover variable
+//      - Finally, a third helper function should be fired which:
+//        - Changes back to the main view
+//        - Displays the updated currentCover
+//
+// ITERATION 4: SAVING & VIEWING COVERS
+//  - On clicking 'Save Cover', the current cover's elements should be
+//    added to the savedCovers array (saved as an object)
+//    - Unique covers can only be saved once (use a for-loop?)
+//  - When a user clicks 'View Saved Covers', the saved covers section
+//    should be displayed
+//  - All saved covers in the savedCovers array should be displayed in a grid
+//    - Note: None of this needs to persist on page load
+//
+// ITERATION 5: DELETING SAVED COVERS
+//  - In the saved covers view, double-clicking a saved poster will delete it
+//    - Potential code structure:
+//    - Use event targeting to identify cover object
+//    - Nested function variables will be assigned to the image, title, and text
+//      - We may be able to access the entire object. In either case, the
+//        variables will need to be stored as an object for deletion
+//    - A for-loop will iterate through the savedPosters array to find
+//      a matching object.
+//      - Use savedPosters.splice(i, 1) to remove poster from array
+//    - Helper function should be invoked to update the Saved Posters view
+//
+// (See project spec for optional extensions:
+//  https://frontend.turing.edu/projects/module-1/romcom-pair.html)
