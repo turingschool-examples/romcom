d3cad58b976921add45f670fc91fd57bf36f52e4
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-07-14 20:06:21 -0700

    Randomize title on load

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 let cover = document.querySelector('.cover-image');
 let title = document.querySelector('.cover-title');
 let tagline = document.querySelector('.tagline');
-let randomIndex = getRandomIndex(covers);
+let randomCoversIndex = getRandomIndex(covers);
+let randomTitlesIndex = getRandomIndex(titles);
+console.log(randomTitlesIndex);
 
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 var currentCover = cover;
 
-cover.src = covers[randomIndex];
+cover.src = covers[randomCoversIndex];
+
+title.innerText = titles[randomTitlesIndex];
 
 // Add your event listeners here 👇
 
