e6f165a2946525ab3a135e26b91d73594ad6908d
Joshua Benefield <benefield.joshua@gmail.com>
2021-07-14 20:30:49 -0600

    Refactor variables

--- src/main.js
+++ src/main.js
@@ -1,16 +1,21 @@
 // Create variables targetting the relevant DOM elements here 👇
+//current cover
 var coverImage = document.querySelector('.cover-image');
 var coverTitle = document.querySelector('.cover-title');
 var coverTagline1 = document.querySelector('.tagline-1');
 var coverTagline2 = document.querySelector('.tagline-2');
-var randomButton = document.querySelector('.random-cover-button');
-var form = document.querySelector('.form-view');
-var home = document.querySelector('.home-view');
-var newCoverButton = document.querySelector('.make-new-button');
-var saveCoverButton = document.querySelector('.save-cover-button');
-var homeButton = document.querySelector('.home-button');
+//pages
+var homeView = document.querySelector('.home-view');
 var savedCoversView = document.querySelector('.saved-view');
+var formView = document.querySelector('.form-view');
+//buttons
+var homeButton = document.querySelector('.home-button');
+var randomButton = document.querySelector('.random-cover-button');
+var saveCoverButton = document.querySelector('.save-cover-button');
 var viewSavedButton = document.querySelector('.view-saved-button');
+var newCoverButton = document.querySelector('.make-new-button');
+var generateCoverButton = document.querySelector('create-new-book-button');
+//form inputs
 
 // We've provided a few variables below
 var savedCovers = [
@@ -18,14 +23,22 @@ var savedCovers = [
 ];
 var currentCover;
 
+//home page
+coverImage.src = covers[getRandomIndex(covers)];
+coverTitle.innerText = titles[getRandomIndex(titles)];
+coverTagline1.innerText = descriptors[getRandomIndex(descriptors)];
+coverTagline2.innerText = descriptors[getRandomIndex(descriptors)];
+
 // Add your event listeners here 👇
-randomButton.onclick = newCover;
+homeButton.onclick = viewHome;
+randomButton.onclick = randomCover;
 newCoverButton.onclick = viewForm;
 viewSavedButton.onclick = viewSavedCover;
-homeButton.onclick = viewHome;
+generateCoverButton.onclick = generateCustomCover;
+
 
 // Create your event handlers and other functions here 👇
-function newCover() {
+function randomCover() {
  currentCover = new Cover(covers[getRandomIndex(covers)], titles[getRandomIndex(titles)], descriptors[getRandomIndex(descriptors)], descriptors[getRandomIndex(descriptors)]);
  coverImage.src = currentCover.cover;
  coverTitle.innerText = currentCover.title;
@@ -34,38 +47,48 @@ function newCover() {
 }
 
 function viewForm() {
-  form.style.display = "block";
-  home.style.display = "none";
-  saveCoverButton.style.display = "none";
-  randomButton.style.display = "none";
+  homeView.style.display = "none";
+  savedCoversView.style.display = "none";
+  formView.style.display = "block";
   homeButton.style.display = "block";
+  randomButton.style.display = "none";
+  saveCoverButton.style.display = "none";
+  viewSavedButton.style.display = "block";
   newCoverButton.style.display = "none";
 }
 
 function viewSavedCover() {
-  home.style.display = "none";
-  homeButton.style.display = "block";
-  saveCoverButton.style.display = "none";
+  homeView.style.display = "none";
   savedCoversView.style.display = "block";
+  formView.style.display = "none";
+  homeButton.style.display = "block";
   randomButton.style.display = "none";
+  saveCoverButton.style.display = "none";
   viewSavedButton.style.display = "none";
-}
-
-function viewHome() {
-  home.style.display = "block";
-  form.style.display = "none";
-  savedCoversView.style.display = "none";
-  randomButton.style.display = "block";
-  viewSavedButton.style.display = "block";
-  saveCoverButton.style.display = "block";
-  homeButton.style.display = "none";
   newCoverButton.style.display = "block";
 }
 
-coverImage.src = covers[getRandomIndex(covers)];
-coverTitle.innerText = titles[getRandomIndex(titles)];
-coverTagline1.innerText = descriptors[getRandomIndex(descriptors)];
-coverTagline2.innerText = descriptors[getRandomIndex(descriptors)];
+function viewHome() {
+  homeView.style.display = "block";
+  savedCoversView.style.display = "none";
+  formView.style.display = "none";
+  homeButton.style.display = "none";
+  randomButton.style.display = "block";
+  saveCoverButton.style.display = "block";
+  viewSavedButton.style.display = "block";
+  newCoverButton.style.display = "block";
+}
+
+/*function generateCustomCover() {
+  var imageInput = document.querySelector('.user-cover'.value);
+  var titleInput = document.querySelector('.user-title'.value);
+  coverImage.src = imageInput;
+  coverTitle.innerText = titleInput;
+  covers.push(imageInput);
+  viewhome();
+}*/
+
+
 // We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
