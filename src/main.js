311ba5584eca4eaf5d6fff0c8a20817c95618405
Wil Huffman <me@wil-huffman.io>
2021-07-14 19:53:40 -0500

    Make Note line 25

--- src/main.js
+++ src/main.js
@@ -17,13 +17,12 @@ var titleInput = document.querySelector("#title");
 var descriptorInput1 = document.querySelector("#descriptor1");
 var descriptorInput2 = document.querySelector("#descriptor2");
 
-
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 var currentCover;
 
-
+//Programming is fun...?
 
 
 
