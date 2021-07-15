79928aaf5ee5383932cf8a1d10282d30d0e4ac50
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-07-14 19:43:33 -0700

    Randomize cover image on load

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
-// Create variables targetting the relevant DOM elements here 👇
+let cover = document.querySelector('.cover-image');
+let title = document.querySelector('.cover-title');
+let tagline = document.querySelector('.tagline');
+let randomIndex = getRandomIndex(covers);
 
-
-// We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-var currentCover;
+var currentCover = cover;
+
+cover.src = covers[randomIndex];
 
 // Add your event listeners here 👇
 
@@ -17,3 +20,5 @@ var currentCover;
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
+
+var exapmpelaklsjjf;alsd;klfajskld;f
