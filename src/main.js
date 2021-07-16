62b3203fda69e2fe83d9471364809a131be6369a
Joshua Benefield <benefield.joshua@gmail.com>
2021-07-16 01:10:09 -0600

    Complete iteration 3

--- src/main.js
+++ src/main.js
@@ -8,26 +8,34 @@ var coverTagline2 = document.querySelector('.tagline-2');
 var homeView = document.querySelector('.home-view');
 var savedCoversView = document.querySelector('.saved-view');
 var formView = document.querySelector('.form-view');
+var savedCoversSection = document.querySelector('.saved-covers-section');
 //buttons
 var homeButton = document.querySelector('.home-button');
 var randomButton = document.querySelector('.random-cover-button');
 var saveCoverButton = document.querySelector('.save-cover-button');
 var viewSavedButton = document.querySelector('.view-saved-button');
 var newCoverButton = document.querySelector('.make-new-button');
-var generateCoverButton = document.querySelector('create-new-book-button');
+var generateCoverButton = document.querySelector('.create-new-book-button');
 //form inputs
 
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-var currentCover;
+var currentCover = {
+  cover: covers[getRandomIndex(covers)],
+  title: titles[getRandomIndex(titles)],
+  tagline1: descriptors[getRandomIndex(descriptors)],
+  tagline2: descriptors[getRandomIndex(descriptors)],
 
-//home page
-coverImage.src = covers[getRandomIndex(covers)];
-coverTitle.innerText = titles[getRandomIndex(titles)];
-coverTagline1.innerText = descriptors[getRandomIndex(descriptors)];
-coverTagline2.innerText = descriptors[getRandomIndex(descriptors)];
+};
+//homepage
+coverImage.src = currentCover.cover;
+coverTitle.innerText = currentCover.title;
+coverTagline1.innerText = currentCover.tagline1;
+coverTagline2.innerText = currentCover.tagline2;
+
+
 
 // Add your event listeners here 👇
 homeButton.onclick = viewHome;
@@ -35,6 +43,7 @@ randomButton.onclick = randomCover;
 newCoverButton.onclick = viewForm;
 viewSavedButton.onclick = viewSavedCover;
 generateCoverButton.onclick = generateCustomCover;
+saveCoverButton.onclick = saveCover;
 
 
 // Create your event handlers and other functions here 👇
@@ -66,6 +75,19 @@ function viewSavedCover() {
   saveCoverButton.style.display = "none";
   viewSavedButton.style.display = "none";
   newCoverButton.style.display = "block";
+  //this is what creates all the saved covers
+  for (var i=0; i<savedCovers.length; i++) { //iterates every index in the saved covers
+    var span = document.createElement("span"); // this creates a span for each cover
+    savedCoversSection.append(div); // this adds that span to the saved covers page
+      span.classList.add("mini-cover"); //this adds the css style to the span we created
+      span.innerHTML =`
+        <img class = "cover-image" src=${savedCovers[i].cover}>
+        <h2 class = "cover-title ">${savedCovers[i].title}</h2>
+        <h3 class = "tagline">A tale of <span class="tagline-1">${savedCovers[i].tagline1}</span> and <span class="tagline-2">${savedCovers[i].tagline2}</span></h3>
+        <img class="price-tag" src="./assets/price.png">
+        <img class="overlay" src="./assets/overlay.png">
+        `;// lines 83-89 are put into the span we created. It contains the html that creates the covers, and the css that styles them. I modeled them after the main cover.
+  }
 }
 
 function viewHome() {
@@ -79,17 +101,34 @@ function viewHome() {
   newCoverButton.style.display = "block";
 }
 
-/*function generateCustomCover() {
-  var imageInput = document.querySelector('.user-cover'.value);
-  var titleInput = document.querySelector('.user-title'.value);
-  coverImage.src = imageInput;
-  coverTitle.innerText = titleInput;
-  covers.push(imageInput);
-  viewhome();
-}*/
 
+function generateCustomCover() {
+  event.preventDefault();
+  var imageInput = document.querySelector('.user-cover').value;
+  var titleInput = document.querySelector('.user-title').value;
+  var descriptor1Input = document.querySelector('.user-desc1').value;
+  var descriptor2Input = document.querySelector('.user-desc2').value;
+  currentCover = new Cover(imageInput, titleInput, descriptor1Input, descriptor2Input);
+  coverImage.src = currentCover.cover;
+  coverTitle.innerText = currentCover.title;
+  coverTagline1.innerText = currentCover.tagline1;
+  coverTagline2.innerText = currentCover.tagline2;
+  covers.push(imageInput);
+  titles.push(titleInput);
+  descriptors.push(descriptor1Input, descriptor2Input);
+  viewHome();
+}
+
+function saveCover() { //this saves the current cover as long as it's unique 
+  for (var i=0; i<savedCovers.length; i++) {
+    if (savedCovers.indexOf(currentCover) === -1) {
+      savedCovers.unshift(currentCover);
+    }
+  }
+}
 
 // We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
+
 }
