b2bb4302a394a7d7c991ffb85284c27fa17586dd
Brian Peterson <bpeterson1579@gmail.com>
2021-07-15 20:05:49 -0600

    Add event listeners for random book cover button

--- src/main.js
+++ src/main.js
@@ -2,44 +2,52 @@
 
 var coverImgSrc = getRandomCover(covers); //random img source from covers array
 var title = getRandomTitle(titles); //random title from titles array
-var descriptor1 = getRandomDescriptor(descriptors); //random first descriptor from array
-var descriptor2 = getRandomDescriptor(descriptors); //random second descriptor from array
+var descriptor1 = getRandomDescriptor1(descriptors); //random first descriptor from array
+var descriptor2 = getRandomDescriptor2(descriptors); //random second descriptor from array
 
 var changeCover = document.querySelector('img');
 var changeTitle = document.querySelector('h2');
 var changeDescriptor1 = document.querySelector('.tagline-1');
 var changeDescriptor2 = document.querySelector('.tagline-2');
-
-
-// We've provided a few variables below
-var savedCovers = [
-  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
-];
 var currentCover = new Cover(coverImgSrc, title, descriptor1, descriptor2);
 
-changeCover.src = currentCover.cover;
-changeTitle.innerText = currentCover.title;
-changeDescriptor1.innerText = this.descriptor1;
-changeDescriptor2.innerText = this.descriptor2;
+// We've provided a few variables below
+// var savedCovers = [
+//   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
+// ];
+
+
+function createCover() {
+  changeCover.src = currentCover.cover;
+  changeTitle.innerText = currentCover.title;
+  changeDescriptor1.innerText = currentCover.descriptor1;
+  changeDescriptor2.innerText = currentCover.descriptor2;
+};
 
 // Add your event listeners here 👇
-
+var pageLoad = document.querySelector('body');
 
 // Create your event handlers and other functions here 👇
-
+pageLoad.addEventListener('load', createCover);
+// randomButton.addEventListener('click', createCover);
 
 // We've provided one function to get you started
 function getRandomCover(coversArray) {
   var random = Math.floor(Math.random() * covers.length);
   return covers[random];
-}
+};
 
 function getRandomTitle(titlesArray) {
   var random = Math.floor(Math.random() * titles.length);
   return titles[random];
-}
+};
 
-function getRandomDescriptor(descriptorsArray) {
+function getRandomDescriptor1(descriptorsArray) {
     var random = Math.floor(Math.random() * descriptors.length);
     return descriptors[random];
-}
+};
+
+function getRandomDescriptor2(descriptorsArray) {
+    var random = Math.floor(Math.random() * descriptors.length);
+    return descriptors[random];
+};
