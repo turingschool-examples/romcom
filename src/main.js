d0cfac457db5ab86691513fe55fe51bce294200e
Nadia Naji <nadialnaji@gmail.com>
2021-07-17 00:29:50 -0600

    Create new cover feature

--- src/main.js
+++ src/main.js
@@ -97,28 +97,61 @@ var descriptors = [
 
 
 
-
-
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 var currentCover;
 
+var makeMyBookButton = document.querySelector('.create-new-book-button');
+
+
+
+makeMyBookButton.addEventListener('click', function(event){
+  var userCoverURL = document.querySelector('#cover');
+  var userTitle = document.querySelector('#title');
+  var userDescriptor1 = document.querySelector('#descriptor1');
+  var userDescriptor2 = document.querySelector('#descriptor2');
+  var uniqueCoverInput = new UniqueCover(userCoverURL.value, userTitle.value, userDescriptor1.value, userDescriptor2.value);
+  console.log(uniqueCoverInput);
+  toggleToHome();
+  document.querySelector('.cover-image').src = uniqueCoverInput.coverURL;
+  document.querySelector('.cover-title').innerText = uniqueCoverInput.title;
+  document.querySelector('.tagline-1').innerText = uniqueCoverInput.descriptor1;
+  document.querySelector('.tagline-2').innerText = uniqueCoverInput.descriptor2;
+  event.preventDefault();
+});
+
+class UniqueCover {
+  constructor(userInputURL, userInputTitle, userDescriptor1, userDescriptor2){
+    this.coverURL = userInputURL;
+    this.title = userInputTitle;
+    this.descriptor1 = userDescriptor1;
+    this.descriptor2 = userDescriptor2;
+  }
+};
+
+
 var randomCoverButton = document.querySelector('.random-cover-button');
 var makeYourOwnCoverButton = document.querySelector('.make-new-button');
 var homeView = document.querySelector('.home-view');
 var form = document.querySelector('.form-view');
+var savedCoversView = document.querySelector('.saved-view')
 var showNewRandomCoverButton = document.querySelector('.random-cover-button');
-var viewSavedCoversButton = document.querySelector('.view-saved-button');
+var saveCoverButton = document.querySelector('.save-cover-button');
 var homeButton = document.querySelector('.home-button');
+var viewSavedCoversButton = document.querySelector('.view-saved-button')
 
 // Add your event listeners here 👇
-randomCoverButton.addEventListener('click',function(){
+randomCoverButton.addEventListener('click',function() {
   generateCover(coverImages, coverTitles, descriptors)
 });
 
 makeYourOwnCoverButton.addEventListener('click', toggleToForm);
+
+viewSavedCoversButton.addEventListener('click', displaySavedCovers);
+
+homeButton.addEventListener('click', toggleToHome);
 
 
 // Create your event handlers and other functions here 👇
@@ -126,17 +159,26 @@ function toggleToForm(){
   homeView.classList.add('hidden');
   form.classList.remove('hidden');
   showNewRandomCoverButton.classList.add('hidden');
-  viewSavedCoversButton.classList.add('hidden');
+  saveCoverButton.classList.add('hidden');
   homeButton.classList.remove('hidden');
 };
 
+function displaySavedCovers() {
+  savedCoversView.classList.remove('hidden');
+  form.classList.add('hidden');
+}
 
-
+function toggleToHome() {
+  homeView.classList.remove('hidden');
+  savedCoversView.classList.add('hidden');
+  showNewRandomCoverButton.classList.remove('hidden');
+  homeButton.classList.add('hidden');
+  saveCoverButton.classList.remove('hidden')
+}
 
 
 
 function generateCover(listOfImages, listOfTitles, listOfDescriptors) {
-  console.log(listOfImages, listOfTitles, listOfDescriptors);
   var randomImage = listOfImages[Math.floor(Math.random() * listOfImages.length)];
   var randomTitle = listOfTitles[Math.floor(Math.random() * listOfTitles.length)];
   var randomDescriptor1 = listOfDescriptors[Math.floor(Math.random() * listOfDescriptors.length)];
