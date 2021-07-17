4fec822bde9a9625f57aa44ab0c7b45458c0a4b2
Jessica Organ <jorgan612@gmail.com>
2021-07-17 11:07:13 -0600

    Add home page function and conditional statement

--- src/main.js
+++ src/main.js
@@ -7,8 +7,12 @@ var randomCoverBttn = document.querySelector('.random-cover-button');
 var makeCoverBttn = document.querySelector('.make-new-button');
 var homePage = document.querySelector('.home-view');
 var formPage = document.querySelector('.form-view');
-var saveCover = document.querySelector('.save-cover-button');
+var saveCoverBttn = document.querySelector('.save-cover-button');
 var homeBttn = document.querySelector('.home-button');
+var viewSavedCoversBttn = document.querySelector('.view-saved-button');
+var savedCoversPage = document.querySelector('.saved-view');
+
+
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
@@ -21,7 +25,8 @@ var currentCover;
 
 randomCoverBttn.addEventListener('click', displayRandomCover);
 makeCoverBttn.addEventListener('click', displayFormView);
-
+viewSavedCoversBttn.addEventListener('click', displaySavedCovers);
+homeBttn.addEventListener('click', displayHomeView);
 
 // Create your event handlers and other functions here 👇
 
@@ -52,12 +57,40 @@ function displayRandomCover() {
 }
 
 function displayFormView() {
-  formPage.classList.toggle('hidden');
-  homePage.classList.toggle('hidden');
-  saveCover.classList.toggle('hidden');
-  randomCoverBttn.classList.toggle('hidden');
-  homeBttn.classList.toggle('hidden');
+  formPage.classList.remove('hidden');
+  homePage.classList.add('hidden');
+  saveCoverBttn.classList.add('hidden');
+  randomCoverBttn.classList.add('hidden');
+  homeBttn.classList.remove('hidden');
+  makeCoverBttn.disabled = true;
 };
+
+function displaySavedCovers() {
+  savedCoversPage.classList.remove('hidden');
+  homePage.classList.add('hidden');
+  saveCoverBttn.classList.add('hidden');
+  randomCoverBttn.classList.add('hidden');
+  homeBttn.classList.remove('hidden');
+};
+
+function displayHomeView() {
+  if (formPage.classList !== 'hidden') {
+    formPage.classList.add('hidden');
+    homePage.classList.remove('hidden');
+    savedCoversPage.classList.add('hidden');
+    saveCoverBttn.classList.remove('hidden');
+    randomCoverBttn.classList.remove('hidden');
+    homeBttn.classList.add('hidden');
+    makeCoverBttn.disabled = false;
+  } else {
+    savedCoversPage.classList.add('hidden');
+    homePage.classList.remove('hidden');
+    saveCoverBttn.classList.remove('hidden');
+    randomCoverBttn.classList.remove('hidden');
+    homeBttn.classList.add('hidden');
+  } 
+}
+
 
 // Iteration 2
 // To be able to switch the views when the appropriate buttons are clicked.
@@ -70,8 +103,16 @@ function displayFormView() {
 // On the home page we should see all buttons except home bttn
 
 // Steps:
+// Form view-
 // [X]Create a fcn that will handle event.
 // [X]This fcn will hide home page and show the form.
-// []It will hide the "show new random cover" and "save cover" bttns.
-// []but we will now see the home page bttn.
-//
+// [X]It will hide the "show new random cover" and "save cover" bttns.
+// [X]but we will now see the home page bttn.
+// Saved covers view:
+// []Create a fcn that will display saved cover views and hide homepage view.
+// []Hide "show new random cover" and "save cover" bttns.
+// []"Home" bttn should be visible.
+// NEED:
+// []function
+// []view saved covers button querySelector
+// []addEventListener for bttn "saved covers", "home"
