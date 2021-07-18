fe7941bd59c50758c3544093c2424609834e9b0e
Elise Beall <elisebeall@gmail.com>
2021-07-17 22:27:24 -0600

    Add proper transition between buttons

--- src/main.js
+++ src/main.js
@@ -7,6 +7,11 @@ var secondDescriptorOnLoad = document.querySelector('.tagline-2');
 
 // Button variables
 var randomButton = document.querySelector('.random-cover-button');
+var homeButton = document.querySelector('.home-button');
+// var saveCoverButton = document.querySelector('.save-cover-button');
+var viewSavedCoversButton = document.querySelector('.view-saved-button');
+var makeNewCoverButton = document.querySelector('.make-new-button');
+var formView = document.querySelector('form');
 
 var currentPage = 'home-view';
 
@@ -15,6 +20,33 @@ var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 
+// Add your event listeners here 👇
+document.addEventListener("DOMContentLoaded", createCover);
+
+randomButton.addEventListener("click", createCover);
+
+homeButton.addEventListener('click', function() {
+  changePage('home-view', currentPage);
+  changeButton2('random-cover-button', 'save-cover-button', 'home-button');
+});
+
+// saveCoverButton.addEventListener('click', saveCover);
+
+viewSavedCoversButton.addEventListener('click', function() {
+  changePage('saved-view', currentPage);
+  changeButton1('home-button', 'save-cover-button', 'random-cover-button');
+  formView.classList.add('hidden');
+});
+
+makeNewCoverButton.addEventListener('click', function() {
+  changePage('form-view', currentPage);
+  changeButton1('home-button', 'save-cover-button', 'random-cover-button');
+  formView.classList.remove('hidden');
+});
+
+
+
+// Create your event handlers and other functions here 👇
 function createCover() {
   var currentCover = new Cover(covers[getRandomIndex(covers)], titles[getRandomIndex(titles)], descriptors[getRandomIndex(descriptors)], descriptors[getRandomIndex(descriptors)]);
   coverOnLoad.src = currentCover.cover;
@@ -23,48 +55,19 @@ function createCover() {
   secondDescriptorOnLoad.innerText = currentCover.tagline2;
 }
 
-// Add your event listeners here 👇
-document.addEventListener("DOMContentLoaded", createCover);
-
-var homeButton = document.querySelector('.home-button');
-homeButton.addEventListener('click', function() {
-  changePage('home-view', currentPage);
-  changeButton2('random-cover-button', 'save-cover-button', 'home-button');
-});
-
-// var saveCoverButton = document.querySelector('.save-cover-button');
-// saveCoverButton.addEventListener('click', saveCover);
-
-var viewSavedCoversButton = document.querySelector('.view-saved-button');
-viewSavedCoversButton.addEventListener('click', function() {
-  changePage('saved-view', currentPage);
-  changeButton1('home-button', 'save-cover-button', 'random-cover-button');
-});
-
-var makeNewCoverButton = document.querySelector('.make-new-button');
-makeNewCoverButton.addEventListener('click', function() {
-  changePage('form-view', currentPage);
-  changeButton1('home-button', 'save-cover-button', 'random-cover-button');
-});
-
-// Create your event handlers and other functions here 👇
-randomButton.addEventListener("click", createCover);
-
 function changePage(newPage, currentPage) {
-  var showPage = document.querySelector(`.${newPage}`);
-  showPage.classList.add('view');
-  showPage.classList.remove('hidden');
-
   var hidePage = document.querySelector(`.${currentPage}`);
   hidePage.classList.add('hidden');
-  hidePage.classList.remove('view');
+
+  var showPage = document.querySelector(`.${newPage}`);
+  showPage.classList.remove('hidden');
 
   currentPage = `${newPage}`;
 }
 
 function changeButton1(newButton, removeButton1, removeButton2) {
   var showButton = document.querySelector(`.${newButton}`);
-  showButton.classList.add('view');
+  // showButton.classList.add('view');
   showButton.classList.remove('hidden');
 
   var hideButton1 = document.querySelector(`.${removeButton1}`);
@@ -78,11 +81,11 @@ function changeButton1(newButton, removeButton1, removeButton2) {
 
 function changeButton2(newButton1, newButton2, removeButton) {
   var showButton1 = document.querySelector(`.${newButton1}`);
-  showButton1.classList.add('view');
+  // showButton1.classList.add('view');
   showButton1.classList.remove('hidden');
 
   var showButton2 = document.querySelector(`.${newButton2}`);
-  showButton2.classList.add('view');
+  // showButton2.classList.add('view');
   showButton2.classList.remove('hidden');
 
   var hideButton = document.querySelector(`.${removeButton}`);
