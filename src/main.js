53afe6d516d042066ccbbe1f3be9a86fc006f8e0
Emily Corcoran <cathleencorcoran@gmail.com>
2021-10-20 19:58:55 -0400

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 // Create variables targetting the relevant DOM elements here 👇
 
+var covers = require('../src/data')
 
-// We've provided a few variables below
+var titles = require('../src/data')
+
+var desriptors = require('../src/data')
+
+
+var randomCoverButton = document.querySelector(".random-cover-button")
+
+
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
@@ -17,3 +25,11 @@ var currentCover;
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
+
+
+function generateRandomCoverButton() {
+  currentCover = new Cover(images[getRandomIndex])
+}
+
+
+randomCoverButton.addEventListener("click", generateRandomCoverButton);
