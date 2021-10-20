acaab276b9b81ea47ba5c3683d2207651dc667ff
Daniel O'Connell <danieloconnell.maker@gmail.com>
2021-10-19 18:56:14 -0700

    Finish creating Javascript button variables

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
 var homeButton = document.querySelector('.home-button');
 var randomCoverButton = document.querySelector('.random-cover-button');
 var saveCoverButton = document.querySelector('.save-cover-button');
+var viewSavedButton = document.querySelector('.view-saved-button');
+var makeNewButton = document.querySelector('.make-new-button')
 
 // We've provided a few variables below
 var savedCovers = [
