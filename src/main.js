cd770c23a48e34415e0991384f72073f4b367a72
Rana Jurjus <rjurjus11@mail.bw.edu>
2021-10-19 22:05:45 -0400

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,17 +1,26 @@
 // Create variables targetting the relevant DOM elements here 👇
-
+var coverImg = document.querySelector("cover-image");
+var coverTitle = document.querySelector("cover-title");
+//coverTitle.innerText = getRandomIndex(titles);
+var tagline1 = document.querySelector("tagline-1");
+//tagline1.innerText = getRandomIndex(descriptors);
+var tagline2 = document.querySelector("tagline-2");
+//tagline2.innerText = getRandomIndex(descriptors);
 
 // We've provided a few variables below
 var savedCovers = [
-  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
+  new Cover(
+    "http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg",
+    "Sunsets and Sorrows",
+    "sunsets",
+    "sorrows"
+  )
 ];
 var currentCover;
 
 // Add your event listeners here 👇
 
-
 // Create your event handlers and other functions here 👇
-
 
 // We've provided one function to get you started
 function getRandomIndex(array) {
