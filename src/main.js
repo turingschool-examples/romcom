786267b1c3ec6d1fdc2d8b83fb427ca57fbfaed5
Kai Kuller <kavakai@gmail.com>
2021-10-20 20:15:06 -0600

    Add variables relevent to DOM elements

--- src/main.js
+++ src/main.js
@@ -1,14 +1,22 @@
 // Create variables targetting the relevant DOM elements here 👇
+var coverImage = document.querySelector('.cover-image');
+// coverImage.src = './assets/ruffles.jpg'
+
+var coverTitle = document.querySelector('.cover-title');
+
+var tagline1 = document.querySelector('.tagline-1');
+
+var tagline2 = document.querySelector('.tagline-2');
+
 
 
 // We've provided a few variables below
 var savedCovers = [
-  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
+  new Cover("Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 var currentCover;
 
 // Add your event listeners here 👇
-
 
 // Create your event handlers and other functions here 👇
 
