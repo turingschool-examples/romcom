33e103113c87f7692deae06abf48fcde287ebba1
Johnathan Moore <johnathan.moore@thehoofed.com>
2021-10-21 18:35:03 -0500

    Complete Iteration 2

--- src/main.js
+++ src/main.js
@@ -5,20 +5,16 @@ var tagline1 = document.querySelector(".tagline-1");
 var tagline2 = document.querySelector(".tagline-2");
 var coverTitle = document.querySelector(".cover-title");
 var coverImage = document.querySelector(".cover-image");
-
 var randomButton = document.querySelector(".random-cover-button");
 var homeButton = document.querySelector(".home-button");
-var makeNewButton = document.querySelector(".make-new-button");
-var viewSavedButton = document.querySelector(".view-saved-cover");
-
+var makeYourOwnCoverButton = document.querySelector(".make-new-button");
+var viewSavedCoverButton = document.querySelector(".view-saved-cover");
 var homeView = document.querySelector(".home-view");
 var formView = document.querySelector(".form-view");
 var savedView = document.querySelector(".saved-view")
-
 var saveCoverButton = document.querySelector(".save-cover-button");
 var savedCoversSection = document.querySelector(".saved-covers-section");
 var viewSavedCoversButton = document.querySelector(".view-saved-button");
-
 var formMakeMyBookButton = document.querySelector(".create-new-book-button");
 var formUserCoverInput = document.querySelector(".user-cover");
 var formUserTitleInput = document.querySelector(".user-title");
@@ -28,27 +24,20 @@ var formUserDescriptor2Input = document.querySelector(".user-desc2");
 var savedCovers = [];
 var currentCover;
 
-
-// We've provided a few variables below
-var savedCovers = [
-  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
-];
-var currentCover;
-
 // Add your event listeners here 👇
 window.onload = randomCoverGenerator;
 
-var makeYourOwnCoverButton.addEventListener('click', makeACover);
-var homeButton.addEventListener('click', makeAHome);
-// var viewSavedCoversButton.addEventListener('click', makeSavedCoversButton);
-// var formMakeMyBookButton.addEventListener('click', makeCustomBook);
-// var saveCoverButton.addEventListener('click', saveHomeCover);
-// var savedCoversSection.addEventListener('dblclick', deleteCover);
-
-
-
-
 randomButton.addEventListener("click", randomCoverGenerator);
+makeYourOwnCoverButton.addEventListener("click", makeACover);
+homeButton.addEventListener("click", makeAHome);
+viewSavedCoversButton.addEventListener("click", showSavedCovers);
+formMakeMyBookButton.addEventListener("click", makeCustomBook);
+saveCoverButton.addEventListener("click", saveHomeCover);
+savedCoversSection.addEventListener("dblclick", deleteCover);
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
 
 // Create your event handlers and other functions here 👇
 function randomCoverGenerator() {
@@ -63,16 +52,49 @@ function randomCoverGenerator() {
   currentCover = new Cover(covers[coverIndex], titles[titleIndex], descriptors[tag1Index], descriptors[tag2Index]);
   }
 
-function makeAHome() {
-  homeView.classList.remove('hidden');
-  formView.classList.add('hidden');
-  savedView.classList.add('hidden');
-  homeButton.classList.add('hidden');
-  saveCoverButton.classList.remove('hidden');
-  randomButton.classList.remove('hidden');
+function saveHomeCover() {
+  if (isUniqueCover(currentCover) === true) {
+  saveCover(currentCover.cover, currentCover.title, currentCover.tagline1, currentCover.tagline2);
+  }
 }
 
-// We've provided one function to get you started
-function getRandomIndex(array) {
-  return Math.floor(Math.random() * array.length);
+
+function makeAHome() {
+  homeView.classList.remove("hidden");
+  formView.classList.add("hidden");
+  savedView.classList.add("hidden");
+  homeButton.classList.add("hidden");
+  saveCoverButton.classList.remove("hidden");
+  randomButton.classList.remove("hidden");
+}
+
+function makeACover() {
+  homeView.classList.add("hidden");
+  formView.classList.remove("hidden");
+  homeButton.classList.remove("hidden");
+  saveCoverButton.classList.add("hidden");
+  randomButton.classList.add("hidden");
+  savedView.classList.add("hidden");
+}
+
+function showSavedCovers() {
+  savedCoversSection.innerHTML = "";
+  homeView.classList.add("hidden");
+  formView.classList.add("hidden");
+  savedView.classList.remove("hidden");
+  savedCoversSection.classList.remove("hidden");
+  saveCoverButton.classList.add("hidden");
+  randomButton.classList.add("hidden");
+  homeButton.classList.remove("hidden");
+  displaySavedCovers();
+}
+
+function deleteCover(event) {
+  var bookToDelete = event.target.closest(".mini-cover");
+    for (var i = 0; i < savedCovers.length; i++) {
+      if (savedCovers[i].id == bookToDelete.dataset.id) {
+        savedCovers.splice(i, 1);
+      }
+    }
+    showSavedCovers();
 }
