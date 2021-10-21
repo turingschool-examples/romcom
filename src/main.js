d8d8cf9f5829ab9c3e1f482eb84474798764876f
Jacob Quintana <jacobquintaa@gmail.com>
2021-10-21 16:11:33 -0600

    Hide main page, and show form

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ var coverImage = document.querySelector(".cover-image");
 var coverTitle = document.querySelector(".cover-title");
 var coverTagline1 = document.querySelector(".tagline-1");
 var coverTagline2 = document.querySelector(".tagline-2");
-
+var mainPage = document.querySelector(".home-view");
+var formPage = document.querySelector(".form-view");
 //buttons
 var randomCoverButton = document.querySelector(".random-cover-button");
 var makeCoverButton = document.querySelector(".make-new-button");
+var saveCoverButton = document.querySelector(".save-cover-button");
+var homeButton = document.querySelector(".home-button");
 
 
 // We've provided a few variables below
@@ -16,9 +19,9 @@ var savedCovers = [
 var currentCover;
 
 // Add your event listeners here 👇
-window.addEventListener("load", randomCover);
-randomCoverButton.addEventListener("click", randomCover);
-makeCoverButton.addEventListener("click", )
+window.addEventListener("load", makeRandomCover);
+randomCoverButton.addEventListener("click", makeRandomCover);
+makeCoverButton.addEventListener("click", changeVisibility);
 
 // Create your event handlers and other functions here 👇
 
@@ -27,7 +30,7 @@ makeCoverButton.addEventListener("click", )
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
-function randomCover() {
+function makeRandomCover() {
   var randomImage = covers[getRandomIndex(covers)];
   var randomTitle = titles[getRandomIndex(titles)];
   var randomTaglineOne = descriptors[getRandomIndex(descriptors)];
@@ -39,5 +42,19 @@ function randomCover() {
   coverImage.src = currentCover.cover;
   coverTagline1.innerText = currentCover.tagline1;
   coverTagline2.innerText = currentCover.tagline2;
+}
 
+function changeVisibility(){
+  randomCoverButton.classList.add("hidden");
+  saveCoverButton.classList.add("hidden");
+  mainPage.classList.add("hidden");
+  homeButton.classList.remove("hidden")
+  formPage.classList.remove("hidden")
+
+  console.log('hello');
+
+  //we want to show the home buttons
+  // we want to hide the main page, and show the form.
+
+  //js: need to access
 }
