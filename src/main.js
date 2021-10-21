2efdbf1bae9e26a812b9c67cd1261c2eea7a7cd0
Devon Wigle <devonwigle@gmail.com>
2021-10-21 16:59:11 -0500

    Randomize cover iamge

--- src/main.js
+++ src/main.js
@@ -1,34 +1,26 @@
 // Create variables targetting the relevant DOM elements here 👇
 var coverImgSrc = document.querySelector('.cover-image')
-var title = document.querySelector('.cover-title')
-var tagLine = document.querySelector ('.tagline')
-var descriptor1 = document.querySelector('.tagline-1')
-var descriptor2 = document.querySelector('.tagline-2')
-class Cover {
-  constructor(coverImgSrc, title, descriptor1, descriptor2) {
-    this.id = Date.now();
-    this.cover = coverImgSrc;
-    this.title = title;
-    this.tagline1 = descriptor1;
-    this.tagline2 = descriptor2;
+// var title = document.querySelector('.cover-title')
+// var tagLine = document.querySelector ('.tagline')
+// var descriptor1 = document.querySelector('.tagline-1')
+// var descriptor2 = document.querySelector('.tagline-2')
 
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-var currentCover = document.querySelector('./cover-image');
+var currentCover =
 
-// Add your event listeners here
-👇
-window.randomCover('load',() => {
-  //Add content Here!
+// Add your event listeners here👇
+window.addEventListener('load', function() {
+  randomizer()
 })
 
 
 
 // Create your event handlers and other functions here 👇
-function randomizer(cover) {
-  picture = covers[getRandomIndex(covers)]
+function randomizer() {
+  coverImgSrc.src = covers[getRandomIndex(covers)]
 }
 
 // We've provided one function to get you started
