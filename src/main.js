fac26aa576bc7f93a471a0b14dc194e3afdc7639
Lynn Nguyen <AmandaN22@outlook.com>
2021-10-21 15:09:24 -0600

    Test commit

--- src/main.js
+++ src/main.js
@@ -96,6 +96,8 @@ var coverImage = document.querySelector(".cover-image");
 var coverTitle = document.querySelector(".cover-title");
 var tagline1 = document.querySelector(".tagline-1");
 var tagline2 = document.querySelector(".tagline-2");
+var makeCoverButton = document.querySelector(".make-new-button");
+
 
 
 // We've provided a few variables below
@@ -103,6 +105,9 @@ var tagline2 = document.querySelector(".tagline-2");
   //new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 //];
 //var randomCover = new Cover(covers[coversIndex], titles[titlesIndex], descriptors[descriptorsIndex], descriptors[descriptorsIndex]);
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * (array.length))
+}
 
 function createRandomCover() {
   coverImage.src = covers[getRandomIndex(covers)];
@@ -117,9 +122,12 @@ createRandomCover();
 randomButton.addEventListener("click", createRandomCover)
 
 
-
+//add comment to test commit
 //? will this randomizer function work if I insert the variable covers from data.js
-
+//need access the make cover button .querySelector
+//need to access the form
+//create event listener for the make cover button
+//when event listener is clicked, change to form
 
 
 
@@ -127,6 +135,3 @@ randomButton.addEventListener("click", createRandomCover)
 
 
 // We've provided one function to get you started
-  function getRandomIndex(array) {
-    return Math.floor(Math.random() * (array.length))
-}
