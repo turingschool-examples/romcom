667c16f2c3d5c9882329cb522e202201a09fb876
Ryan McBride <mr.ryan444@gmail.com>
2021-10-20 22:56:14 -0600

    Add makeMyBook function

--- src/main.js
+++ src/main.js
@@ -5,11 +5,18 @@ var descriptorOne = document.querySelector('.tagline-1');
 var descriptorTwo = document.querySelector('.tagline-2');
 var homeView = document.querySelector('.home-view');
 var form = document.querySelector('.form-view');
+var mainCover = document.querySelector('.main-cover');
 
 var coverButton = document.querySelector('.random-cover-button');
 var homeButton = document.querySelector('.home-button');
 var makeNewButton = document.querySelector('.make-new-button');
 var saveCoverButton = document.querySelector('.save-cover-button');
+var createNewBookButton = document.querySelector('.create-new-book-button');
+
+var coverInputValue = document.querySelector('.user-cover');
+var titleInputValue = document.querySelector('.user-title');
+var firstDescriptorInputValue = document.querySelector('.user-desc1');
+var secondDescriptorInputValue = document.querySelector('.user-desc2');
 
 // We've provided a few variables below
 var savedCovers = [
@@ -19,8 +26,8 @@ var currentCover = new Cover(coverImageSource, coverTitle, descriptorOne, descri
 
 // Add your event listeners here 👇
 coverButton.addEventListener('click', changeCover);
-
 makeNewButton.addEventListener('click', displayForm);
+createNewBookButton.addEventListener('click', makeMyBook);
 // Create your event handlers and other functions here 👇
 function changeCover() {
   currentCover.cover.src = covers[getRandomIndex(covers)];
@@ -52,6 +59,22 @@ function hideCoverButton() {
 function hideSaveCoverButton() {
   saveCoverButton.style.display = 'none';
 }
+
+function makeMyBook() {
+  displayForm();
+  covers.push(coverInputValue.value);
+  titles.push(titleInputValue.value);
+  descriptors.push(firstDescriptorInputValue.value);
+  descriptors.push(secondDescriptorInputValue.value);
+
+  currentCover.cover.src = covers[covers.length - 1];
+  currentCover.title.innerText = titles[titles.length - 1];
+  currentCover.tagline1.innerText = descriptors[descriptors.length - 2];
+  currentCover.tagline2.innerText = descriptors[descriptors.length -1];
+
+  mainCover.style.display = 'inherit';
+}
+
 // We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
