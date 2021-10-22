a6af9b1a3468433ffa9a09b65b0d7234c14611ac
Christine Rowland <fordo29@gmail.com>
2021-10-21 19:39:27 -0600

    Tweak event listener function

--- src/main.js
+++ src/main.js
@@ -12,11 +12,6 @@ var ranNum = getRandomIndex(titles);
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-var currentCover = [
-  new Cover(covers[ranNum1], titles[ranNum], descriptors[ranNum2], descriptors[ranNum3])
-];
-
-console.log(currentCover);
 
 var image = document.querySelector('img');
 image.src = covers[ranNum1];
@@ -30,15 +25,23 @@ descriptor1.innerText = descriptors[ranNum2];
 var descriptor2 = document.querySelector('.tagline-2');
 descriptor2.innerText = descriptors[ranNum3];
 
+
+
+var currentCover =
+new Cover (covers[ranNum1], titles[ranNum], descriptors[ranNum2], descriptors[ranNum3]);
+
 var button = document.querySelector('.random-cover-button')
 button.addEventListener('click', changeCover);
 
 function changeCover() {
-  image.src = currentCover.cover;
-  bookTitle.innerText = currentCover.title;
-  descriptor1.innerText = currentCover.tagline1;
-  descriptor2.innerText = currentCover.tagline2;
+image.src = currentCover.cover;
+bookTitle.innerText = currentCover.title;
+descriptor1.innerText = currentCover.tagline1;
+descriptor2.innerText = currentCover.tagline2;
 }
+
+console.log(descriptor2.innerText);
+
 //
 //
 //
@@ -72,7 +75,7 @@ function changeCover() {
 // var currentCover;
 //
 // // Add your event listeners here 👇
-window.addEventListener("click",  )
+// window.addEventListener("click",  )
 // //
 // // function whateverName () {
 // //
