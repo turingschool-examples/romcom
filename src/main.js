de643dbfff283b8d44269b8d4a647a51273d1ff5
Stevie Ambroise <s.ambroise@protonmail.com>
2021-10-22 17:27:25 -0600

    Add organized code

--- src/main.js
+++ src/main.js
@@ -9,21 +9,7 @@ function getRandomIndex(array) {
 var ranNum2 = getRandomIndex(descriptors);
 var ranNum1 = getRandomIndex(covers);
 var ranNum = getRandomIndex(titles);
-//
-//
 
-// var image = document.querySelector('img');
-// image.src = covers[ranNum1];
-//
-// var bookTitle = document.querySelector('h2');
-// bookTitle.innerText = titles[ranNum];
-//
-// var descriptor1 = document.querySelector('.tagline-1');
-// descriptor1.innerText = descriptors[ranNum2];
-//
-// var descriptor2 = document.querySelector('.tagline-2');
-// descriptor2.innerText = descriptors[ranNum1];
-//
 var button = document.querySelector('.random-cover-button');
 
 
@@ -38,9 +24,6 @@ function randomCover() {
   descriptor1.innerText = descriptors[ranNum2];
   var descriptor2 = document.querySelector('.tagline-2');
   descriptor2.innerText = descriptors[ranNum1];
-
-  //var button = document.querySelector('.random-cover-button');
-
 }
 
 function newCov () {
@@ -67,17 +50,7 @@ console.log(button);
 //   coverArray[i].innerText = coverArray[i][ranNum]
 // }
 // }*/
-//
-// //randomizeIt();
-// //We want to randomize the image.
-// //We have the html element in a variable that we can manipulate,
-// //We need the html for JS, how do we put the element into JS?
-// //How do we randomize the photos?
-// //How do we target A photo? What is syntax for targetting photo? How do we
-// //specifically the src part for the image link thing?
-// //How do we hardcode change the photo?
-// //How do we make it dynamic?
-//
+
 // //<img class="cover-image" src="./assets/prairie.jpg">
 //
 //We've provided a few variables below
@@ -85,7 +58,7 @@ var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 
-var currentCover;
+var currentCover
 
 // // Add your event listeners here 👇
 // // window.addEventListener("load",  )
