81a8b69ca34522d37ac485f0355a78a7d643c215
Christine Rowland <fordo29@gmail.com>
2021-10-21 23:02:36 -0600

    Activate button click

--- src/main.js
+++ src/main.js
@@ -1,46 +1,42 @@
-function getRandomIndex(array) {
-   return Math.floor(Math.random() * array.length);
-}
-var ranNum3 = getRandomIndex(descriptors);
-var ranNum2 = getRandomIndex(descriptors);
-var ranNum1 = getRandomIndex(covers);
-var ranNum = getRandomIndex(titles);
+
+var ranCover = covers[getRandomIndex(covers)];
+var ranTitles = titles[getRandomIndex(titles)];
+var ranDes1 = descriptors[getRandomIndex(descriptors)];
+var ranDes2 = descriptors[getRandomIndex(descriptors)];
 // 1. create object instance using our class cover
 // 2. use class cover with addEventListener (access button)
 
-
-var savedCovers = [
-  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
-];
-
-var image = document.querySelector('img');
-image.src = covers[ranNum1];
-
-var bookTitle = document.querySelector('h2');
-bookTitle.innerText = titles[ranNum];
-
-var descriptor1 = document.querySelector('.tagline-1');
-descriptor1.innerText = descriptors[ranNum2];
-
-var descriptor2 = document.querySelector('.tagline-2');
-descriptor2.innerText = descriptors[ranNum3];
-
-
-
-var currentCover =
-new Cover (covers[ranNum1], titles[ranNum], descriptors[ranNum2], descriptors[ranNum3]);
-
 var button = document.querySelector('.random-cover-button')
-button.addEventListener('click', changeCover);
+var image = document.querySelector('.cover-image');
+var bookTitle = document.querySelector('.cover-title');
+var descriptor1 = document.querySelector('.tagline-1');
+var descriptor2 = document.querySelector('.tagline-2');
 
-function changeCover() {
-image.src = currentCover.cover;
-bookTitle.innerText = currentCover.title;
-descriptor1.innerText = currentCover.tagline1;
-descriptor2.innerText = currentCover.tagline2;
-}
+window.addEventListener('load', pageLoad);
+//document.addEventListener('DOMContentLoaded', pageLoad);
+//document.onload = pageLoad();
+//window.onload = pageLoad();
+button.addEventListener('click', pageLoad);
 
-console.log(descriptor2.innerText);
+
+
+function pageLoad() {
+  image.src = covers[getRandomIndex(covers)];
+  bookTitle.innerText = titles[getRandomIndex(titles)];
+  descriptor1.innerText = descriptors[getRandomIndex(descriptors)];
+  descriptor2.innerText = descriptors[getRandomIndex(descriptors)];
+};
+
+
+// function pageLoad() {
+// currentCover = new Cover (ranCover, ranTitles, ranDes1, ranDes2);
+// image.src = currentCover.cover;
+// bookTitle.innerText = currentCover.title;
+// descriptor1.innerText = currentCover.tagline1;
+// descriptor2.innerText = currentCover.tagline2;
+// };
+
+console.log(pageLoad);
 
 //
 //
@@ -66,17 +62,17 @@ console.log(descriptor2.innerText);
 // //How do we hardcode change the photo?
 // //How do we make it dynamic?
 //
-// //<img class="cover-image" src="./assets/prairie.jpg">
 //
 // //We've provided a few variables below
-// var savedCovers = [
-//   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
-// ];
-// var currentCover;
+var savedCovers = [
+new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
+var currentCover;
 //
 // // Add your event listeners here 👇
-// window.addEventListener("click",  )
-// //
-// // function whateverName () {
-// //
-// // }
+
+
+
+
+function getRandomIndex(array) {
+   return Math.floor(Math.random() * array.length);
+}
