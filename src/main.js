d02bcacfcdff343368422b6a5b2ffbca28940bf2
Kayla Durrett <kayla.durrett@gmail.com>
2021-10-23 09:04:19 -0600

    Add save cover functionality

--- src/main.js
+++ src/main.js
@@ -15,6 +15,8 @@ var customImageInput = document.querySelector('.user-cover')
 var customTitleInput = document.querySelector('.user-title')
 var customDescriptor1 = document.querySelector('.user-desc1')
 var customDescriptor2 = document.querySelector('.user-desc2')
+var showSavedCoversView = document.querySelector('.saved-view')
+
 
 
 var savedCovers = [
@@ -26,67 +28,71 @@ var currentCover;
 function getRandomIndex(array) {
   var randomIndex = Math.floor(Math.random() * array.length);
   return array[randomIndex];
-}
+};
 
 
 
 function generateRandomCoverButton() {
   currentCover = new Cover(getRandomIndex(covers), getRandomIndex(titles), getRandomIndex(descriptors), getRandomIndex(descriptors));
-
-  coverImage.src = currentCover.cover
-  coverTitle.innerText = currentCover.title
-  tagline1.innerText = currentCover.tagline1
-  tagline2.innerText = currentCover.tagline2
-}
+  coverImage.src = currentCover.cover;
+  coverTitle.innerText = currentCover.title;
+  tagline1.innerText = currentCover.tagline1;
+  tagline2.innerText = currentCover.tagline2;
+};
 
 function showForm() {
-  mainCover.classList.add('hidden')
-  form.classList.remove('hidden')
-  randomCoverButton.classList.add('hidden')
-  saveCoverButton.classList.add('hidden')
-  homeButton.classList.remove('hidden')
+  mainCover.classList.add('hidden');
+  form.classList.remove('hidden');
+  randomCoverButton.classList.add('hidden');
+  saveCoverButton.classList.add('hidden');
+  homeButton.classList.remove('hidden');
 };
 
 function showSavedCovers() {
-  form.classList.add('hidden')
+  form.classList.add('hidden');
+  mainCover.classList.add('hidden');
+  showSavedCoversView.classList.remove('hidden');
+
 };
 
 function showHomePage() {
-  homeButton.classList.add('hidden')
-  randomCoverButton.classList.remove('hidden')
-  saveCoverButton.classList.remove('hidden')
-  mainCover.classList.remove('hidden')
-
+  homeButton.classList.add('hidden');
+  randomCoverButton.classList.remove('hidden');
+  saveCoverButton.classList.remove('hidden');
+  mainCover.classList.remove('hidden');
 };
 
 function createCustomBook(event) {
-event.preventDefault();
+  event.preventDefault();
+  currentCover = new Cover(customImageInput.value, customTitleInput.value, customDescriptor1.value, customDescriptor2.value);
+  covers.push(customImageInput.value);
+  titles.push(customTitleInput.value);
+  descriptors.push(customDescriptor1.value, customDescriptor2.value);
+  coverImage.src = currentCover.cover;
+  coverTitle.innerText = currentCover.title;
+  tagline1.innerText = currentCover.tagline1;
+  tagline2.innerText = currentCover.tagline2;
+  form.classList.add('hidden');
+  homeButton.classList.add('hidden');
+  randomCoverButton.classList.remove('hidden');
+  saveCoverButton.classList.remove('hidden');
+  mainCover.classList.remove('hidden');
+};
 
-currentCover = new Cover(customImageInput.value, customTitleInput.value, customDescriptor1.value, customDescriptor2.value)
+function saveCover() {
+  if (savedCovers.includes(currentCover)) {
+    return savedCovers;
+  } else {
+    savedCovers.push(currentCover);
+  }
+  console.log(savedCovers);
 
-covers.push(customImageInput.value);
-titles.push(customTitleInput.value);
-descriptors.push(customDescriptor1.value, customDescriptor2.value );
-
-coverImage.src = currentCover.cover
-coverTitle.innerText = currentCover.title
-tagline1.innerText = currentCover.tagline1
-tagline2.innerText = currentCover.tagline2
-
-
-form.classList.add('hidden')
-homeButton.classList.add('hidden')
-randomCoverButton.classList.remove('hidden')
-saveCoverButton.classList.remove('hidden')
-mainCover.classList.remove('hidden')
-
-}
-
-
+};
 
 randomCoverButton.addEventListener("click", generateRandomCoverButton);
 makeYourOwnCoverButton.addEventListener("click", showForm);
 viewSavedCoversButton.addEventListener("click", showSavedCovers);
 homeButton.addEventListener("click", showHomePage);
 generateCustomBookButton.addEventListener("click", createCustomBook);
-window.addEventListener("load", generateRandomCoverButton)
+window.addEventListener("load", generateRandomCoverButton);
+saveCoverButton.addEventListener("click", saveCover);
