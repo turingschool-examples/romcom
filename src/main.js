d167934b03d2db780e10bd204b81581c1df9a581
Ryan McBride <mr.ryan444@gmail.com>
2021-10-24 12:23:47 -0600

    Add annotations

--- src/main.js
+++ src/main.js
@@ -35,15 +35,15 @@ coverButton.addEventListener('click', changeCover);
 saveCoverButton.addEventListener('click', saveCover);
 makeNewButton.addEventListener('click', displayForm);
 viewSavedButton.addEventListener('click', displaySaved);
-createNewBookButton.addEventListener('click', getUserData);
 savedCoversSection.addEventListener('dblclick', removeCover);
+createNewBookButton.addEventListener('click', displayCustomCover);
 
 // Calculates a random index number
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
 
-// Randomly selects values from the respective arrays 
+// Randomly selects values from the respective arrays, 
 // applys them to a class and inserts them on the DOM
 function changeCover() {
   currentCover = new Cover(
@@ -75,7 +75,7 @@ function makeNewCover() {
   descriptorTwo.innerText = currentCover.tagline2;
 }
 
-// Displays 'form' page
+// Displays 'Form' view
 function displayForm() {
   form.classList.remove('hidden');
   displayHomeButton();
@@ -85,7 +85,7 @@ function displayForm() {
   hideSavedCoversPage();
 }
 
-// Displays 'saved covers' page
+// Displays 'Saved Covers' view
 function displaySaved() {
   savedView.classList.remove('hidden');
   displaySavedCovers();
@@ -96,7 +96,7 @@ function displaySaved() {
   hideSaveCoverButton();
 }
 
-// Display 'home' page
+// Display 'Home' view
 function displayHome() {
   homeView.classList.remove('hidden');
   hideHomeButton();
@@ -106,10 +106,9 @@ function displayHome() {
   hideSavedCoversPage();
 }
 
-// Applies the user's input to create a new cover
-function getUserData(event) {
+// Applies the user's input to create a new cover and displays it
+function displayCustomCover(event) {
   event.preventDefault();
-
   displayHome();
   storeCoverInputValue();
   storeTitleInputValue();
@@ -118,81 +117,96 @@ function getUserData(event) {
   makeNewCover();
 }
 
-// Funcitons that show/hide various elements
+// Displays the 'Home' button
 function displayHomeButton() {
   homeButton.classList.remove('hidden');
 }
 
+// Displays the 'Save Cover' button
 function displaySaveCoverButton() {
   saveCoverButton.classList.remove('hidden');
 }
 
+// Displays the 'Show New Random Cover' button
 function displayCoverButton() {
   coverButton.classList.remove('hidden');
 }
 
+// Hides the 'Form' view
 function hideForm() {
   form.classList.add('hidden');
 }
 
+// Hides the 'Home' button
 function hideHomeButton() {
   homeButton.classList.add('hidden');
 }
 
+// Hides the 'Home' view
 function hideHome() {
   homeView.classList.add('hidden');
 }
 
+// Hides the 'Show New Random Cover' button
 function hideCoverButton() {
   coverButton.classList.add('hidden');
 }
 
+// Hides the 'Save Cover' button
 function hideSaveCoverButton() {
   saveCoverButton.classList.add('hidden');
 }
 
+// Hides the 'Saved Covers' view
 function hideSavedCoversPage() {
   savedView.classList.add('hidden');
 }
 
-// Functions that collect user input and push the data into their arrays
+// Gets the value from the 'Cover' input field
+// and stores it in the covers array
 function storeCoverInputValue() {
   var coverValue = coverInputValue.value;
   covers.push(coverValue);
 }
 
+// Gets the value fom the 'Title' input field
+// and stores it in the titles array
 function storeTitleInputValue() {
   var titleValue = titleInputValue.value;
   titles.push(titleValue);
 }
 
+// Gets the value from the 'First descriptor' input
+// field and stores it in the descriptors array 
 function storeDescriptorOneInputValue() {
   var descriptorOneValue = firstDescriptorInputValue.value;
   descriptors.push(descriptorOneValue);
 }
 
+// Gets the value from the 'Second descriptor' input
+// field and stores it in the descriptors array
 function storeDescriptorTwoInputValue() {
   var descriptorTwoValue = secondDescriptorInputValue.value;
   descriptors.push(descriptorTwoValue);
 }
 
-// Does not add the cover to the array if one already exists
+// Does not add the cover to the 'savedCovers' array if one already exists (prevents duplicates)
 function saveCover() {
   if (!savedCovers.includes(currentCover)) {
     savedCovers.push(currentCover);
   }
 }
 
-// Creates mini sized covers and displays them on the 'saved covers' page
+// Creates mini sized covers and displays them on the 'Saved Covers' view
 function displaySavedCovers() {
   savedCoversSection.innerHTML = ``;
   for (var i = 0; i < savedCovers.length; i++) {
     savedCoversSection.innerHTML += 
     `<section class="mini-cover">
-      <img class="cover-image" id=${savedCovers[i].id} src="${savedCovers[i].cover}" alt="No Image Available">
-      <h2 class="cover-title">${savedCovers[i].title}</h2>
-      <h3 class="tagline">A tale of <span class="tagline-1">${savedCovers[i].tagline1}</span> and <span class="tagline-2">${savedCovers[i].tagline2}</span></h3>
-    </section>`
+       <img class="cover-image" id="${savedCovers[i].id}" src="${savedCovers[i].cover}" alt="No Image Available">
+       <h2 class="cover-title">"${savedCovers[i].title}"</h2>
+       <h3 class="tagline">A tale of <span class="tagline-1">"${savedCovers[i].tagline1}"</span> and <span class="tagline-2">"${savedCovers[i].tagline2}"</span></h3>
+     </section>`
   }
 }
 
