c11e13943940976872d4a18b6b8f0343fa23ef70
danielsolomon332 <daniel.solomon332@gmail.com>
2021-10-23 21:40:11 -0600

    Finish iteration 4 - saveCoverButton

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 var currentCoverImage = document.querySelector(".cover-image");
 var currentTitle = document.querySelector(".cover-title");
 var currentDescriptors = document.querySelector(".tagline");
+var currentDescriptor1 = document.querySelector(".tagline-1");
+var currentDescriptor2 = document.querySelector(".tagline-2");
 var showNewRandomCoverButton = document.querySelector(".random-cover-button");
 var makeYourOwnCoverButton = document.querySelector(".make-new-button");
 var saveCoverButton = document.querySelector(".save-cover-button");
@@ -10,17 +12,24 @@ var homeView = document.querySelector(".home-view")
 var formView = document.querySelector(".form-view")
 var savedView = document.querySelector(".saved-view")
 
+var tempBook;
+
 // ITERATION 0: This code randomizes book on home page load, currently turned off to troubleshoot iteration 3:
-currentCoverImage.src = covers[Math.floor(Math.random() * covers.length)];
-currentTitle.innerText = titles[Math.floor(Math.random() * titles.length)];
-currentDescriptors.innerText = "A tale of " + descriptors[Math.floor(Math.random() * descriptors.length)] + " and " + descriptors[Math.floor(Math.random() * descriptors.length)];
+
+window.addEventListener("load", changeCover);
 
 // ITERATION 1:
 showNewRandomCoverButton.addEventListener("click", changeCover);
 function changeCover() {
+
   currentCoverImage.src = covers[Math.floor(Math.random() * covers.length)];
   currentTitle.innerText = titles[Math.floor(Math.random() * titles.length)];
-  currentDescriptors.innerText = "A tale of " + descriptors[Math.floor(Math.random() * descriptors.length)] + " and " + descriptors[Math.floor(Math.random() * descriptors.length)];
+  word1 = descriptors[Math.floor(Math.random() * descriptors.length)]
+  word2 = descriptors[Math.floor(Math.random() * descriptors.length)]
+  currentDescriptors.innerText = "A tale of " + word1 + " and " + word2;
+
+  tempBook = new Cover(currentCoverImage.src, currentTitle.innerText, word1, word2)
+
 };
 
 //ITERATION 2:
@@ -66,50 +75,37 @@ var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 
-///***TROUBLESHOOTING SHORT VERSION Create book button on Form Page
 formMakeBookButton.addEventListener("click", createFormMakeBook);
 function createFormMakeBook() {
   event.preventDefault()
 
   covers.push(formNewCoverImage.value)
   titles.push(formNewTitleText.value)
-  descriptors.push(formNewFirstDescriptorText.value)
-  descriptors.push(formNewSecondDescriptorText.value)
+  descriptors.push(formNewFirstDescriptorText.value, formNewSecondDescriptorText.value)
 
-  var tempBook = new Cover(formNewCoverImage.value, formNewTitleText.value, formNewFirstDescriptorText.value, formNewSecondDescriptorText.value)
+  tempBook = new Cover(formNewCoverImage.value, formNewTitleText.value, formNewFirstDescriptorText.value, formNewSecondDescriptorText.value)
   savedCovers.push(tempBook)
 
   currentCoverImage.src = covers[covers.length - 1];
   currentTitle.innerText = titles[titles.length - 1];
-  currentDescriptors.innerText = "A tale of " + descriptors[descriptors.length - 2] + " and " + descriptors[descriptors.length - 1];
+  word1 = descriptors[descriptors.length - 2]
+  word2 = descriptors[descriptors.length - 1]
+  currentDescriptors.innerText = "A tale of " + word1 + " and " + word2;
 
   viewHomePage()
 
 }
 
 // ITERATION 4
+saveCoverButton.addEventListener("click", saveCover);
+function saveCover() {
+  savedCovers.push(tempBook)
+}
 
-  // var tempBook = new Cover(formNewCoverImage.value, formNewTitleText.value, formNewFirstDescriptorText.value, formNewSecondDescriptorText.value)
-  // savedCovers.push(tempBook)
-
-
-
-// ///***SAME AS ABOVE, LONGER VERSION WITH CLASS CREATION AND IF/THEN EMPTY VALUE CHECK
-// formCreateBookButton.addEventListener("click", createFormBook);
-// function createFormBook() {
 //   //if (!formNewCoverImage || !formNewTitleText || !formNewFirstDescriptorText || !formNewSecondDescriptorText) {
 //   //  window.alert("You need to fill out all the forms");
 //   //} else {
-//   ///***code to make the object, untested but should be close if not working)
-//   //new Cover(formNewCoverImage, formNewTitleText, formNewFirstDescriptorText, new2ndDescriptorText)
-//   ///***This should work, but on homepage load it has a default book. We need to find that code and make it an either or, specifically "if book form then bookform, if not then default"
-//   currentCoverImage.src = formNewCoverImage.value;
-//   currentTitle.innerText = formNewTitleText.value;
-//   currentDescriptors.innerText = "A tale of " + formNewFirstDescriptorText.value + " and " + formNewSecondDescriptorText.value;
-//   //}
 // }
-
-
 
 var currentCover;
 
