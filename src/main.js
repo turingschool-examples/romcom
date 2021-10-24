710acefe3488893cce1c1b8ef933f85222e39634
Eric Matlock <ermatlock@gmail.com>
2021-10-24 19:02:35 -0400

    Final touches and refactoring where possible

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-// Create variables targetting the relevant DOM elements here 👇
-
 var homeButton = document.querySelector(".home-button");
 var randomCoverButton = document.querySelector(".random-cover-button")
 var saveCoverButton = document.querySelector(".save-cover-button");
@@ -23,55 +21,20 @@ var currentTitle = document.querySelector(".cover-title");
 var currentTag1 = document.querySelector(".tagline-1");
 var currentTag2 = document.querySelector(".tagline-2");
 
-
-
-// We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-// var currentCover;
+var currentCover;
 
 
-// Add your event listeners here 👇
 window.addEventListener("load", createRandomCover);
-
 randomCoverButton.addEventListener("click", createRandomCover);
-
 makeNewButton.addEventListener("click", viewCoverForm);
-
 viewSavedButton.addEventListener("click", viewSavedPage);
-
 homeButton.addEventListener("click", viewHomePage);
-
 makeMyBook.addEventListener("click", pushMyBook);
-
 saveCoverButton.addEventListener("click", saveCover);
 
-removeSavedCover.addEventListener("dblclick", removeCover)
-
-// Create your event handlers and other functions here 👇
-function displayNewCover(coverValue, titleValue, descriptor1Value, descriptor2Value) {
-
-currentCover = new Cover(coverValue, titleValue, descriptor1Value, descriptor2Value);
-
-currentImage.src = currentCover.cover;
-currentTitle.innerText = currentCover.title;
-currentTag1.innerText = currentCover.tagline1;
-currentTag2.innerText = currentCover.tagline2;
-};
-
-function getRandomIndex(array) {
-  return Math.floor(Math.random() * array.length);
-}
-
-function createRandomCover() {
-  var newCover = covers[getRandomIndex(covers)];
-  var newTitle = titles[getRandomIndex(titles)];
-  var newTagline1 = descriptors[getRandomIndex(descriptors)];
-  var newTagline2 = descriptors[getRandomIndex(descriptors)];
-
-  displayNewCover(newCover, newTitle, newTagline1, newTagline2);
-}
 
 function viewHomePage() {
   randomCoverButton.classList.remove("hidden");
@@ -80,7 +43,7 @@ function viewHomePage() {
   homeView.classList.remove("hidden");
   viewForm.classList.add("hidden");
   savedCoversView.classList.add("hidden");
-}
+};
 
 function viewSavedPage() {
   randomCoverButton.classList.add("hidden");
@@ -91,7 +54,7 @@ function viewSavedPage() {
   savedCoversView.classList.remove("hidden");
 
   showSaved();
-}
+};
 
 function viewCoverForm() {
   randomCoverButton.classList.add("hidden");
@@ -100,28 +63,18 @@ function viewCoverForm() {
   homeView.classList.add("hidden");
   viewForm.classList.remove("hidden");
   savedCoversView.classList.add("hidden");
-}
-
-function pushMyBook(event) {
-  event.preventDefault();
-  var formCoverValue = formCover.value;
-  var formTitleValue = formTitle.value;
-  var formDescriptor1Value = formDescriptor1.value;
-  var formDescriptor2Value = formDescriptor2.value;
-
-  covers.push(formCoverValue);
-  titles.push(formTitleValue);
-  descriptors.push(formDescriptor1Value);
-  descriptors.push(formDescriptor2Value);
-
-  viewHomePage();
-
-  displayNewCover(formCoverValue, formTitleValue, formDescriptor1Value, formDescriptor2Value);
 };
 
+function displayNewCover(coverValue, titleValue, descriptor1Value, descriptor2Value) {
+  currentCover = new Cover(coverValue, titleValue, descriptor1Value, descriptor2Value);
+    currentImage.src = currentCover.cover;
+    currentTitle.innerText = currentCover.title;
+    currentTag1.innerText = currentCover.tagline1;
+    currentTag2.innerText = currentCover.tagline2;
+};
 
 function showSaved() {
-  savedCoverSection.innerHTML = ``
+  savedCoverSection.innerHTML = ``;
   for(var i = 0; i < savedCovers.length; i++) {
     savedCoverSection.innerHTML += `
       <section id="${savedCovers[i].id}" class="mini-cover">
@@ -138,6 +91,32 @@ function saveCover() {
   }
 };
 
-function removeCover() {
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+};
 
+function createRandomCover() {
+  var newCover = covers[getRandomIndex(covers)];
+  var newTitle = titles[getRandomIndex(titles)];
+  var newTagline1 = descriptors[getRandomIndex(descriptors)];
+  var newTagline2 = descriptors[getRandomIndex(descriptors)];
+
+  displayNewCover(newCover, newTitle, newTagline1, newTagline2);
+};
+
+function pushMyBook(event) {
+  event.preventDefault();
+  var formCoverValue = formCover.value;
+  var formTitleValue = formTitle.value;
+  var formDescriptor1Value = formDescriptor1.value;
+  var formDescriptor2Value = formDescriptor2.value;
+
+  covers.push(formCoverValue);
+  titles.push(formTitleValue);
+  descriptors.push(formDescriptor1Value);
+  descriptors.push(formDescriptor2Value);
+
+  viewHomePage();
+
+  displayNewCover(formCoverValue, formTitleValue, formDescriptor1Value, formDescriptor2Value);
 };
