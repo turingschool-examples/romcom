36524ac21d1f2e99f34fe432de277d16542c712b
Daniel O'Connell <danieloconnell.maker@gmail.com>
2021-11-28 00:42:59 -0800

    Replace displaySavedCovers with ES6 language

--- src/main.js
+++ src/main.js
@@ -112,16 +112,16 @@ function saveCurrentCover() {
 };
 
 function displaySavedCovers() {
-  savedCoversSection.innerHTML = ``;
-  for(var i = 0; i < savedCovers.length; i++) {
-  savedCoversSection.innerHTML += `
+	savedCoversSection.innerHTML = ``;
+	savedCovers.forEach((item, i) => {
+		savedCoversSection.innerHTML += `
     <section class="mini-cover">
       <img class="cover-image" id="${savedCovers[i].id}" src="${savedCovers[i].cover}">
       <h2 class="cover-title">${savedCovers[i].title}</h2>
       <h3 class="tagline">A tale of <span class="tagline-1">${savedCovers[i].tagline1}</span> and <span class="tagline-2">${savedCovers[i].tagline2}</span></h2>
     </section>`
-  };
-};
+	});
+}
 
 function deleteSavedCover() {
   for(var i = 0; i < savedCovers.length; i++) {
