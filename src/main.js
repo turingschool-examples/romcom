7636b19be2b5580e2e647283771f2e160a098c29
Zachary Saile <zwsaile@gmail.com>
2022-02-08 20:55:53 -0700

    Generate random cover upon load

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
 // Create variables targetting the relevant DOM elements here 👇
+var randomCover = document.querySelector(".random-cover-button");
+var saveCover = document.querySelector(".save-cover-button");
+var viewSaved = document.querySelector(".view-saved-button");
+var makeNewCover = document.querySelector(".make-new-button");
+
+var coverImage = document.querySelector(".cover-image")
+var coverTitle = document.querySelector(".cover-title")
+var firstDescriptor = document.querySelector(".tagline-1")
+var secondDescriptor = document.querySelector(".tagline-2")
 
 
 // We've provided a few variables below
-var savedCovers = [
-  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
-];
-var currentCover;
+var savedRomCom = [];
+var currentRomCom;
 
 // Add your event listeners here 👇
 
@@ -16,4 +23,22 @@ var currentCover;
 // We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
-}
+};
+
+function storeCover() {
+  currentRomCom = new Cover(coverImage.src, coverTitle.innerText, firstDescriptor.innerText, secondDescriptor.innerText)
+};
+
+function randomRomCom() {
+  coverImage.src = covers[getRandomIndex(covers)];
+  coverTitle.innerText = titles[getRandomIndex(titles)];
+  firstDescriptor.innerText = descriptors[getRandomIndex(descriptors)];
+  secondDescriptor.innerText = descriptors[getRandomIndex(descriptors)];
+//  if (firstDescriptor === secondDescriptor) {
+//    return secondDescriptor.innerText = getRandomIndex(descriptors);
+//  };
+  storeCover();
+
+};
+
+randomRomCom();
