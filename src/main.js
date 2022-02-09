73433d730f6bf477e48dd734b601f06ed12f977c
Stephen Tedesco <s_tylert@icloud.com>
2022-02-09 16:39:33 -0700

    Finish itteration 0

--- src/main.js
+++ src/main.js
@@ -1,19 +1,33 @@
+
+
 // Create variables targetting the relevant DOM elements here 👇
-
-
+var movieImg = document.querySelector(".cover-image");
+var randomCoverBtn = document.querySelector(".random-cover-button");
+var movieTitle = document.querySelector(".cover-title");
+var movieDescription1 = document.querySelector(".tagline-1")
+var movieDescription2 = document.querySelector(".tagline-2");
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-var currentCover;
 
 // Add your event listeners here 👇
-
+window.addEventListener("load", generateCoverImg)
+randomCoverBtn.addEventListener("click", generateCoverImg);
 
 // Create your event handlers and other functions here 👇
 
+function generateCoverImg(){
+  movieImg.src = covers[getRandomIndex(covers)]
+  movieTitle.innerText = titles[getRandomIndex(titles)]
+  movieDescription1.innerText = descriptors[getRandomIndex(descriptors)]
+  movieDescription2.innerText = descriptors[getRandomIndex(descriptors)]
+};
 
-// We've provided one function to get you started
+//movieImg.src = newCover;
+
+//currentCover = new Cover(newImg);
+
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
