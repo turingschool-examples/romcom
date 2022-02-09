7276dd8e903015ae42e4ab7c5be6a30a9e994ca2
Artan Myrtolli <artanmyrtolli@gmail.com>
2022-02-09 13:21:05 -0500

    Randomized All elements and refactor

--- src/main.js
+++ src/main.js
@@ -1,33 +1,23 @@
+
 // Create variables targetting the relevant DOM elements here 👇
+document.querySelector('.cover-title').innerText = titles[getRandomIndex(titles)];
+document.querySelector('.tagline-1').innerText = descriptors[getRandomIndex(descriptors)];
+document.querySelector('.tagline-2').innerText = descriptors[getRandomIndex(descriptors)];
+document.querySelector ('.cover-image').src = covers[getRandomIndex(covers)];
+
 
 
 // We've provided a few variables below
-// var savedCovers = [
-//   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
-// ];
-// var currentCover;
+var savedCovers = [
+  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
+];
+var currentCover;
 
-var savedTitle = ["Addicted","Michael's Mic in The Wind", "Beyond the Highland Mist", "Collide","Galactic Space Lover","Kathrine","It Wasn't You it Was Me", "Caress of the Armpit","The Mighty Storm","My Favorite Mistake","On the Island","Pleasure Unbound","Promises","Neil Eating Pineapple Pizza", "Twisted Perfection","Wallbanger","The Winter Sea","Texas Destiny"]
-var savedTagLine = ["Mistakes","Alcohol","Drought","Passion","Musk","Humidity","Madness","Secrets","Lies","Tension","Romance","Intentions","Promises","Perils","Pleasure","Midnight","Summer","Desperation"]
+console.log(titles);
 
 
-function getRandomTitle() {
-var currentTitleIndex = getRandomIndex(savedTitle)
-var currentTitle = savedTitle[currentTitleIndex]
-return currentTitle
-}
-console.log(getRandomTitle())
 
-function getRandomTagLine() {
 
-var currentTagLine1 = savedTagLine[getRandomIndex(savedTagLine)]
-var currentTagLine2 = savedTagLine[getRandomIndex(savedTagLine)]
-if (currentTagLine1 === currentTagLine2) {
-  currentTagLine1 = 'Unquenchable Thirst'
-  } return `A tale of ${currentTagLine1} and ${currentTagLine2}`
-}
-
-console.log(getRandomTagLine())
 // Add your event listeners here 👇
 
 
