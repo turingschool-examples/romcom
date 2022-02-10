90ec170b19ebb3c206276857493bb26a7aa08e0c
Kyle Howard <kylehoward1228@yahoo.com>
2022-02-10 15:18:55 -0700

    start make your own button

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 // Create variables targetting the relevant DOM elements here 👇
 
-
-// We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
@@ -10,24 +8,30 @@ var coverImage = document.querySelector(".cover-image");
 var coverTitle = document.querySelector(".cover-title");
 var tagLine1 = document.querySelector(".tagline-1");
 var tagLine2 = document.querySelector(".tagline-2");
-
-
+var randomCover = document.querySelector(".random-cover-button")
+var makeYourOwn = document.querySelector(".make-new-button")
 // Add your event listeners here 👇
 
-
+randomCover.addEventListener('click', showNewRandomCover)
+makeYourOwn.addEventListener('click', )
 // Create your event handlers and other functions here 👇
 
-
-// We've provided one function to get you started
-function getRandomIndex(array) {
-  return Math.floor(Math.random() * array.length);
+function showForm() {
+  
 }
-
 function showNewRandomCover() {
 coverTitle.innerText = titles[getRandomIndex(titles)]
 coverImage.src = covers[getRandomIndex(covers)]
 tagLine1.innerText = descriptors[getRandomIndex(descriptors)]
 tagLine2.innerText = descriptors[getRandomIndex(descriptors)]
-}
 
+ newRandomCover()
+}
+ function newRandomCover() {
+   currentCover = new Cover(coverImage.src, coverTitle.innerText, tagLine1.innerText, tagLine2.innerText)
+ }
 showNewRandomCover();
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
