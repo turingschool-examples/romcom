541ff53d47e1d5b3895e9c26170ae7396818119c
Joe Rupp <joerpp8@gmail.com>
2022-02-10 22:42:59 -0700

    Add functionalitt for user to populate book cover

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
-// Create variables targetting the relevant DOM elements here 👇
+// VARIABLES  __________________________________
+
+// Global Variables
+var currentCover;
+
+var savedCovers = [
+  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
+];
 
 // Cover section
 var title = document.querySelector('h2');
@@ -8,18 +15,20 @@ var image = document.querySelector('.cover-image');
 
 // Buttons
 var makeRandomCoverButton = document.querySelector('.random-cover-button');
-var makeOwnCoverButton = document.querySelector('.make-new-button')
-var saveCoverButton = document.querySelector('.save-cover-button')
-var viewSavedCoverButton = document.querySelector('.view-saved-button')
-var homeButton = document.querySelector('.home-button')
+var makeOwnCoverButton = document.querySelector('.make-new-button');
+var saveCoverButton = document.querySelector('.save-cover-button');
+var viewSavedCoverButton = document.querySelector('.view-saved-button');
+var homeButton = document.querySelector('.home-button');
+var makeMyBookButton = document.querySelector('.create-new-book-button');
 
-var currentCover;
+// Input Fields
+var userCover = document.querySelector('.user-cover');
+var userTitle = document.querySelector('.user-title');
+var userDesc1 = document.querySelector('.user-desc1');
+var userDesc2 = document.querySelector('.user-desc2');
 
-var savedCovers = [
-  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
-];
 
-// Add your event listeners here 👇
+// EVENT LISTENERS __________________________________
 
 window.addEventListener('load', newCover);
 
@@ -48,6 +57,37 @@ viewSavedCoverButton.addEventListener('click', function(){
 });
 
 homeButton.addEventListener('click', function(){
+  homeButtonSwitch()
+});
+
+makeMyBookButton.addEventListener('click', function(){
+  // save submitted data to the respective arrays
+  saveToArray();
+  // input values creates new instance of cover class
+  myCover();
+  // Change back to main home view
+  event.preventDefault();
+  // Display the newly created cover
+  homeButtonSwitch();
+});
+
+saveCoverButton.addEventListener('click', function(){});
+
+// FUNCTIONS __________________________________
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+};
+
+function newCover() {
+  title.innerText = titles[getRandomIndex(titles)];
+  tagline1.innerText = descriptors[getRandomIndex(descriptors)];
+  tagline2.innerText = descriptors[getRandomIndex(descriptors)];
+  image.src = covers[getRandomIndex(covers)];
+  currentCover = new Cover(image.src, title.innerText, tagline1.innerText, tagline2.innerText);
+};
+
+function homeButtonSwitch() {
   document.querySelector('.home-view').classList.remove('hidden');
   document.querySelector('.form-view').classList.add('hidden');
   document.querySelector('.saved-view').classList.add('hidden');
@@ -56,20 +96,19 @@ homeButton.addEventListener('click', function(){
   document.querySelector('.home-button').classList.add('hidden');
   document.querySelector('.view-saved-button').classList.remove('hidden');
   document.querySelector('.make-new-button').classList.remove('hidden');
-});
+};
 
-saveCoverButton.addEventListener('click');
-
-// Create your event handlers and other functions here 👇
-
-function getRandomIndex(array) {
-  return Math.floor(Math.random() * array.length);
-}
-
-function newCover() {
-  title.innerText = titles[getRandomIndex(titles)];
-  tagline1.innerText = descriptors[getRandomIndex(descriptors)];
-  tagline2.innerText = descriptors[getRandomIndex(descriptors)];
-  image.src = covers[getRandomIndex(covers)];
+function myCover() {
+  title.innerText = userTitle.value;
+  tagline1.innerText = userDesc1.value;
+  tagline2.innerText = userDesc2.value;
+  image.src = userCover.value;
   currentCover = new Cover(image.src, title.innerText, tagline1.innerText, tagline2.innerText);
-}
+};
+
+function saveToArray() {
+  titles.unshift(userTitle.value);
+  descriptors.unshift(userDesc1.value);
+  descriptors.unshift(userDesc2.value);
+  covers.unshift(userCover.value);
+};
