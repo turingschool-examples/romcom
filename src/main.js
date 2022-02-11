b5ec8f3f592f1e72c308c222515dedc43dcea79e
Kendall McGree <mcgreekendall@gmail.com>
2022-02-10 18:06:20 -0800

    Complete Iteration 1

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-var currentCover;
+var currentCover = Cover
 
 // Add your event listeners here 👇
 
@@ -18,24 +18,37 @@ var currentCover;
 //   return Math.floor(Math.random() * array.length);
 // }
 
-class RomCom {
-  constructor() {
+// class RomCom {
+//   constructor() {
+//
+//   }
+// }
 
-  }
-}
 
+
+var randomCoverButton = document.querySelector(".random-cover-button");
 var currentTitle = document.querySelector('.cover-title');
-currentTitle.innerText = getRandomBookInfo(titles);
-
 var currentImageSrc = document.querySelector('.cover-image').src = getRandomBookInfo(covers);
-currentImageSrc.innerText = getRandomBookInfo(covers);
-
 var currentTagLine1 = document.querySelector('.tagline-1');
 var currentTagLine2 = document.querySelector('.tagline-2');
+
+
+randomCoverButton.addEventListener("click", changeButton);
+function changeButton() {
+currentTitle.innerText = getRandomBookInfo(titles);
+//currentImageSrc.innerText = getRandomBookInfo(covers);
+currentImageSrc = document.querySelector('.cover-image').src = getRandomBookInfo(covers);
+
 currentTagLine1.innerText = getRandomBookInfo(descriptors);
 currentTagLine2.innerText = getRandomBookInfo(descriptors);
+}
 
-
+currentTitle.innerText = getRandomBookInfo(titles);
+currentImageSrc = document.querySelector('.cover-image').src = getRandomBookInfo(covers);
+//why doesnt line 39/49 work like 50
+//currentImageSrc.innerText = getRandomBookInfo(covers);
+currentTagLine1.innerText = getRandomBookInfo(descriptors);
+currentTagLine2.innerText = getRandomBookInfo(descriptors);
 
 function getRandomBookInfo(bookInfo) {
   return bookInfo[Math.floor(Math.random() * bookInfo.length)];
