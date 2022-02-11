16f4b1414ff148b8279a93e532bdbc49194ec6a8
Zachary Saile <zwsaile@gmail.com>
2022-02-10 20:09:39 -0700

    Refactor randomRomCom and currentRomCom

--- src/main.js
+++ src/main.js
@@ -1,37 +1,42 @@
 // Create variables targetting the relevant DOM elements here 👇
-var randomCover = document.querySelector(".random-cover-button");
-var saveCover = document.querySelector(".save-cover-button");
-var viewSaved = document.querySelector(".view-saved-button");
-var makeNewCover = document.querySelector(".make-new-button");
-var homeButton = document.querySelector(".home-button");
+var randomCoverButton = document.querySelector('.random-cover-button');
+var saveCoverButton = document.querySelector('.save-cover-button');
+var viewSavedButton = document.querySelector('.view-saved-button');
+var makeCoverButton = document.querySelector('.make-new-button');
+var homeButton = document.querySelector('.home-button');
+var makeBookButton = document.querySelector('.create-new-book-button');
 
-var coverImage = document.querySelector(".cover-image")
-var coverTitle = document.querySelector(".cover-title")
-var firstDescriptor = document.querySelector(".tagline-1")
-var secondDescriptor = document.querySelector(".tagline-2")
+var coverImage = document.querySelector('.cover-image');
+var coverTitle = document.querySelector('.cover-title');
+var firstDescriptor = document.querySelector('.tagline-1');
+var secondDescriptor = document.querySelector('.tagline-2');
 
-var homeView = document.querySelector(".home-view");
-var formView = document.querySelector(".form-view");
-var savedView = document.querySelector(".saved-view");
+var homeView = document.querySelector('.home-view');
+var formView = document.querySelector('.form-view');
+var savedView = document.querySelector('.saved-view');
 
+var coverInput = document.querySelector('#cover');
+var titleInput = document.querySelector('#title');
+var firstDescriptorInput = document.querySelector('#descriptor1');
+var secondDescriptorInput = document.querySelector('#descriptor2');
 
 
 // We've provided a few variables below
 var savedRomCom = [];
-var currentRomCom = new Cover(coverImage.src, coverTitle.innerText, firstDescriptor.innerText, secondDescriptor.innerText)
+var currentRomCom = new Cover(covers[getRandomIndex(covers)], titles[getRandomIndex(titles)], descriptors[getRandomIndex(descriptors)], descriptors[getRandomIndex(descriptors)]);
 
 // Add your event listeners here 👇
 
-randomCover.addEventListener('click', randomRomCom);
-
 window.addEventListener('load', randomRomCom);
 
-makeNewCover.addEventListener('click', switchFormView);
+makeCoverButton.addEventListener('click', switchFormView);
 
 homeButton.addEventListener('click', returnHome);
 
-viewSaved.addEventListener('click', switchSavedView);
+viewSavedButton.addEventListener('click', switchSavedView);
 
+makeBookButton.addEventListener('click', submitNewBook)
+//https://media.istockphoto.com/photos/book-cover-for-a-vampire-novel-three-attractive-vampires-picture-id685834128?k=20&m=685834128&s=612x612&w=0&h=zmBYDVHfG7l2FpCy5PH5J25lLTFuPCQSHqe-LW5yZlk=
 // Create your event handlers and other functions here 👇
 
 
@@ -43,35 +48,38 @@ function getRandomIndex(array) {
 function switchFormView() {
   homeView.className = 'view home-view hidden';
   formView.className = 'view form-view';
-  randomCover.className = 'random-cover-button hidden';
-  saveCover.className = 'save-cover-button hidden';
+  randomCoverButton.className = 'random-cover-button hidden';
+  saveCoverButton.className = 'save-cover-button hidden';
   homeButton.className = 'home-button';
-}
+};
 
 function switchSavedView() {
   homeView.className = 'view home-view hidden';
   savedView.className = 'view saved-view';
   formView.className = 'view form-view hidden';
-  randomCover.className = 'random-cover-button hidden';
-  saveCover.className = 'save-cover-button hidden';
+  randomCoverButton.className = 'random-cover-button hidden';
+  saveCoverButton.className = 'save-cover-button hidden';
   homeButton.className = 'home-button';
-}
+};
 
 function returnHome() {
   homeView.className = 'view home-view';
   formView.className = 'view form-view hidden';
-  randomCover.className = 'random-cover-button';
-  saveCover.className = 'save-cover-button';
+  randomCoverButton.className = 'random-cover-button';
+  saveCoverButton.className = 'save-cover-button';
   homeButton.className = 'home-button hidden';
-}
+};
+
+function submitNewBook() {
+  covers.shift(currentRomCom.cover);
+  titles.shift(currentRomCom.title);
+  descriptors.shift(currentRomCom.tagline1);
+  descriptors.shift(currentRomCom.tagline2);
+};
 
 function randomRomCom() {
-  coverImage.src = covers[getRandomIndex(covers)];
-  coverTitle.innerText = titles[getRandomIndex(titles)];
-  firstDescriptor.innerText = descriptors[getRandomIndex(descriptors)];
-  secondDescriptor.innerText = descriptors[getRandomIndex(descriptors)];
-//  if (firstDescriptor === secondDescriptor) {
-//    return secondDescriptor.innerText = getRandomIndex(descriptors);
-//  };
-  // storeCover();
+coverImage.src = currentRomCom.cover;
+coverTitle.innerText = currentRomCom.title;
+firstDescriptor.innerText = currentRomCom.tagline1;
+secondDescriptor.innerText = currentRomCom.tagline2;
 };
