80dc4b585b25a7e61bba422c8b4d1778dc4ed58e
Angele Williams <angele.williams5@gmail.com>
2022-02-11 14:52:28 -0700

    Add Home button functionality

--- src/main.js
+++ src/main.js
@@ -4,14 +4,17 @@ var coverImage = document.querySelector(".cover-image");
 var coverTitle = document.querySelector(".cover-title");
 var tagline1 = document.querySelector(".tagline-1");
 var tagline2 = document.querySelector(".tagline-2");
-var randomCoverButton = document.querySelector(".random-cover-button");
-var diyCover = document.querySelector(".make-new-button");
-var viewSaved = document.querySelector(".view-saved-button");
-//var homeView = document.querySelector(".home-view", ".form-view");
-//var homeButton = document.querySelector(".home-button");
-//var formView = document.querySelector(".form-view");
-//var showRandCover = document.querySelector(".random-cover-button");
-//var saveCover = document.querySelector(".save-cover-button");
+
+var savedView = document.querySelector(".saved-view");
+var homeView = document.querySelector(".home-view");
+var formView = document.querySelector(".form-view");
+
+var viewSavedB = document.querySelector(".view-saved-button");
+var homeB = document.querySelector(".home-button");
+var randomCoverB = document.querySelector(".random-cover-button");
+var makeNewB = document.querySelector(".make-new-button");
+var saveCoverB = document.querySelector(".save-cover-button");
+
 var currentCover = new Cover();
 
 //savedCovers will be used in  ITERATION 2
@@ -27,26 +30,37 @@ var currentCover = new Cover();
 
 // Add your event listeners here 👇
 window.onload = makeRandomBook();
-randomCoverButton.addEventListener("click", makeRandomBook);
+randomCoverB.addEventListener("click", makeRandomBook);
 // diyCover.addEventListener("click", function () {
 //   document.querySelector(".view home-view").hidden = true;
 //   document.querySelector(".view form-view hidden").hidden = false;
 // });
-diyCover.addEventListener("click", function () {
-  document.querySelector(".form-view").classList.remove("hidden"));
-  document.querySelector(".home-button").classList.remove("hidden"));
-  hideElement(document.querySelector(".home-view"));
-  hideElement(document.querySelector(".random-cover-button"));
-  hideElement(document.querySelector(".save-cover-button"));
+makeNewB.addEventListener("click", function () {
+  viewElement(formView);
+  viewElement(homeB);
+  hideElement(homeView);
+  hideElement(randomCoverB);
+  hideElement(saveCoverB);
 });
 //..........TRY AN ARRAY? for dryer code since we're repeating ourselves..?
-viewSaved.addEventListener("click", function () {
-  viewElement(document.querySelector(".saved-view"));
-  viewElement(document.querySelector(".home-button"));
-  hideElement(document.querySelector(".home-view"));
-  hideElement(document.querySelector(".random-cover-button"));
-  hideElement(document.querySelector(".save-cover-button"));
+viewSavedB.addEventListener("click", function () {
+  viewElement(savedView);
+  viewElement(homeB);
+  hideElement(homeView);
+  hideElement(randomCoverB);
+  hideElement(saveCoverB);
+  hideElement(formView)
 });
+
+homeB.addEventListener("click", function () {
+  viewElement(homeView);
+  viewElement(saveCoverB);
+  viewElement(randomCoverB);
+  hideElement(homeB);
+  hideElement(formView);
+  hideElement(savedView);
+});
+
 // Create your event handlers and other functions here 👇
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
