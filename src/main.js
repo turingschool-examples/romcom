4dfa9d546d51a6233a4d708d91f3502521f3a0ce
Jeffrey Cook <cookturingedu@gmail.com>
2022-05-19 17:28:51 -0400

    Show random covers on the main page

--- src/main.js
+++ src/main.js
@@ -1,18 +1,46 @@
 // Create variables targetting the relevant DOM elements here 👇
+var coverImage = document.querySelector('.cover-image');
+var title = document.querySelector('.cover-title');
+var tagline1 = document.querySelector('.tagline-1');
+var tagline2 = document.querySelector('tagline-2');
 
+var randomCoverButton = document.querySelector('.random-cover-button');
+var saveButton = document.querySelector('.save-cover-button');
+var viewButton = document.querySelector('.view-saved-button');
+var createButton = document.querySelector('.make-new-button');
+var homeButton = document.querySelector('.home-button');
+var newBookButton = document.querySelector('.create-new-book-button');
+
+var homeView = document.querySelector('.home-view');
+var formView = document.querySelector('.form-view');
 
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-var currentCover;
+
+var currentCover = new Cover();
 
 // Add your event listeners here 👇
-
+randomCoverButton.addEventListener('click', displayRandomCover);
+createButton.addEventListener('click', viewForm);
 
 // Create your event handlers and other functions here 👇
+function displayRandomCover() {
+  var coverImage = covers[getRandomIndex(covers)];
+  var title = titles[getRandomIndex(titles)];
+  var tagline1 = descriptors[getRandomIndex(descriptors)];
+  var tagline2 = descriptors[getRandomIndex(descriptors)];
+  currentCover = new Cover(coverImage, title, tagline1, tagline2);
+  showCurrentCover();
+}
 
-
+function showCurrentCover() {
+  coverImage.src = currentCover.cover;
+  title.innerText = currentCover.title;
+  tagline1.innerText = currentCover.tagline1;
+  tagline2.innerText = currentCover.tagline2;
+}
 // We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
