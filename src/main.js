8f245564aa7b0c6258e8b620cd2aaebd9f840af5
Maya Kappen <mayakappen@gmail.com>
2022-05-21 12:18:36 -0600

    add form variables

--- src/main.js
+++ src/main.js
@@ -23,6 +23,24 @@ var viewHomeView = document.querySelector('.home-view');
 
 var viewSavedView = document.querySelector('.saved-view');
 
+var userCover = document.querySelector('.user-cover');
+
+var userTitle = document.querySelector('.user-title');
+
+var userDesc1 = document.querySelector('.user-desc1');
+
+var userDesc2 = document.querySelector('.user-desc2');
+
+var formCover = document.querySelector('#cover');
+
+var formTitle = document.querySelector('#title');
+
+var formDescriptor1 = document.querySelector('#descriptor1')
+
+var formDescriptor2 = document.querySelector('#descriptor2')
+
+var createNewBookButton = document.querySelector('.create-new-book-button');
+
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
@@ -65,20 +83,25 @@ viewFormView.classList.add('hidden');
 }
 
 function viewHome() {
-  viewHomeView.classList.remove('hidden');
-  viewSavedView.classList.add('hidden');
-  viewFormView.classList.add('hidden');
-
-
+viewHomeView.classList.remove('hidden');
+viewSavedView.classList.add('hidden');
+viewFormView.classList.add('hidden');
+homeButton.classList.add('hidden');
+getRandomButton.classList.remove('hidden');
+saveCoverButton.classList.remove('hidden');
 }
 
 
 function getRandomCover() {
-  currentCover = new Cover(imageArray, coverTitle, taglineArrayOne, taglineArrayTwo);
-  imageArray.src = covers[getRandomIndex(covers)];
-  coverTitle.innerText = titles[getRandomIndex(titles)];
-  taglineArrayOne.innerText = descriptors[getRandomIndex(descriptors)];
-  taglineArrayTwo.innerText = descriptors[getRandomIndex(descriptors)];
+currentCover = new Cover(imageArray, coverTitle, taglineArrayOne, taglineArrayTwo);
+imageArray.src = covers[getRandomIndex(covers)];
+coverTitle.innerText = titles[getRandomIndex(titles)];
+taglineArrayOne.innerText = descriptors[getRandomIndex(descriptors)];
+taglineArrayTwo.innerText = descriptors[getRandomIndex(descriptors)];
+}
+
+function makeOwnCover() {
+
 }
 
 getRandomCover()
