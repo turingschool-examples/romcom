77924e33ec340f4277900281cab5644152efe2c0
Ali <anix1@kent.edu>
2022-05-21 17:35:46 -0600

    Add functionality to save covers section

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ var randomTag2 = document.querySelector(".tagline-2");
 var formView = document.querySelector(".form-view");
 var mainCover = document.querySelector(".home-view");
 var viewSavedCovers = document.querySelector(".saved-view");
+var saveCoverSection = document.querySelector(".saved-covers-section");
 
 var homeButton = document.querySelector(".home-button");
 var savedCoversButton = document.querySelector(".view-saved-button");
@@ -16,29 +17,31 @@ var makeNewCoverButton = document.querySelector(".make-new-button");
 var saveCoverButton = document.querySelector(".save-cover-button");
 var createBookButton = document.querySelector(".create-new-book-button");
 
-var inputCover = document.querySelector("#cover").value;
-var inputTitle = document.querySelector("#title").value;
-var input1descriptor = document.querySelector("#descriptor1").value;
-var input2descriptor = document.querySelector("#descriptor2").value;
+var inputCover = document.querySelector("#cover");
+var inputTitle = document.querySelector("#title");
+var input1descriptor = document.querySelector("#descriptor1");
+var input2descriptor = document.querySelector("#descriptor2");
 
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
+
 var currentCover;
 // Add your event listeners here 👇
 randomButton.addEventListener('click', randomCovers);
 
 makeNewCoverButton.addEventListener('click', displayForm);
 
-savedCoversButton.addEventListener('click', displaySavedCovers);
+savedCoversButton.addEventListener('click', showSaveCovers);
 
 homeButton.addEventListener('click', displayHome);
 
 createBookButton.addEventListener('click', makeCover);
+
+saveCoverButton.addEventListener('click', saveCover);
 // Create your event handlers and other functions here 👇
 
-randomCovers()
 function randomCovers() {
   randomCover.src = covers[getRandomIndex(covers)];
   randomTag1.innerText = descriptors[getRandomIndex(descriptors)];
@@ -57,8 +60,8 @@ function displayForm() {
   mainCover.classList.add('hidden');
   randomButton.classList.add('hidden');
   saveCoverButton.classList.add('hidden');
-};
 
+};
 
 function displaySavedCovers() {
   savedCoversButton.classList.remove('hidden');
@@ -68,6 +71,7 @@ function displaySavedCovers() {
   randomButton.classList.add('hidden');
   saveCoverButton.classList.add('hidden');
   formView.classList.add('hidden');
+
 };
 
 function displayHome() {
@@ -80,28 +84,31 @@ function displayHome() {
 
 };
 
-function makeCover () {
+function makeCover() {
   event.preventDefault();
   getUserInput();
+
 };
-  function getUserInput() {
-    var inputCover = document.querySelector("#cover").value;
-    var inputTitle = document.querySelector("#title").value;
-    var input1descriptor = document.querySelector("#descriptor1").value;
-    var input2descriptor = document.querySelector("#descriptor2").value;
-    covers.push(inputCover);
-    titles.push(inputTitle);
-    descriptors.push(input1descriptor, input2descriptor);
-    displayCover(inputCover, inputTitle, input1descriptor, input2descriptor)
+
+function getUserInput() {
+  var inputCover = document.querySelector("#cover").value;
+  var inputTitle = document.querySelector("#title").value;
+  var input1descriptor = document.querySelector("#descriptor1").value;
+  var input2descriptor = document.querySelector("#descriptor2").value;
+  covers.push(inputCover);
+  titles.push(inputTitle);
+  descriptors.push(input1descriptor, input2descriptor);
+  displayCover(inputCover, inputTitle, input1descriptor, input2descriptor)
+
 };
 
 function displayCover(inputCover, inputTitle, input1descriptor, input2descriptor) {
   currentCover = new Cover(inputCover, inputTitle, input1descriptor, input2descriptor);
   displayHome();
-    randomCover.src = currentCover.cover;
-    randomTitle.innerText = currentCover.title;
-    randomTag1.innerText =  currentCover.tagline1;
-    randomTag2.innerText = currentCover.tagline2;
+  randomCover.src = currentCover.cover;
+  randomTitle.innerText = currentCover.title;
+  randomTag1.innerText = currentCover.tagline1;
+  randomTag2.innerText = currentCover.tagline2;
 
 };
 
@@ -109,4 +116,25 @@ function displayCover(inputCover, inputTitle, input1descriptor, input2descriptor
 
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
+};
+
+function saveCover() {
+  if (!savedCovers.includes(currentCover)) {
+    savedCovers.push(currentCover);
+  }
+};
+
+function showSaveCovers() {
+  displaySavedCovers();
+  var smallCover = '';
+  for (var i = 0; i < savedCovers.length; i++) {
+    smallCover = smallCover + `<section class="main-cover">
+        <img class="cover-image" src="${savedCovers[i].cover}">
+        <h2 class="cover-title">${savedCovers[i].title}</h2>
+        <h3 class="tagline">A tale of <span class="tagline-1">${savedCovers[i].tagline1}</span> and <span class="tagline-2">${savedCovers[i].tagline2}</span></h3>
+        <img class="price-tag" src="./assets/price.png">
+        <img class="overlay" src="./assets/overlay.png">
+      </section>`
+  }
+  saveCoverSection.innerHTML = smallCover;
 };
