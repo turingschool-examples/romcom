0a2d4ea3cc5cdeaed2b9ea4133b0bd33993cb781
Anthony Shellman <atshellman@gmail.com>
2022-05-21 12:18:29 -0600

    Add new variables for user input/output

--- src/main.js
+++ src/main.js
@@ -1,20 +1,31 @@
 // Create variables targetting the relevant DOM elements here 👇
-var coverImage = document.querySelector(".cover-image")
-var coverTitle = document.querySelector(".cover-title")
-var taglineNum1 = document.querySelector(".tagline-1")
-var taglineNum2 = document.querySelector(".tagline-2")
+var coverImage = document.querySelector(".cover-image");
+var coverTitle = document.querySelector(".cover-title");
+var taglineNum1 = document.querySelector(".tagline-1");
+var taglineNum2 = document.querySelector(".tagline-2");
 
-var randomButton = document.querySelector(".random-cover-button")
-var homeButton = document.querySelector(".home-button")
-var saveButton = document.querySelector(".save-cover-button")
-var viewSavedbutton = document.querySelector(".view-saved-button")
-var makeNewButton = document.querySelector(".make-new-button")
+var randomButton = document.querySelector(".random-cover-button");
+var homeButton = document.querySelector(".home-button");
+var saveButton = document.querySelector(".save-cover-button");
+var viewSavedbutton = document.querySelector(".view-saved-button");
+var makeNewButton = document.querySelector(".make-new-button");
 
-var homeView = document.querySelector(".home-view")
-var savedView = document.querySelector(".saved-view")
-var formView = document.querySelector(".form-view")
+var homeView = document.querySelector(".home-view");
+var savedView = document.querySelector(".saved-view");
+var formView = document.querySelector(".form-view");
 
-//add new variable for each button on the main page
+var userCoverInput = document.querySelector("#cover");
+var userTitleInput = document.querySelector("#title");
+var userDesc1Input =document.querySelector("#descriptor1");
+var userDesc2Input = document.querySelector("#descriptor2");
+
+var userCoverOutput = document.querySelector(".user-cover");
+var userTitleOutput = document.querySelector(".user-title");
+var userDesc1Output =document.querySelector(".user-desc1");
+var userDesc2Output = document.querySelector(".user-desc2");
+
+var createBookButton = document.querySelector(".create-new-book-button");
+// add new variable for each button on the main page
 
 //document is everything, everything on the page is part of doc
 //this one is just focused on the cover image
@@ -96,6 +107,14 @@ function getRandomCover() {
     formView.classList.add('hidden');
     homeView.classList.remove('hidden')
   }
+
+  // function makeOwnCover() {
+
+// }
+
+  function
+
+
 //assign that html element next
 
 
