1019ff6bc0aef5935af08ed7d8b6720ad4ae2f8b
Ivy Nguyen <animeweirdo24@gmail.com>
2022-05-21 21:48:56 -0600

    refactored naming conventions and delete pseudocoded code to clean the main.js page

--- src/main.js
+++ src/main.js
@@ -6,30 +6,23 @@ var viewSavedButton = document.querySelector('.view-saved-button');
 var makeCoverButton = document.querySelector('.make-new-button');
 var makeBookButton = document.querySelector('.create-new-book-button');
 
-var titleCover = document.querySelector('.cover-title');
-var titleImage = document.querySelector('.cover-image');
-var titleTagLine1 = document.querySelector('.tagline-1');
-var titleTagLine2 = document.querySelector('.tagline-2');
+var coverTitle = document.querySelector('.cover-title');
+var imageCover = document.querySelector('.cover-image');
+var tagline1 = document.querySelector('.tagline-1');
+var tagline2 = document.querySelector('.tagline-2');
 
-var romControls = document.querySelector('.controls');
-var romViewHome = document.querySelector('.view.home-view');
-var romViewSave = document.querySelector('.view.saved-view');
-var romViewForm = document.querySelector('.view.form-view');
+// var romControls = document.querySelector('.controls');
+var homeViewSection = document.querySelector('.view.home-view');
+var viewSavedSection = document.querySelector('.view.saved-view');
+var viewForm = document.querySelector('.view.form-view');
 
-var savedCoversSect = document.querySelector('.saved-covers-section')
+var savedCoversSection = document.querySelector('.saved-covers-section')
 
 //getElementById's 🫣
 var coverInput = document.getElementById('cover');
 var titleInput = document.getElementById('title');
 var descriptor1Input = document.getElementById('descriptor1');
 var descriptor2Input = document.getElementById('descriptor2');
-
-//add currentCover to array
-// var html = `<img class="cover-image" src=${savedCovers[i].cover}>
-//   <h2 class="cover-title">${savedCovers[i].title}</h2>
-//   <h3 class="tagline">A tale of <span class="tagline-1">${savedCovers[i].tagline1}</span> and <span class="tagline-2">${savedCovers[i].tagline2}</span></h3>
-//   <img class="price-tag" src="./assets/price.png">
-//   <img class="overlay" src="./assets/overlay.png">`
 
 // We've provided a few variables below
 var savedCovers = [
@@ -41,12 +34,11 @@ var currentCover;
 window.addEventListener('load', getRandomizedCover)
 showRandomCoverButton.addEventListener("click", getRandomizedCover)
 makeCoverButton.addEventListener('click', makeCoverPage)
-viewSavedButton.addEventListener('click', savedCoversSection)
+viewSavedButton.addEventListener('click', savedCoversSectionFunction)
 homeButton.addEventListener('click', homeButtonFunction)
 makeBookButton.addEventListener('click', createNewCover)
 saveCoverButton.addEventListener('click', saveCovers)
-savedCoversSect.addEventListener('dblclick', deleteCovers)
-// Create your event handlers and other functions here 👇
+savedCoversSection.addEventListener('dblclick', deleteCovers)
 
 
 // We've provided one function to get you started
@@ -55,49 +47,43 @@ savedCoversSect.addEventListener('dblclick', deleteCovers)
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
-//og
-// function getRandomizedCover() {
-//   currentCover = new Cover(covers[getRandomIndex(covers)], titles[getRandomIndex(titles)], descriptors[getRandomIndex(descriptors)], descriptors[getRandomIndex(descriptors)])
-//   titleImage.src = currentCover.cover;
-//   titleCover.innerText = currentCover.title;
-//   titleTagLine1.innerText = currentCover.tagline1;
-//   titleTagLine2.innerText = currentCover.tagline2;
-// }
+
+// Create your event handlers and other functions here 👇
 
 //REFACTORED
 function getRandomizedCover(coverToDisplay) {
-  titleImage.src = coverToDisplay.cover || covers[getRandomIndex(covers)];
-  titleCover.innerText = coverToDisplay.title || titles[getRandomIndex(titles)];
-  titleTagLine1.innerText = coverToDisplay.tagline1 || descriptors[getRandomIndex(descriptors)];
-  titleTagLine2.innerText = coverToDisplay.tagline2 || descriptors[getRandomIndex(descriptors)];
-  currentCover = new Cover(titleImage.src, titleCover.innerText, titleTagLine1.innerText, titleTagLine2.innerText)
+  imageCover.src = coverToDisplay.cover || covers[getRandomIndex(covers)];
+  coverTitle.innerText = coverToDisplay.title || titles[getRandomIndex(titles)];
+  tagline1.innerText = coverToDisplay.tagline1 || descriptors[getRandomIndex(descriptors)];
+  tagline2.innerText = coverToDisplay.tagline2 || descriptors[getRandomIndex(descriptors)];
+  currentCover = new Cover(imageCover.src, coverTitle.innerText, tagline1.innerText, tagline2.innerText)
 }
 
 //iteration1
 function makeCoverPage() {
-  romViewForm.classList.remove('hidden');
-  romViewHome.classList.add('hidden');
-  romViewSave.classList.add('hidden');
+  viewForm.classList.remove('hidden');
+  homeViewSection.classList.add('hidden');
+  viewSavedSection.classList.add('hidden');
   showRandomCoverButton.classList.add('hidden');
   saveCoverButton.classList.add('hidden');
   homeButton.classList.remove('hidden');
 }
 
 function savedCoversView() {
-  romViewForm.classList.add('hidden');
-  romViewHome.classList.add('hidden');
-  romViewSave.classList.remove('hidden');
+  viewForm.classList.add('hidden');
+  homeViewSection.classList.add('hidden');
+  viewSavedSection.classList.remove('hidden');
   showRandomCoverButton.classList.add('hidden');
   saveCoverButton.classList.add('hidden');
   homeButton.classList.remove('hidden');
   viewSavedButton.classList.add('hidden');
-  savedCoversSect.classList.remove('hidden')
+  savedCoversSection.classList.remove('hidden')
 }
 
 function homeButtonFunction() {
-  romViewForm.classList.add('hidden');
-  romViewHome.classList.remove('hidden');
-  romViewSave.classList.add('hidden');
+  viewForm.classList.add('hidden');
+  homeViewSection.classList.remove('hidden');
+  viewSavedSection.classList.add('hidden');
   showRandomCoverButton.classList.remove('hidden');
   saveCoverButton.classList.remove('hidden');
   homeButton.classList.add('hidden');
@@ -106,57 +92,43 @@ function homeButtonFunction() {
 
 //iteration2
 
-//OG
-// function createNewCover(event) {
-//   event.preventDefault()
-//   currentCover = new Cover(coverInput.value, titleInput.value, descriptor1Input.value, descriptor2Input.value)
-//   covers.push(coverInput.value);
-//   titles.push(titleInput.value);
-//   descriptors.push(descriptor1Input.value);
-//   descriptors.push(descriptor2Input.value)
-//   console.log(currentCover)
-//   romViewForm.classList.add('hidden');
-//   romViewHome.classList.remove('hidden')
-//   saveCoverButton.classList.remove('hidden');
-//   viewSavedButton.classList.remove('hidden');
-//   getCover()
-// }
-
 //refactored
 function createNewCover(event) {
   event.preventDefault()
   currentCover = new Cover(coverInput.value, titleInput.value, descriptor1Input.value, descriptor2Input.value)
+
   covers.push(coverInput.value);
   titles.push(titleInput.value);
   descriptors.push(descriptor1Input.value);
   descriptors.push(descriptor2Input.value)
-  console.log(currentCover)
-  romViewForm.classList.add('hidden');
-  romViewHome.classList.remove('hidden')
+
+  viewForm.classList.add('hidden');
+  homeViewSection.classList.remove('hidden')
   saveCoverButton.classList.remove('hidden');
+
   viewSavedButton.classList.remove('hidden');
   getRandomizedCover(currentCover)
 }
 
 function getCover() {
-  titleImage.src = currentCover.cover;
-  titleCover.innerText = currentCover.title;
-  titleTagLine1.innerText = currentCover.tagline1;
-  titleTagLine2.innerText = currentCover.tagline2;
+  imageCover.src = currentCover.cover;
+  coverTitle.innerText = currentCover.title;
+  tagline1.innerText = currentCover.tagline1;
+  tagline2.innerText = currentCover.tagline2;
 }
 
 //iteration 3
-function savedCoversSection() {
-  savedCoversSect.innerHTML = ""
+function savedCoversSectionFunction() {
+  savedCoversSection.innerHTML = ""
+
   for (var i = 0; i < savedCovers.length; i++) {
-    console.log('savedcoverssect', savedCovers)
-    savedCoversSect.innerHTML += `<section class="mini-cover"> <img class="cover-image" id='${savedCovers[i].id}' src=${savedCovers[i].cover}>
+    savedCoversSection.innerHTML += `<section class="mini-cover"> <img class="cover-image" id='${savedCovers[i].id}' src=${savedCovers[i].cover}>
       <h2 class="cover-title">${savedCovers[i].title}</h2>
       <h3 class="tagline">A tale of <span class="tagline-1">${savedCovers[i].tagline1}</span> and <span class="tagline-2">${savedCovers[i].tagline2}</span></h3>`
 
-      romViewHome.classList.add('hidden')
-      romViewForm.classList.add('hidden')
-      romViewSave.classList.remove('hidden')
+      homeViewSection.classList.add('hidden')
+      viewForm.classList.add('hidden')
+      viewSavedSection.classList.remove('hidden')
       saveCoverButton.classList.add('hidden')
       showRandomCoverButton.classList.add('hidden')
       homeButton.classList.remove('hidden')
@@ -176,8 +148,7 @@ function deleteCovers(event) {
   for (var i = 0; i < savedCovers.length; i++) {
     if (savedCovers[i].id == event.target.id) {
       savedCovers.splice(i, 1)
-      console.log(savedCovers)
-      savedCoversSection()
+      savedCoversSectionFunction()
     }
   }
 }
