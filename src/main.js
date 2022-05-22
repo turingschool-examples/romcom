07c5d08c10db881531f84a95b22bc06ce7a3d404
Shauna MacFarlane <shauna.macfarlane579@gmail.com>
2022-05-21 22:55:40 -0500

    complete iteration 4 and refactor code

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-// Create variables targetting the relevant DOM elements here
 // 👇🏼 Cover Book image components 👇🏼
 var coverImg = document.querySelector('.cover-image')
 var coverTitle = document.querySelector('.cover-title')
@@ -7,10 +6,8 @@ var coverTagline2 = document.querySelector('.tagline-2')
 
 // 👇🏼 Screen views 👇🏼
 var homeView = document.querySelector(".home-view")
-var viewForm = document.querySelector(".form-view")
+var form = document.querySelector(".form-view")
 var savedCoversSection = document.querySelector(".saved-view")
-//possibly repititious var?
-//var viewSavedCoversSection = document.querySelector('.saved-covers-section')
 
 // 👇🏼 Buttons 👇🏼
 var homeButton = document.querySelector(".home-button")
@@ -19,8 +16,6 @@ var saveCoverButton = document.querySelector('.save-cover-button')
 var viewSavedCoversButton = document.querySelector('.view-saved-button')
 var makeYourOwnCoverButton = document.querySelector('.make-new-button')
 var makeMyBookButton = document.querySelector('.create-new-book-button')
-//possibly repititious var?
-//var createNewBookButton = document.querySelector('.create-new-book-button')
 
 // 👇🏼 Form input fields 👇🏼
 var formCover = document.querySelector(".user-cover")
@@ -35,20 +30,17 @@ var savedCovers = [
 var currentCover;
 
 // 👇🏼 Add Event listeners here 👇🏼
-// you'll have to have an onload evenlistener to load to your random functions
 window.addEventListener("load", getRandomCover);
 homeButton.addEventListener("click", goHome);
 showRandomCoverButton.addEventListener("click", getRandomCover);
-saveCoverButton.addEventListener('click', saveCoversButton)
+saveCoverButton.addEventListener('click', saveCoversButton);
 viewSavedCoversButton.addEventListener("click", seeSavedCovers);
 makeYourOwnCoverButton.addEventListener("click", displayForm);
 makeMyBookButton.addEventListener("click", makeMyBook);
-//savedCoversSection.addEventListener('dblclick', performDelete);
-//viewSavedCoversSection.addEventListener('load', savedCovers)
+savedCoversSection.addEventListener('dblclick', performDelete);
 
 
 // 👇🏼 Event handlers and functions 👇🏼
-// Random index is needed for data points of creating random book cover
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
@@ -73,29 +65,21 @@ function getRandomCover(){
 // 👇🏼 Displays form when "Make Your Own Cover" button is clicked
   function displayForm() {
      homeView.classList.add('hidden');
-     viewForm.classList.remove('hidden');
+     form.classList.remove('hidden');
      showRandomCoverButton.classList.add('hidden');
      saveCoverButton.classList.add('hidden');
      homeButton.classList.remove('hidden');
- }
-
-// 👇🏼 Displays saved covers section when "View Saved Covers" button is clicked
- function seeSavedCovers() {
-   savedCoversSection.classList.remove('hidden');
-   homeView.classList.add('hidden');
-   showRandomCoverButton.classList.add('hidden');
-   saveCoverButton.classList.add('hidden');
-   homeButton.classList.remove('hidden');
-   viewForm.classList.add('hidden');
-   viewSavedCovers();
+     savedCoversSection.classList.add('hidden')
  }
 
 // 👇🏼 Takes user home when "Home" button is clicked
  function goHome() {
-   homeView.classList.remove('hidden')
-   homeButton.classList.add('hidden')
-   showRandomCoverButton.classList.remove('hidden')
-   saveCoverButton.classList.remove('hidden')
+   homeView.classList.remove('hidden');
+   homeButton.classList.add('hidden');
+   showRandomCoverButton.classList.remove('hidden');
+   saveCoverButton.classList.remove('hidden');
+   form.classList.add('hidden');
+   savedCoversSection.classList.add('hidden');
  }
 
 // 👇🏼 Creates custom book cover when "Make My Book" button is clicked
@@ -119,15 +103,16 @@ function getRandomCover(){
    clearFields();
   }
 
-// 👇🏼 Takes user back to homepage to view custom cover
-// used in makeMyBook function
+// 👇🏼 Takes user to view custom cover used in makeMyBook function
 function displayCustomCover() {
-  goHome();
-  viewForm.classList.add('hidden');
+  homeView.classList.remove('hidden');
+  form.classList.add('hidden');
+  showRandomCoverButton.classList.remove('hidden');
+  saveCoverButton.classList.remove('hidden');
+  homeButton.classList.add('hidden');
 }
 
-// 👇🏼 Clears the input fields in "Make Your Own Cover" form
-// used in makeMyBook function
+// 👇🏼 Clears the input fields in "Make Your Own Cover" form used in makeMyBook function
 function clearFields() {
   userCover = "";
   userTitle = "";
@@ -135,6 +120,18 @@ function clearFields() {
   userDesc2 = "";
 }
 
+// 👇🏼 Displays saved covers section when "View Saved Covers" button is clicked
+ function seeSavedCovers() {
+   savedCoversSection.classList.remove('hidden');
+   homeView.classList.add('hidden');
+   showRandomCoverButton.classList.add('hidden');
+   saveCoverButton.classList.add('hidden');
+   homeButton.classList.remove('hidden');
+   form.classList.add('hidden');
+   viewSavedCovers();
+ }
+
+// 👇🏼 Saves the current cover with "Save Cover" button is clicked
 function saveCoversButton() {
   if(!savedCovers.includes(currentCover)) {
     savedCovers.push(currentCover);
@@ -144,69 +141,29 @@ function saveCoversButton() {
   }
 }
 
+// 👇🏼 Displays saved covers section when "View Saved Covers" button is clicked
 function viewSavedCovers() {
-  innerHTML = '';
+  var htmladd = '';
   for(var i = 0; i < savedCovers.length; i ++) {
-    innerHTML += '<section id="' + savedCovers[i].id + '" class="saved-covers-section">';
-    innerHTML += '<img class="cover-image dblClickTarget" src="' + savedCovers[i].cover + '">';
-    innerHTML += '<h2 class="saved-cover-title">' + savedCovers[i].title + '</h2>';
-    innerHTML += '<h3 class="saved-tagline">A tale of <span class="tagline-1">' + savedCovers[i].tagline1 + '</span> and <span class="tagline-2">' + savedCovers[i].tagline2 + '</span></h3>';
-    innerHTML += 'img class="price-tag" src="./assets/price.png">';
-    innerHTML += 'img class="overlay" src="./assets/overlay.png">';
-    innerHTML += '</section>';
-    }
-  savedCoversSection.innerHTML = innerHTML;
+    htmladd += `<section class="mini-cover">
+      <img class="cover-image" id=${savedCovers[i].id} src=${savedCovers[i].cover}>
+      <h2 class="cover-title">${savedCovers[i].title}</h2>
+      <h3 class="tagline">A tale of <span class="tagline-1">${savedCovers[i].tagline1}</span> and <span class="tagline-2">${savedCovers[i].tagline2}</span></h3>
+      <img class="price-tag" src="./assets/price.png">
+      <img class="overlay" src="./assets/overlay.png">
+      </section>`;
+  }
+  savedCoversSection.innerHTML = htmladd;
 }
 
-
-// //double click function
-// // 1. select the elements
-// // 2. add dblclick handlers
-// // 3. create the function to handle the event
-// // 4. perform delete
-//
-// // var bookElement = document.querySelector('.saved-covers-section');
-// // bookElement.addEventListener('dblclick', performDelete(e));
-// //
-// // function performDelete(e) {
-// //   alert(e.target.parentElement.id);
-// //   //for {
-// //     if (savedCovers[i].id = e.target.parentElement.id) {
-// //       savedCovers.splice(i);
-// //     }
-// //   }
-//
-// //looped through array, built dynamic HTML, assigning dynamic HTML to parent section.
-// //parent section is 'view saved-view hidden'
-// //bound the dbl click event to the parent element when the program starts
-// //performDelete is the double click handler
-// // create for loop - if savedCovers[i].id = e.target.parentElement.id) {
-// //savedCovers.splice(i);
-// //break;
-// //then call viewSavedCovers function(); to refresh page without the one removed
-//
-//
-//
-//
-// 1. save the id to a variables
-// 2. loop thorugh savedCovers array to find the idex of the targeted array elements
-// 3. remove the element from the arrays
-// 4. call view saved posters to repaint the page with revised data.
-
-// card.addEventListener('dblclick', function (e) {
-//   card.classList.toggle('large');
-// });
-
-// code taken from getRandomCover function - created new function from this code.
-// coverImg.src = newCover;
-// coverTitle.innerText = newTitle;
-// coverTagline1.innerText = newDescriptor1;
-// coverTagline2.innerText = newDescriptor2;
-
-//Redundant function created and used at beginning of functions
-//  function displayData(cover, title, descriptor1, descriptor2) {
-//    coverImg.src = cover;
-//    coverTitle.innerText = title;
-//    coverTagline1.innerText = descriptor1;
-//    coverTagline2.innerText = descriptor2;
-// }
+// 👇🏼 Deletes book cover from Saved Covers when cover is double clicked
+function performDelete(event) {
+  var deleteCover = document.getElementById(event.target.id)
+  deleteCover.parentElement.remove();
+  for (var i = 0; i < savedCovers.length; i ++) {
+    if (savedCovers[i].id == event.target.id) {
+      savedCovers.splice(i, 1);
+    }
+  }
+  viewSavedCovers();
+}
