74942200c8d5d776647071e3f0fdd60c545c4621
Colby Pearce <crpearce892@gmail.com>
2022-05-22 10:07:33 -0600

    start refactor process

--- src/main.js
+++ src/main.js
@@ -1,26 +1,22 @@
 // Create variables targetting the relevant DOM elements here 👇
+// Below are all query selectors
 var mainCover = document.querySelector('.main-cover')
-
 var randomButton = document.querySelector('.random-cover-button')
 var saveCoverButton = document.querySelector('.save-cover-button')
 var savedButton = document.querySelector('.view-saved-button')
 var makeNewButton = document.querySelector('.make-new-button')
 var homeButton = document.querySelector('.home-button')
 var createNewButton = document.querySelector('.create-new-book-button')
-
 var formView = document.querySelector('.form-view')
 var savedView = document.querySelector('.saved-view')
-
 var coverTitle = document.querySelector('.cover-title')
 var coverImage = document.querySelector('.cover-image')
 var taglineOne = document.querySelector('.tagline-1')
 var taglineTwo = document.querySelector('.tagline-2')
-
 var userCoverInput = document.querySelector('.user-cover')
 var userTitleInput = document.querySelector('.user-title')
 var userDescriptorOneInput = document.querySelector('.user-desc1')
 var userDescriptorTwoInput = document.querySelector('.user-desc2')
-
 var userCover = document.querySelector('.user-cover')
 var userTitle = document.querySelector('.user-title')
 var userDescriptorOne = document.querySelector('.user-desc1')
@@ -42,52 +38,55 @@ homeButton.addEventListener('click', goHome);
 createNewButton.addEventListener('click', makeBook);
 saveCoverButton.addEventListener('click', savedCover);
 
-
-// We've provided one function to get you started
-
+// We've provided one function to get you started Cover.js
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
-
+// This will create a new instance of the Cover class referenced in
 var currentCover = new Cover()
-
+//This function is tied to the function getRandonIndex, and will generate new
+//romcom covers by clicking the appropriate button
 function getRandomCoverPageLoad(event) {
   var cover = covers[getRandomIndex(covers)]
   var title = titles[getRandomIndex(titles)]
   var descriptor1 = descriptors[getRandomIndex(descriptors)]
   var descriptor2 = descriptors[getRandomIndex(descriptors)]
+//This will create a new instance of the Cover class with four randomly
+// generated properties.
   currentCover = new Cover(cover, title, descriptor1, descriptor2)
   coverImage.src = currentCover.cover
   coverTitle.innerText = currentCover.title
   taglineOne.innerText = currentCover.tagline1
   taglineTwo.innerText = currentCover.tagline2
 }
+//This will call the function getRandomCoverPageLoad and generate the new
+//coverpage with random properties of the Cover class
 getRandomCoverPageLoad()
 
-
 function makeNewCover() {
-  mainCover.classList.remove("visible");
+//  mainCover.classList.remove("visible");
   mainCover.classList.add("hidden");
   formView.classList.remove("hidden");
-  formView.classList.add("visible");
+//  formView.classList.add("visible");
   randomButton.classList.add("hidden");
-  randomButton.classList.remove("visible");
+//  randomButton.classList.remove("visible");
   saveCoverButton.classList.add("hidden");
-  saveCoverButton.classList.remove("visible");
+//  saveCoverButton.classList.remove("visible");
   homeButton.classList.remove("hidden");
-  homeButton.classList.add("visible");
+//  homeButton.classList.add("visible");
 }
 function goHome() {
   mainCover.classList.remove("hidden");
-  mainCover.classList.add("visible");
-  formView.classList.remove("visible");
+//  mainCover.classList.add("visible");
+//  formView.classList.remove("visible");
   formView.classList.add("hidden");
-  randomButton.classList.add("visible");
+//  randomButton.classList.add("visible");
   randomButton.classList.remove("hidden");
-  saveCoverButton.classList.add("visible");
+//  saveCoverButton.classList.add("visible");
   saveCoverButton.classList.remove("hidden");
-  homeButton.classList.remove("visible");
+//  homeButton.classList.remove("visible");
   homeButton.classList.add("hidden");
+  savedView.classList.add("hidden");
 }
 function viewSaved() {
   randomButton.classList.add("hidden");
