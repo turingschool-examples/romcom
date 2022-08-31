b6aa062902b0dc6dc727554ad16a4b5a76f53783
Jacob Zimmer <jacobzimmer1@outlook.com>
2022-08-31 16:42:15 -0600

    feat: built randomCover function, to produce a random cover

--- src/main.js
+++ src/main.js
@@ -98,15 +98,37 @@ var descriptors = [
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
+
 var currentCover;
 
 // Add your event listeners here 👇
-
+window.addEventListener('load', loadRandomCover)
 
 // Create your event handlers and other functions here 👇
 
 
-// We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
+}
+
+function randomCover () {
+  currentCover = new Cover (
+  getRandomIndex(covers),
+  getRandomIndex(titles),
+  getRandomIndex(descriptors),
+  getRandomIndex(descriptors),
+  )
+  console.log (currentCover)
+}
+
+function loadNewCover () {
+  covers.src = currentCover.imageURL
+  title.innerHTML = currentCover.titles
+  descriptors.innerHTML = currentCover.descriptors
+  
+}
+
+function loadRandomCover () {
+  randomCover()
+  loadNewCover()
 }
