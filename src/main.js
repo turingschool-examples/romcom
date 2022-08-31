d31bffd61723fe4fcfc9de44c9dc1614f23262cc
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-08-31 16:09:31 -0700

    add iteration 1 feature. Random button now randomizes cover images, title and descriptor

--- src/main.js
+++ src/main.js
@@ -1,17 +1,32 @@
 // Create variables targetting the relevant DOM elements here 👇
 
-
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-var currentCover;
+var coverImage = document.querySelector('.cover-image');
+var randomButton = document.querySelector('.random-cover-button');
+var title = document.querySelector('.cover-title');
+var subtitle = document.querySelector('.tagline');
 
 // Add your event listeners here 👇
+randomButton.addEventListener('click', changeImage);
+randomButton.addEventListener('click', changeTitle);
+randomButton.addEventListener('click', changeSubtitle);
 
 
 // Create your event handlers and other functions here 👇
+function changeImage() {
+  coverImage.src=covers[getRandomIndex(covers)];
+}
 
+function changeTitle(){
+  title.innerText = titles[getRandomIndex(titles)];
+}
+
+function changeSubtitle(){
+  subtitle.innerText = descriptors[getRandomIndex(descriptors)];
+}
 
 // We've provided one function to get you started
 function getRandomIndex(array) {
