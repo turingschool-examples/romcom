8dd066f0b684a6d1cd1b26b1428e5378759e4889
Tricia Holmes <tricia.michele.holmes@gmail.com>
2022-08-31 18:16:28 -0500

    Add random cover button / refactor

--- src/main.js
+++ src/main.js
@@ -1,21 +1,28 @@
 // Create variables targetting the relevant DOM elements here 👇
+// Main Cover
 var coverImage = document.querySelector('.cover-image')
 var coverTitle = document.querySelector('.cover-title')
 var descriptor1 = document.querySelector('.tagline-1')
 var descriptor2 = document.querySelector('.tagline-2')
 
+// Buttons
+var showNewRandomCoverButton = document.querySelector('.random-cover-button') 
+
+
+
 // We've provided a few variables below
-var savedCovers = [
-  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
-];
+var savedCovers = [];
 var currentCover;
-var randomCover = new Cover(covers[getRandomIndex(covers)],titles[getRandomIndex(titles)],descriptors[getRandomIndex(descriptors)],descriptors[getRandomIndex(descriptors)])
-console.log(randomCover);
+
 // Add your event listeners here 👇
-window.addEventListener('load',randomizeCover)
+window.addEventListener('load', randomizeCover)
+showNewRandomCoverButton.addEventListener('click', randomizeCover)
+
 
 // Create your event handlers and other functions here 👇
 function randomizeCover(){
+  var randomCover = new Cover(covers[getRandomIndex(covers)],titles[getRandomIndex(titles)],descriptors[getRandomIndex(descriptors)],descriptors[getRandomIndex(descriptors)])
+  
   coverImage.src = randomCover.cover
   coverTitle.innerText = randomCover.title
   descriptor1.innerText = randomCover.tagline1
