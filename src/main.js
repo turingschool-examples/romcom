3c65eec598fa016c47e23e922f7aaa997bb5a0ac
Gus Deribeaux <Gusderibeaux@gmail.com>
2022-08-31 19:05:27 -0400

    Created DOM Elements

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 // Create variables targetting the relevant DOM elements here 👇
-
-
+var coverImage = document.querySelector('.cover-image')
+var coverTitle = document.querySelector('.cover-title')
+var tagLine = document.querySelector('.tagline')
+var tagLine1 = document.querySelector('.tagline-1')
+var tagLine2 = document.querySelector('.tagline-2')
+var randomCoverButton = document.querySelector('.random-cover-button')
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
