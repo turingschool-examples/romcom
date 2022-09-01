69afcfcb1aa45b28bb83f2a960faaee44c236ab1
Adam Hughes <adamjordanhughes@gmail.com>
2022-08-31 20:38:04 -0400

    Add saved covers view

--- src/main.js
+++ src/main.js
@@ -6,12 +6,15 @@ var descriptor1 = document.querySelector('.tagline-1')
 var descriptor2 = document.querySelector('.tagline-2')
 var homeView = document.querySelector('.home-view')
 var formView = document.querySelector('.form-view')
+var savedCoversView = document.querySelector('.saved-view')
 
 // Buttons
 var showNewRandomCoverButton = document.querySelector('.random-cover-button')
 var makeYourOwnCoverButton = document.querySelector('.make-new-button')
 var saveCoverButton = document.querySelector('.save-cover-button')
 var homeButton = document.querySelector('.home-button')
+var viewSavedCoversButton =  document.querySelector('.view-saved-button')
+
 
 // We've provided a few variables below
 var savedCovers = [];
@@ -21,7 +24,7 @@ var currentCover;
 window.addEventListener('load', randomizeCover)
 showNewRandomCoverButton.addEventListener('click', randomizeCover)
 makeYourOwnCoverButton.addEventListener('click', changeToFormView)
-
+viewSavedCoversButton.addEventListener('click', changeToSavedCoversView)
 
 // Create your event handlers and other functions here 👇
 function randomizeCover(){
@@ -38,6 +41,16 @@ function randomizeCover(){
 function changeToFormView (){
   homeView.classList.add('hidden')
   formView.classList.remove('hidden')
+  savedCoversView.classList.add('hidden')
+  showNewRandomCoverButton.classList.add('hidden')
+  saveCoverButton.classList.add('hidden')
+  homeButton.classList.remove('hidden')
+}
+
+function changeToSavedCoversView(){
+  savedCoversView.classList.remove('hidden')
+  homeView.classList.add('hidden')
+  formView.classList.add('hidden')
   showNewRandomCoverButton.classList.add('hidden')
   saveCoverButton.classList.add('hidden')
   homeButton.classList.remove('hidden')
