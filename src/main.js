6f4a37b7a3a63c080c685d93a65d1611d13bda2d
Josh Pierce <joshua.alan.pierce@gmail.com>
2022-08-31 19:40:40 -0600

    Generate random cover on load

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 // Create variables targetting the relevant DOM elements here 👇
-
-
+var coverImg = document.querySelector(".cover-image")
+var coverTitle = document.querySelector(".cover-title")
+var tagLine1 = document.querySelector(".tagline-1")
+var tagLine2 = document.querySelector(".tagline-2")
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
@@ -9,6 +11,7 @@ var currentCover;
 
 // Add your event listeners here 👇
 
+window.addEventListener('load', displayRandomCover)
 
 // Create your event handlers and other functions here 👇
 
@@ -16,4 +19,15 @@ var currentCover;
 // We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
+}
+
+
+
+function displayRandomCover() {
+  currentCover = new Cover(covers[getRandomIndex(covers)], titles[getRandomIndex(titles)], descriptors[getRandomIndex(descriptors)], descriptors[getRandomIndex(descriptors)])
+  coverImg.src = currentCover.cover
+  coverTitle.innerText = currentCover.title
+  tagLine1.innerText = currentCover.tagline1
+  tagLine2.innerText = currentCover.tagline2
+  return currentCover
 }
