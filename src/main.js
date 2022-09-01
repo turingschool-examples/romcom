e7065603d5740f63ec9aed7ddb0d87d43cef301d
Jocelle Bautista <jcllebautista@gmail.com>
2022-09-01 09:58:15 -0400

    add display function and query selectors

--- src/main.js
+++ src/main.js
@@ -4,28 +4,36 @@ var saveCoverButton = document.querySelector('.save-cover-button')
 var viewSavedCovers = document.querySelector('.view-save-button')
 var makeYourOwnCover = doccument.querySelector('.make-new-button')
 var homeButton = document.querySelector('.home-button')
-
+var displayCoverImage = document.querySelector('.cover-image')
+var displayCoverTitle = document.querySelector('.cover-title')
+var displayTagline1 = document.querySelector('.tagline-1')
+var displayTagline2 = document.querySelector('.tagline-2')
 
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-var currentCover;
+var currentCover =
+currentCover.cover = Math.floor(Math.random() * covers.length)
+currentCover.title = Math.floor(Math.random() * titles.length)
+currentCover.tagline1 = Math.floor(Math.random() * descriptors.length)
+currentCover.tagline2 = Math.floor(Math.random() * descriptors.length)
 
 // Add your event listeners here 👇
 randomCoverButton.addEventListener('click', displayNewRandom)
 
 // Create your event handlers and other functions here 👇)
 function createNewRandom() {
-  var currentCover = new Cover
-  currentCover.title = Math.floor(Math.random() * titles.length)
-  currentCover.cover = Math.floor(Math.random() * covers.length)
-  currentCover.tagline1 = Math.floor(Math.random() * descriptors.length)
-  currentCover.tagline2 = Math.floor(Math.random() * descriptors.length)
   return currentCover
 }
 
 function displayNewRandom() {
-  mainCoverDisplay.innerHTML = createNewRandom()
+  displayCover = new Cover
+  displayCoverImage.innerHTML = currentCover.cover[i]
+  displayCoverTitle.innerHTML = currentCover.title[i]
+  displayCoverTagline1.innerHTML = currentCover.tagline1[i]
+  displayCoverTagline2.innerHTML = currentCover.tagline2[i]
+  return displayCover
+  
 }
 
 
