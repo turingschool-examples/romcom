a2f8abf36742e729056e1ad019badf5c9564c2cf
Brett Kuhn <brettkuhn87@gmail.com>
2022-09-01 13:39:07 -0600

    Created button functionality for new random cover

--- src/main.js
+++ src/main.js
@@ -5,6 +5,19 @@ var htmlTitle = document.querySelector(".cover-title")
 var htmlTagline1 = document.querySelector(".tagline-1")
 var htmlTagline2 = document.querySelector(".tagline-2")
 
+var randomCoverButton = document.querySelector(".random-cover-button")
+
+
+var currentCover
+var randomCover = new Cover(covers[getRandomIndex(covers)], titles[getRandomIndex(titles)], descriptors[getRandomIndex(descriptors)], descriptors[getRandomIndex(descriptors)]);
+currentCover = randomCover
+
+htmlCoverImage.src = randomCover.cover
+htmlTitle.innerText = randomCover.title
+htmlTagline1.innerText = randomCover.tagline1
+htmlTagline2.innerText = randomCover.tagline2
+
+
 // var dataArray = document.querySelector()
 
 
@@ -15,10 +28,11 @@ var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 
-var currentCover
+
 
 // Add your event listeners here 👇
 
+randomCoverButton.addEventListener("click", createRandomCover)
 
 // Create your event handlers and other functions here 👇
 
