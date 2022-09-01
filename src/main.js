1512f60e3bd312516e50f9fbfbe905e1851af514
Evan Swanson <evan.sundelius@gmail.com>
2022-08-31 21:18:07 -0500

    Complete iteration 0

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 // Create variables targetting the relevant DOM elements here 👇
-
-
+var coverImage = document.querySelector(`.cover-image`)
+var coverTitle = document.querySelector(`.cover-title`)
+var tagline = document.querySelector(`.tagline`)
+var tagline1 = document.querySelector(`.tagline-1`)
+var overlay = document.querySelector(`.overlay`)
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
@@ -9,11 +12,14 @@ var currentCover;
 
 // Add your event listeners here 👇
 
-
 // Create your event handlers and other functions here 👇
-
 
 // We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
+
+var randomCover = getRandomIndex(covers)
+
+coverImage.src = covers[randomCover]
+console.log(coverImage.src)
