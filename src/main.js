b5936f4f4e7cb3551a8616de08206126e0839ce7
Courtney Lippman <courtney.lippman@colorado.edu>
2022-08-31 21:07:44 -0600

    [WIP]: Add ability to randomly change image.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 // Create variables targetting the relevant DOM elements here 👇
-
+var newRandomCoverButton = document.querySelector('.random-cover-button');
+// var newRandomtitle = document.querySelector('.cover-title');
+// var newRandomtagline = document.querySelector('.tagline');
+var newRandomCover = document.querySelector('.cover-image');
 
 // We've provided a few variables below
 var savedCovers = [
@@ -8,12 +11,30 @@ var savedCovers = [
 var currentCover;
 
 // Add your event listeners here 👇
+ //newRandomCoverButton.addEventListener('click', createRandomCover());
+// newRandomTitle.addEventListener('click', createRandomCover());
+// newRandomtagline.addEventListener('click', createRandomCover());
+// newRandomCover.addEventListener('click', createRandomCover());
 
-
+//document.querySelector('.cover-image').addEventListener('click', createRandomCover())
 // Create your event handlers and other functions here 👇
-
-
+//   currentCover = new Cover
+  // currentCover.cover = getRandomIndex(covers.length);
+  // currentCover.title = getRandomIndex(titles.length);
+  // currentCover.tagline1 = getRandomIndex(descriptors.length);
+  // currentCover.tagline2 = getRandomIndex(descriptors.length);
+  // currentCover.cover = Math.floor(Math.random() * covers.length);
+  //  return covers[currentCover.cover]
+  // currentCover.title = Math.floor(Math.random() * titles.length);
+  // currentCover.tagline1 = Math.floor(Math.random() * descriptors.length);
+  // currentCover.tagline2 = Math.floor(Math.random() * descriptors.length);
+//}
 // We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
+
+//pseudocode: click button and cover changes randomly.
+var newCover = getRandomIndex(covers);
+newRandomCover.src = covers[newCover]
+newRandomCover.src
