29c9405eec1937c96075b25b774e5f003d8fe2d6
Ryan Nagel <RNagel29@gmail.com>
2022-08-31 19:58:35 -0600

    Add funtion to random cover button

--- src/main.js
+++ src/main.js
@@ -3,17 +3,19 @@ var title = document.querySelector('.cover-title');
 var tagline1 = document.querySelector('.tagline-1');
 var tagline2 = document.querySelector('.tagline-2');
 var coverImage = document.querySelector('.cover-image');
-// coverImage.src = './assets/bluebrocade.jpg';
+var randomCoverButton = document.querySelector('.random-cover-button');
 
 
 // We've provided a few variables below
 var savedCovers = [
-  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
+  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows"),
+//this will be an array of new Cover instances
 ];
-var currentCover;
+
+
 
 // Add your event listeners here 👇
-
+randomCoverButton.addEventListener('click',randomize);
 
 // Create your event handlers and other functions here 👇
 
@@ -24,11 +26,29 @@ function getRandomIndex(array) {
   return array[i]
 }
 
-title.innerText = getRandomIndex(titles);
-tagline1.innerText = getRandomIndex(descriptors);
-tagline2.innerText = getRandomIndex(descriptors);
-  if (tagline1 === tagline2) {
+function randomize() {
+  title.innerText = getRandomIndex(titles);
+  tagline1.innerText = getRandomIndex(descriptors);
   tagline2.innerText = getRandomIndex(descriptors);
-  };
-  //come back to this if statement
-coverImage.src = getRandomIndex(covers)
+    if (tagline1 === tagline2) {
+      tagline2.innerText = getRandomIndex(descriptors);
+    };
+  coverImage.src = getRandomIndex(covers);
+}
+//   var randomTagline1 = getRandomIndex(descriptors);
+//   tagline1.innerText = randomTagline1;
+//
+//   var randomTagline2 = getRandomIndex(descriptors);
+//   tagline2.innerText = randomTagline2;
+//     if (tagline1 === tagline2) {
+//       randomTagline2 = getRandomIndex(descriptors);
+//       tagline2.innerText = randomTagline2;
+//     };
+//     //come back to this if statement
+//   var randomImage = getRandomIndex(covers);
+//   coverImage.src = randomImage;
+// }
+
+randomize();
+
+var currentCover = new Cover(coverImage.src, title.innerText , tagline1.innerText, tagline2.innerText);
