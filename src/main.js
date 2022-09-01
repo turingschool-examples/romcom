e885e918c811b398b376916bac4a6fa5c17f0328
Courtney Lippman <courtney.lippman@colorado.edu>
2022-09-01 15:08:53 -0600

    WIP feat: Trying to create functionality in makeMyBook.

--- src/main.js
+++ src/main.js
@@ -12,7 +12,12 @@ var viewFormView = document.querySelector('.form-view')
 var viewHomeView = document.querySelector('.home-view')
 var viewSavedView = document.querySelector('.saved-view')
 var viewSavedCoversSection = document.querySelector('.saved-covers-section')
+var makeMyBookButton = document.querySelector('.create-new-book-button')
 
+var inputUserCover = document.querySelector('.user-cover')
+var inputTitle = document.querySelector('.user-title')
+var inputDescriptor1 = document.querySelector('.user-desc1')
+var inputDescriptor2 = document.querySelector('.user-desc2')
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
@@ -24,6 +29,7 @@ randomCoverButton.addEventListener('click', createNewCover);
 makeNewButton.addEventListener('click', makeOwnCover)
 viewSavedButton.addEventListener('click', saveCovers)
 homeButton.addEventListener('click', takeMeHome)
+makeMyBookButton.addEventListener('click', makeMyBook)
 
 // We've provided one function to get you started
 function getRandomIndex(array) {
@@ -62,6 +68,10 @@ function takeMeHome() {
   randomCoverButton.classList.remove('hidden')
   saveCoverButton.classList.remove('hidden')
   homeButton.classList.add('hidden')
+}
+
+function makeMyBook() {
+savedCovers.push() = input.value
 }
 
 
