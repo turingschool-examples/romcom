dd84fdb41f9f71cda7e9f04622bb6daf4624e42e
Jacob Zimmer <jacobzimmer1@outlook.com>
2022-08-31 21:27:22 -0600

    feat: finished function to load random cover

--- src/main.js
+++ src/main.js
@@ -1,105 +1,15 @@
 // Create variables targetting the relevant DOM elements here 👇
-
+var coverImg = document.querySelector('.cover-image')
+var coverTitle = document.querySelector('.cover-title')
+var firstDescriptor = document.querySelector('.tagline-1')
+var secondDescriptor = document.querySelector('.tagline-2')
 
 // We've provided a few variables below
-var covers = [
-  './assets/bluebrocade.jpg',
-  './assets/dance.jpg', 
-  './assets/embrace.png',
-  './assets/fire.png',
-  './assets/frock.png',
-  './assets/glorious.jpg',
-  './assets/golden.jpg',
-  './assets/maskedmeeting.jpg',
-  './assets/masquerade.jpg',
-  './assets/moonlitmeadow.jpg',
-  './assets/office.png',
-  './assets/picnic.jpg',
-  './assets/pirate.jpg',
-  './assets/prairie.jpg',
-  './assets/redrenaissance.jpg',
-  './assets/regency.jpg',
-  './assets/ribbons.jpg',
-  './assets/roses.jpg',
-  './assets/ruffles.jpg',
-  './assets/scroll.jpg',
-  './assets/shine.png',
-  './assets/smolder.png',
-  './assets/snow.jpg',
-  './assets/sparkles.jpg',
-  './assets/stripes.png',
-  './assets/wildwest.jpg',
-  './assets/windswept.jpg',
-];
-
-var titles = [
-  "Passionate Moonlight",
-  "Love's Misery",
-  "Roses and Flame",
-  "Innocent Roses",
-  "Silk and Sense",
-  "Hearts Aflame",
-  "Fiery Passion",
-  "Stolen Hearts",
-  "Secrets and Silk",
-  "Dreams of Fire",
-  "Lovers and Enemies",
-  "Passion's Embrace",
-  "Harbinger by Moonlight",
-  "Rouge Red",
-  "Moonlit Mysteries",
-  "Crimson Roses",
-  "Destiny's Fires",
-  "Proposals and Passion",
-  "Silk Wedding",
-  "Masked Seduction",
-  "Crimson Masquerade",
-  "Stolen Rubies",
-  "Emerald Eyes",
-  "Sapphire Skies",
-  "Opal Passions"
-];
-
-var descriptors = [
-  "passion",
-  "glory",
-  "romance",
-  "woe",
-  "sorrow",
-  "pain",
-  "ardor",
-  "devotion",
-  "excitement",
-  "fervor",
-  "rage",
-  "spirit",
-  "zeal",
-  "ecstasy",
-  "fire",
-  "storms",
-  "tempests",
-  "rapture",
-  "ire",
-  "jealousy",
-  "exhilaration",
-  "bliss",
-  "enchantment",
-  "paradise",
-  "calamity",
-  "disaster",
-  "heartache",
-  "misfortune",
-  "agony",
-  "curses",
-  "blessings",
-  "melancholy"
-];
-
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 
-var currentCover;
+var currentCover
 
 // Add your event listeners here 👇
 window.addEventListener('load', loadRandomCover)
@@ -108,7 +18,8 @@ window.addEventListener('load', loadRandomCover)
 
 
 function getRandomIndex(array) {
-  return Math.floor(Math.random() * array.length);
+  var index = Math.floor(Math.random() * array.length)
+  return array[index]
 }
 
 function randomCover () {
@@ -118,15 +29,14 @@ function randomCover () {
   getRandomIndex(descriptors),
   getRandomIndex(descriptors),
   )
-  console.log (currentCover)
 }
 
 function loadNewCover () {
-  covers.src = currentCover.imageURL
-  title.innerHTML = currentCover.titles
-  descriptors.innerHTML = currentCover.descriptors
-  
-}
+  coverImg.src = currentCover.cover
+  coverTitle.innerText = currentCover.title
+  firstDescriptor.innerText = currentCover.tagline1
+  secondDescriptor.innerText = currentCover.tagline2
+  }
 
 function loadRandomCover () {
   randomCover()
