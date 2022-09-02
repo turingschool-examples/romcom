c2b82c4e1b65c1b4706dcc879844e34b30b66b6c
Angela Wirth <angiewirth92@gmail.com>
2022-09-02 10:28:15 -0400

    create user book functions

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ var saveCoverButton = document.querySelector('.save-cover-button')
 var viewSavedButton = document.querySelector('.view-saved-button')
 var makeYourOwnButton = document.querySelector('.make-new-button')
 var homeButton = document.querySelector('.home-button')
+var newBookButton = document.querySelector('.create-new-book-button')
 var mainCoverDisplay = document.querySelector('.main-cover')
 var displayCoverImage = document.querySelector('.cover-image')
 var displayCoverTitle = document.querySelector('.cover-title')
@@ -11,7 +12,10 @@ var displayTagline2 = document.querySelector('.tagline-2')
 var formPage = document.querySelector('.form-view')
 var homePage = document.querySelector('.home-view')
 var savedPage = document.querySelector('.saved-view')
-
+var userCover = document.getElementById('cover')
+var userTitle = document.getElementById('title')
+var userDescriptor1 = document.getElementById('descriptor1')
+var userDescriptor2 = document.getElementById('descriptor2')
 
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
@@ -23,6 +27,7 @@ randomCoverButton.addEventListener('click', makeNewCover)
 makeYourOwnButton.addEventListener('click', displayFormPage)
 viewSavedButton.addEventListener('click', displaySavedPage)
 homeButton.addEventListener('click', displayHomePage)
+newBookButton.addEventListener('click', newCoverForm)
 
 // Create your event handlers and other functions here 👇)
 function getRandomIndex(array) {
@@ -70,4 +75,19 @@ function displaySavedPage() {
   homeButton.classList.remove('hidden')
   randomCoverButton.classList.add('hidden')
   saveCoverButton.classList.add('hidden')
+}
+
+function newCoverForm() {
+  covers.push(userCover)
+  titles.push(userTitle)
+  descriptors.push(userDescriptor1, userDescriptor2)
+  userBook = new Cover (userCover, userTitle, userDescriptor1, userDescriptor2)
+  displayUserBook()
+}
+
+function displayUserBook() {
+  displayCoverImage.src = userBook.cover
+  displayCoverTitle.innerText = userBook.title
+  displayTagline1.innerText = userBook.tagline1
+  displayTagline2.innerText = userBook.tagline2
 }
