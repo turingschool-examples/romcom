a9d4c93f220ffd9b17a6f65498863296f3d08e91
Jacob Zimmer <jacobzimmer1@outlook.com>
2022-09-02 10:21:46 -0600

    feat: show random cover button

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ var coverImg = document.querySelector('.cover-image')
 var coverTitle = document.querySelector('.cover-title')
 var firstDescriptor = document.querySelector('.tagline-1')
 var secondDescriptor = document.querySelector('.tagline-2')
-
+var randomCoverButton = document.querySelector('.random-cover-button')
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
@@ -13,7 +13,7 @@ var currentCover
 
 // Add your event listeners here 👇
 window.addEventListener('load', loadRandomCover)
-
+randomCoverButton.addEventListener('click', loadRandomCover)
 // Create your event handlers and other functions here 👇
 
 
