817657280644723ef45e4a48ad1e5471130b0477
Sam Rice <samsrice@gmail.com>
2022-09-01 19:03:47 -0700

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,19 +1,66 @@
-// Create variables targetting the relevant DOM elements here 👇
+//BUTTON VARIABLES
+var homeButton = document.querySelector(".home-button hidden");
+var randomButton = document.querySelector(".random-cover-button");
+var saveButton = document.querySelector(".save-cover-button");
+var viewSavedButton = document.querySelector(".view-saved-button");
+var makeNewButton = document.querySelector(".make-new-button");
 
+//PAGE VIEWS
+var homeView = document.querySelector('.home-view');
+var formView = document.querySelector('.form-view');
+var savedView = document.querySelector('.saved-view');
+
+//DISPLAYED POSTER ELEMENTS VARIABLES
+var coverImage = document.querySelector(".cover-image");
+var coverTitle = document.querySelector(".cover-title");
+var tagline1 = document.querySelector(".tagline-1");
+var tagline2 = document.querySelector(".tagline-2");
+
+//RANDOM COVER ELEMENTS
+var randomImage = covers[getRandomIndex(covers)];
+var randomTitle = titles[getRandomIndex(titles)];
+var randomTagline1 = descriptors[getRandomIndex(descriptors)];
+var randomTagline2 = descriptors[getRandomIndex(descriptors)];
 
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-var currentCover;
 
-// Add your event listeners here 👇
+//DEFAULT HOME PAGE
+var currentCover = new Cover(randomImage, randomTitle, randomTagline1, randomTagline2);
 
+coverImage.src = currentCover.cover;
+coverTitle.innerText = currentCover.title;
+tagline1.innerText = currentCover.tagline1;
+tagline2.innerText = currentCover.tagline2;
 
-// Create your event handlers and other functions here 👇
+//EVENT LISTENERS
 
+randomButton.addEventListener("click", newRandomCover);
+makeNewButton.addEventListener("click", showForm);
 
-// We've provided one function to get you started
+//EVENT HANDLERS/MISC FUNCTIONS
+
+function newRandomCover() {
+  var randomImage = covers[getRandomIndex(covers)];
+  var randomTitle = titles[getRandomIndex(titles)];
+  var randomTagline1 = descriptors[getRandomIndex(descriptors)];
+  var randomTagline2 = descriptors[getRandomIndex(descriptors)];
+
+  currentCover = new Cover(randomImage, randomTitle, randomTagline1, randomTagline2);
+
+  coverImage.src = currentCover.cover;
+  coverTitle.innerText = currentCover.title;
+  tagline1.innerText = currentCover.tagline1;
+  tagline2.innerText = currentCover.tagline2;
+};
+
+function showForm() {
+  formView.classList.remove("hidden");
+  homeView.classList.add("hidden");
+};
+
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
-}
+};
