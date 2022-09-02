50912c44f685de2a4198b0cd1771a14ec908a8e3
Adelle Pitsas <apitsas4@gmail.com>
2022-09-01 20:30:14 -0400

    make show new cover buttom accessible

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 // Create variables targetting the relevant DOM elements here 👇
 var coverImgSrc = document.querySelector('.cover-image')
 var coverTitle = document.querySelector('.cover-title')
-var tagLine = document.querySelector('.tagline')
 var tagLine1 = document.querySelector('.tagline-1')
 var tagLine2 = document.querySelector('.tagline-2')
 var randomCoverButton = document.querySelector('.random-cover-button')
-var view = document.querySelector('.main-cover')
+var makeCoverButton = document.querySelector('.make-new-button')
+var formView = document.querySelector('form-view')
+var homeView = document.querySelector('home-view')
 
 // We've provided a few variables below
 var savedCovers = [
@@ -17,7 +18,7 @@ var currentCover
 // Add your event listeners here 👇
 randomCoverButton.addEventListener("click", generateRandomCover);
 
-// view.addEventListener("load", getRandomCover);
+makeCoverButton.addEventListener("click", getForm)
 
 // Create your event handlers and other functions here 👇
 function getRandomIndex(array) {
@@ -26,25 +27,15 @@ function getRandomIndex(array) {
 
 
 function generateRandomCover() {
-  var randomImage = covers[getRandomIndex(covers)];
-  var randomTitle = titles[getRandomIndex(titles)];
-  var randomDescriptor1 = descriptors[getRandomIndex(descriptors)];
-  var randomDescriptor2 = descriptors[getRandomIndex(descriptors)];
-  var randomCover = new Cover (randomImage, randomTitle, randomDescriptor1, randomDescriptor2);
-  // var currentCover = randomCover;
-  displayCover()
+  coverImgSrc.src = covers[getRandomIndex(covers)]
+  coverTitle.innerText = titles[getRandomIndex(titles)]
+  tagLine1.innerText = descriptors[getRandomIndex(descriptors)]
+  tagLine2.innerText = descriptors[getRandomIndex(descriptors)]
 }
 
-function displayCover() {
-  coverImgSrc.src = randomCover.cover;
-  coverTitle.innerText = randomCover.title;
-  tagLine1.innerText = randomCover.tagline1;
-  tagLine2.innerText = randomCover.tagline2;
+function getForm () {
+  homeView.style.display = "none"
 }
-
-  // setAttribute (coverImgSrc, currentCover)
-  // // coverImgSrc = currentCover.cover
-  // coverTitle.style.display = currentCover.cover
 
 // We've provided one function to get you started
 // function getRandomIndex(array) {
