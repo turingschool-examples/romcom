952bf2d235d65cd7e5369178b3916d52785ba8bc
Evan Swanson <evan.sundelius@gmail.com>
2022-09-03 15:23:59 -0500

    Work through it3

--- src/main.js
+++ src/main.js
@@ -11,10 +11,10 @@ var makeNewButton = document.querySelector('.make-new-button')
 var homeButton = document.querySelector('.home-button')
 var customButton = document.querySelector('.create-new-book-button')
 //~~
-var mainCoverDisplay = document.querySelector('.main-cover')
 var formPage = document.querySelector('.form-view')
 var homePage = document.querySelector('.home-view')
 var savedPage = document.querySelector('.saved-view')
+var savedSection = document.querySelector('.saved-covers-section')
 //~~
 var userCover = document.querySelector('.user-cover')
 var userTitle = document.querySelector('.user-title')
@@ -22,7 +22,10 @@ var userTag1 = document.querySelector('.user-desc1')
 var userTag2 = document.querySelector('.user-desc2')
 // We've provided a few variables below
 var savedCovers = [
- new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
+ new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg",
+ "Sunsets and Sorrows",
+ "sunsets",
+ "sorrows")
 ];
 var currentCover;
 
@@ -35,20 +38,21 @@ randomButton.addEventListener('click', displayHomePage)
 makeNewButton.addEventListener('click', displayFormPage)
 makeNewButton.addEventListener('click', displayFormButtons)
 
-viewSavedButton.addEventListener('click', displaySavedPage)
-viewSavedButton.addEventListener('click', displayFormButtons)
+viewSavedButton.addEventListener('click', viewSavedCovers)
 
 homeButton.addEventListener('click', displayHomePage)
 homeButton.addEventListener('click', displayHomeButtons)
 
-customButton.addEventListener('click', createCustomBook)
+saveCoverButton.addEventListener('click', save)
+
+customButton.addEventListener('click', createCustom)
 // Create your event handlers and other functions here 👇
 
 // We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//0~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 function makeRandomCover() {
   var randomImage = covers[getRandomIndex(covers)]
   var randomTitle = titles[getRandomIndex(titles)]
@@ -65,55 +69,56 @@ function makeCurrentCover() {
   tag1.innerText = currentCover.tagline1
   tag2.innerText = currentCover.tagline2
 }
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//1~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 function displayFormPage() {
   formPage.classList.remove('hidden')
   homePage.classList.add('hidden')
-  savedPage.clasList.add('hidden')
+  savedPage.classList.add('hidden')
 }
-
 function displayFormButtons() {
   homeButton.classList.remove('hidden')
   randomButton.classList.add('hidden')
   saveCoverButton.classList.add('hidden')
 }
-
+//~~
 function displayHomePage() {
   formPage.classList.add('hidden')
   homePage.classList.remove('hidden')
   savedPage.clasList.add('hidden')
 }
-
 function displayHomeButtons() {
   homeButton.classList.add('hidden')
   randomButton.classList.remove('hidden')
   saveCoverButton.classList.remove('hidden')
+  viewSavedButton.classList.remove('hidden')
 }
-
+//~~
 function displaySavedPage() {
   formPage.classList.add('hidden')
   homePage.classList.add('hidden')
   savedPage.classList.remove('hidden')
 }
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-function createCustomBook() {
+function displaySavedButtons() {
+  homeButton.classList.remove('hidden')
+  randomButton.classList.add('hidden')
+  saveCoverButton.classList.add('hidden')
+  viewSavedButton.classList.add('hidden')
+}
+//2~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function createCustom() {
   event.preventDefault()
 
-  var inputCover = userCover.value
+  var inputImage = userCover.value
   var inputTitle = userTitle.value
   var inputTag1 = userTag1.value
   var inputTag2 = userTag2.value
 
-  covers.push(inputCover)
-  titles.push(inputTitle)
-  descriptors.push(inputTag1, inputTag2)
+  coverImage.src = userCover.value
+  coverTitle.innerText = userTitle.value
+  tag1.innerText = userTag1.value
+  tag2.innerText = userTag2.value
 
-  currentCover = new Cover(inputCover, inputTitle, inputTag1, inputTag2)
-
-  coverImage.src = inputCover
-  coverTitle.innerText = inputTitle
-  tag1.innerText = inputTag1
-  tag2.innerText = inputTag2
+  currentCover = new Cover(coverImage.src, coverTitle.innerText, tag1.innerText, tag2.innerText)
 
   returnHome()
 }
@@ -125,3 +130,48 @@ function returnHome() {
   randomButton.classList.remove('hidden')
   saveCoverButton.classList.remove('hidden')
 }
+//3~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function viewSavedCovers() {
+  savedSection.innerHTML = "";
+  for (var i = 1; i < savedCovers.length; i++) {
+    createCoverElement(savedCovers[i])
+  }
+  homePage.classList.add('hidden')
+  formPage.classList.add('hidden')
+  savedPage.classList.remove('hidden')
+  homeButton.classList.remove('hidden')
+  randomButton.classList.add('hidden')
+  saveCoverButton.classList.add('hidden')
+}
+
+function save() {
+  var isSaved = false
+  for (var i = 1; i < savedCovers.length; i++) {
+    if (currentCover.id === savedCovers[i].id) {
+      isSaved = true
+    }
+  }
+  if (!isSaved) {
+    savedCovers.push(currentCover)
+  }
+}
+
+function createCoverElement(para) {
+  //savedSection.innerHTML = "";
+  //for (var i = 0; i < savedCovers.length; i++) {
+  var newDiv = document.createElement('div')
+  newDiv.setAttribute('id', para.id)
+  newDiv.classList.add('mini-cover')
+  newDiv.style.overflow = 'hidden'
+  newDiv.style.background = `url(${para.cover})`
+  newDiv.style.backgroundSize = 'cover'
+  newDiv.innerHTML = `
+  <h4 class='cover-title'>${para.title}</h4>
+  <p class='tagline'> A tale of ${para.tagline1} and ${para.tagline2}</p> `
+
+  savedSection.appendChild(newDiv)
+//}
+}
+
+
+// if(!savedCovers.includes(currentCover)) {
