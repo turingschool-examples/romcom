96d0e3800e3cc8176342ce7501d7f8d70e5d18b0
Dmitrii Gubko <dalnoboy29region@gmail.com>
2022-09-03 11:52:32 -0600

    Fix bugs on saved posters

--- src/main.js
+++ src/main.js
@@ -54,13 +54,14 @@ homeBtn.addEventListener("click", home);
 viewSavedCoversBtn.addEventListener("click", viewSavedCovers);
 createNewBookButton.addEventListener("click", makeYourCover);
 makeMyBookBtn.addEventListener("click", createNewBook);
-saveCoverBtn.addEventListener('click', saveCover);
+saveCoverBtn.addEventListener("click", saveCover);
 
 // Create your event handlers and other functions here 👇
 
 function viewSavedCovers() {
-  for(var i = 1; i < savedCovers.length; i++) {
-    createCoverElement(savedCovers[i])
+  savedCoverSection.innerHTML = "";
+  for (var i = 1; i < savedCovers.length; i++) {
+    createCoverElement(savedCovers[i]);
   }
   homeView.classList.add("hidden");
   formView.classList.add("hidden");
@@ -99,23 +100,22 @@ function createNewBook(event) {
 }
 
 function saveCover() {
-  savedCovers.push(currentCover)
+  savedCovers.push(currentCover);
 }
 
 function createCoverElement(obj) {
-  var newDiv = document.createElement('div')
-  newDiv.classList.add('mini-cover')
+  var newDiv = document.createElement("div");
+  newDiv.classList.add("mini-cover");
+  newDiv.style.overflow = "hidden";
+  newDiv.style.background = `url(${obj.cover})`;
+  newDiv.style.backgroundSize = "cover";
   newDiv.innerHTML = `
-  <img src = "${obj.cover}" alt = "romcom image" height = "100%">
   <h4 class='cover-title'>${obj.title}</h4>
-  <p class='tagline'> A tale of ${obj.tagLine1} and ${obj.tagLine2}</p>
-  `
+  <p class='tagline'> A tale of ${obj.tagline1} and ${obj.tagline2}</p>
+  `;
 
-  savedCoverSection.appendChild(newDiv)
-
+  savedCoverSection.appendChild(newDiv);
 }
-
-
 
 function home() {
   homeView.classList.remove("hidden");
