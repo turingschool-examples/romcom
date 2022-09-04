2fc44e3889a3896e26e1dd6a75ab04ae5fcf0609
Spencer Haka <spencer.haka@gmail.com>
2022-09-04 13:46:24 -0600

    Complete final refactoring and readme update

--- README.md
+++ README.md
@@ -1,41 +1,37 @@
-# RomCom
-
-A boilerplate repo. 
-
-## Set Up
-1. ONE teammate: fork this boilerplate repository 
-2. Clone down your new, forked repo
-3. cd into the repository
-4. Open it in your text editor
-5. View the project in the browser by running open index.html in your terminal
-6. Add all project partners and your assigned instructor as collaborators on the repository
-
-The project spec & rubric can be found [here](https://frontend.turing.io/projects/module-1/romcom-pair.html)
-
-______________________________________________________  
-# README Template  
-Before turning this project in, erase this line and everything above it and fill in the info below.  
-______________________________________________________  
-
 # RomCom  
 
-### Abstract:
-[//]: <> (Briefly describe what you built and its features. What problem is the app solving? How does this application solve that problem?)
+## Abstract:
+This app randomly generates romance novel covers, allowing the user to generate their own covers, save any cover they choose, and delete the covers they no longer want. The app features a home page, a form in which the user may enter their own data (image, titles, descriptors), and a page where all saved covers may be viewed.<br>
+In a world where romance novels are read, and churned out at an incredible pace, there needs to be way to streamline new novel cover creation, thereby getting the product to those who consume it more effectively.
 
-### Installation Instructions:
-[//]: <> (What steps does a person have to take to get your app cloned down and running?)
+## Installation Instructions:
+The open-sourced code is located at https://github.com/Speekins/romcom. Fork the repository (top right hand side), and clone to the local machine by copying the SSH key and entering "git clone [SSH Key]" into the machine's terminal to create a local working repository. Open the application in the default internet browser by typing "open index.html" into the terminal.
 
-### Preview of App:
-[//]: <> (Provide ONE gif or screenshot of your application - choose the "coolest" piece of functionality to show off.)
+## Preview of App:
+![Alt Text](https://videoapi-muybridge.vimeocdn.com/animated-thumbnails/image/37491318-d47c-495e-bd5a-643986b15be6.gif?ClientID=vimeo-core-prod&Date=1662319122&Signature=e2e2fce916f2c1f5726c01f4e979c7c1b14e09d0)
 
-### Context:
-[//]: <> (Give some context for the project here. How long did you have to work on it? How far into the Turing program are you?)
+## Context:
+We are two Turing students currently in the first module (of four). After studying basic programming principles for a week and a half (yes, that's 10 days), we were tasked with creating this application within the span of 5 days. Each day required roughly 3 hours.
 
-### Contributors:
-[//]: <> (Who worked on this application? Link to their GitHubs.)
+## Contributors:
+[Anna's GitHub]('github.com/AnnaPete')<br>
+[Spencer's GitHub]('github.com/Speekins')
 
-### Learning Goals:
-[//]: <> (What were the learning goals of this project? What tech did you work with?)
+## Learning Goals:
+- Pair programming and writing code collaboratively.<br>
+- Communicate, troubleshoot, and plan effectively as a team.<br>
+- Writing clean and readable JavaScript.<br>
+- Accessing HTML elements via the DOM.<br>
+- Using JavaScript event listeners and handlers to update HTML elements.
 
-### Wins + Challenges:
-[//]: <> (What are 2-3 wins you have from this project? What were some challenges you faced - and how did you get over them?)
+## Wins + Challenges:
+#### Wins
+- Understanding how to read more complex code.
+- Learning how to manage schedules and time constraints with a partner.
+- Figuring out solutions by thorough research and collaboration.
+
+#### Challenges
+- Hyper-focusing on trial & error for coding solutions.
+  - We overcame this by reaching out to the cohort and other contacts for assistance.
+- Technical difficulties with GitHub work-flow.
+  - These were often resolved by leaning on the GitHub documentation.
--- src/main.js
+++ src/main.js
@@ -2,20 +2,26 @@ var coverImage = document.querySelector('.cover-image');
 var title = document.querySelector('.cover-title');
 var descriptor1 = document.querySelector('.tagline-1');
 var descriptor2 = document.querySelector('.tagline-2');
+var priceTag = document.querySelector('.price-tag');
+var overlay = document.querySelector('.overlay');
+
 var randomButton = document.querySelector('.random-cover-button')
 var homeButton = document.querySelector('.home-button');
 var saveCoverButton = document.querySelector('.save-cover-button');
 var viewSavedButton = document.querySelector('.view-saved-button');
 var makeNewButton = document.querySelector('.make-new-button');
+var createNewBookButton = document.querySelector('.create-new-book-button');
+
 var homeView = document.querySelector('.home-view');
 var formView = document.querySelector('.form-view');
 var savedView = document.querySelector('.saved-view');
-var createNewBookButton = document.querySelector('.create-new-book-button');
+var savedSection = document.querySelector('.saved-covers-section');
+
 var userCover = document.querySelector('.user-cover');
 var userTitle = document.querySelector('.user-title');
 var userDesc1 = document.querySelector('.user-desc1');
 var userDesc2 = document.querySelector('.user-desc2');
-var savedSection = document.querySelector('.saved-covers-section');
+
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
@@ -32,12 +38,22 @@ savedSection.addEventListener('dblclick', deleteMiniCover);
 
 function newRandomCover() {
   currentCover = new Cover(covers[getRandomIndex(covers)], titles[getRandomIndex(titles)],
-    descriptors[getRandomIndex(descriptors)], descriptors[getRandomIndex(descriptors)]);
+  descriptors[getRandomIndex(descriptors)], descriptors[getRandomIndex(descriptors)]);
+
+  displayRandomCover();
+}
+
+function displayRandomCover() {
   coverImage.src = currentCover.cover;
   title.innerText = currentCover.title;
   descriptor1.innerText = currentCover.tagline1;
   descriptor2.innerText = currentCover.tagline2;
 }
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
 function makeYourOwn() {
   randomButton.classList.add('hidden');
   homeButton.classList.remove('hidden');
@@ -46,6 +62,7 @@ function makeYourOwn() {
   formView.classList.remove('hidden');
   savedView.classList.add('hidden');
 }
+
 function viewSavedCovers() {
   homeView.classList.add('hidden');
   formView.classList.add('hidden');
@@ -54,6 +71,7 @@ function viewSavedCovers() {
   saveCoverButton.classList.add('hidden');
   homeButton.classList.remove('hidden');
 }
+
 function goHome() {
   homeView.classList.remove('hidden');
   formView.classList.add('hidden');
@@ -64,6 +82,7 @@ function goHome() {
   viewSavedButton.classList.remove('hidden');
   makeNewButton.classList.remove('hidden');
 }
+
 function getNewBookData(event) {
   event.preventDefault();
   currentCover = new Cover(userCover.value, userTitle.value, userDesc1.value, userDesc2.value);
@@ -79,6 +98,7 @@ function getNewBookData(event) {
 
   goHome();
 }
+
 function addToSaved() {
   var coverString = currentCover.cover + currentCover.title + currentCover.tagline1 + currentCover.tagline2;
   for (let i = 0; i < savedCovers.length; i++) {
@@ -88,36 +108,42 @@ function addToSaved() {
     }
   }
   savedCovers.push(currentCover);
+  displayMiniCover();
+}
 
+function displayMiniCover() {
   var savedCover = document.createElement('section');
   var coverImage = document.createElement('img');
+  var price = document.createElement('img');
   var title = document.createElement('h2')
   var descriptors = document.createElement('h3');
-  var indexPosition = savedCovers.length - 1;
+  var lastElement = savedCovers.length - 1;
 
-  coverImage.src = savedCovers[savedCovers.length - 1].cover;
-  title.innerText = savedCovers[savedCovers.length - 1].title;
-  descriptors.innerText = `A tale of ${savedCovers[savedCovers.length - 1].tagline1} and ${savedCovers[savedCovers.length - 1].tagline2}`;
+  coverImage.src = savedCovers[lastElement].cover;
+  price.src = priceTag.src;
+  title.innerText = savedCovers[lastElement].title;
+  descriptors.innerText = `A tale of ${savedCovers[lastElement].tagline1} and ${savedCovers[lastElement].tagline2}`;
  
   savedSection.appendChild(savedCover);
   savedCover.appendChild(coverImage);
   savedCover.appendChild(title);
   savedCover.appendChild(descriptors);
+  savedCover.appendChild(price);
 
   savedCover.id = savedCovers[savedCovers.length - 1].id;
-  savedCover.classList.add("mini-cover");
-  coverImage.classList.add("mini-cover");
-  title.classList.add("cover-title");
-  descriptors.classList.add("tagline");
+  savedCover.classList.add('mini-cover');
+  coverImage.classList.add('mini-cover');
+  title.classList.add('cover-title');
+  descriptors.classList.add('tagline');
+  price.classList.add('price-tag');
 }
-function getRandomIndex(array) {
-  return Math.floor(Math.random() * array.length);
-}
+
 function deleteMiniCover(event) {
   var miniCoverSection = event.target.parentElement;
   miniCoverSection.remove();
+
   var miniCoverId = miniCoverSection.getAttribute('id');
-  console.log(typeof(miniCoverId));
+  
   for (var i = 0; i < savedCovers.length; i++) {
     if (savedCovers[i].id.toString() === miniCoverId) {
       savedCovers.splice(i, 1);
