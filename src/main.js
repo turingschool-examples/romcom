95585d41ea03dc8ab0872629cbc5930437a5862a
trevorfitz0 <trevorfitz0@gmail.com>
2022-12-06 22:52:02 -0700

    Random cover button that generates random numbers

--- src/main.js
+++ src/main.js
@@ -1,16 +1,32 @@
-// Create variables targetting the relevant DOM elements here 👇
-
+var coverRandom 
+var titleRandom
+var descriptorRandom1
+var descriptorRandom2
 
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-var currentCover;
+
+
+//var currentCover = new Cover();
+
+//document.getElementById("cover-image").src = currentCover.cover;
 
 // Add your event listeners here 👇
 
+var elements = document.getElementsByClassName("random-cover-button")
+elements[0].addEventListener('click', randomNumbers)
+
 
 // Create your event handlers and other functions here 👇
+function randomNumbers() {
+    coverRandom = (getRandomIndex(covers))
+    titleRandom = (getRandomIndex(titles))
+    descriptorRandom1 = (getRandomIndex(descriptors))
+    descriptorRandom2 = (getRandomIndex(descriptors))
+}
+
 
 
 // We've provided one function to get you started
