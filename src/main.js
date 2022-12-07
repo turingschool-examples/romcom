4bc45f0a14779a07b6650ef89d0288f44aae7d6b
Carol Bradsen <carolbradsen@gmail.com>
2022-12-06 19:27:44 -0700

    Add experiments with HTML and current cover

--- src/main.js
+++ src/main.js
@@ -1,11 +1,33 @@
 // Create variables targetting the relevant DOM elements here 👇
-
+// Pull up and display a new picture, headline, and tagline everytime we click "Show New Random Cover" button.
 
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-var currentCover;
+var currentCover = [
+  new CurrentCover("./assets/prairie.jpg", "Windswept Hearts", "passion", "pigs")
+]
+
+
+class Cover {
+  constructor(coverImgSrc, title, descriptor1, descriptor2) {
+    this.id = Date.now();
+    this.cover = coverImgSrc;
+    this.title = title;
+    this.tagline1 = descriptor1;
+    this.tagline2 = descriptor2;
+  }
+}
+
+<section class="main-cover">
+        <img class="cover-image" src="./assets/prairie.jpg">
+        <h2 class="cover-title">Windswept Hearts</h2>
+        <h3 class="tagline">A tale of <span class="tagline-1">passion</span> and <span class="tagline-2">woe</span></h3>
+        <img class="price-tag" src="./assets/price.png">
+        <img class="overlay" src="./assets/overlay.png"></img>
+
+
 
 // Add your event listeners here 👇
 
