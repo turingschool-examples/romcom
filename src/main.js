407737f0628bb7908d7859b72082028bb35d85ca
Travis Parks <travisparks0788@gmail.com>
2022-12-07 21:07:54 -0500

    feat createBook function

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
+var savedCovers = [
+  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
+];
+var currentCover;
+
+
+
 // Create variables targetting the relevant DOM elements here 👇
 var coverImgSrc = document.querySelector('.cover-image')
 var title = document.querySelector('.cover-title')
-
-var tagline = document.querySelector('.tagline')
-var tagline = document.querySelector('.tagline')
+var taglineOne = document.querySelector('.tagline-1')
+var taglineTwo = document.querySelector('.tagline-2')
 var priceTag = document.querySelector('.price-tag')
 
 var randomButtonCover = document.querySelector('.random-cover-button')
@@ -12,10 +18,7 @@ var viewSavedCover = document.querySelector('.view-saved-button')
 var makeNewCover = document.querySelector('.make-new-button')
 
 // We've provided a few variables below
-var savedCovers = [
-  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
-];
-var currentCover;
+
 
 // Add your event listeners here 👇
 
@@ -36,23 +39,29 @@ var currentCover;
 //   return Math.floor(Math.random() * array.length);
 // }
 
-function getRandomIndex(savedCovers) {
-  return Math.floor(Math.random() * savedCovers.length);
-}
 
 function createBook(){
-title.innerText = function getRandomIndex(title) {
-  return (title(math.floor(math.random() * title.length)))
+title.innerHTML = function getRandomIndex(titles) {
+  return (titles(math.floor(math.random() * titles.length)))
 }
 
 cover.src = function getRandomIndex(covers) {
-  return ()
+  return (covers(math.floor(math.random() * covers.length)))
+}
+
+taglineOne.innerHTML = function getRandomIndex(descriptors) {
+  return (descriptors(math.floor(math.random() * discriptors.length)))
+}
+
+  taglineTwo.innerHTML = function getRandomIndex(descriptors) {
+    return (descriptors(math.floor(math.random() * discriptors.length)))
+}
+
+
 }
 
 
 
-
-
-
-
-}
+// function getRandomIndex(savedCovers) {
+//   return Math.floor(Math.random() * savedCovers.length);
+// 
