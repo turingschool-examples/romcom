13bdb4d2141617468782d5df7c55812751558a9b
Sophie LaBelle <sophie.r.labelle@gmail.com>
2022-12-07 20:45:32 -0800

    Completed form view page

--- src/main.js
+++ src/main.js
@@ -1,26 +1,30 @@
 // Create variables targetting the relevant DOM elements here 👇
 var homeCover = document.querySelector('.cover-image')
-
 var homeTitle = document.querySelector('.cover-title')
-
 var homeTagline1 = document.querySelector('.tagline-1')
-
 var homeTagline2 = document.querySelector('.tagline-2')
-
-var randomButton = document.querySelector('.random-cover-button')
-
-var homeButton = document.querySelector('.home-button')
-
 var mainCover = document.querySelector('.main-cover')
 
+
+var homeButton = document.querySelector('.home-button')
+var randomButton = document.querySelector('.random-cover-button')
+var saveCoverButton = document.querySelector('.save-cover-button')
+var viewSavedButton = document.querySelector('.view-saved-button')
 var makeCoverButton = document.querySelector('.make-new-button')
 
-var hidden = document.querySelectorAll('.hidden')
+
+var userForm = document.querySelector('.form-view')
+var whatToShow = [homeButton, userForm]
+var whatToHide = [mainCover, randomButton, saveCoverButton]
+var showSavedCovers = [homeButton, ]
 
 // We've provided a few variables below-------------------------------
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
+savedCovers.classList.add('mini-cover')
+
+
 // var currentCover = new Cover(covers[getRandomIndex(covers)], titles[getRandomIndex(titles)], descriptors[getRandomIndex(descriptors)], descriptors[getRandomIndex(descriptors)])
 // console.log(currentCover)
 
@@ -33,8 +37,15 @@ window.addEventListener('load', generateNewCover)
 
 document.querySelector('.random-cover-button').addEventListener("click", generateNewCover)
 
+// --------------- Make new cover Button------------------------- 
 // Can probably use button var names instead of query selectors
-document.querySelector('.make-new-button').addEventListener("click", hideElements)
+// for make your own
+document.querySelector('.make-new-button').addEventListener("click", hideElements.bind(null, whatToHide))
+
+document.querySelector('.make-new-button').addEventListener("click", showElements.bind(null, whatToShow))
+
+// for saved cover
+document.querySelector('.view-saved-button').addEventListener("click", hideElements.bind(null, whatToHide))
 
 // document.querySelector('.random-cover-button').addEventListener("click", showCurrentCover)
 
@@ -55,12 +66,16 @@ function generateNewCover() {
 //   currentCover = new Cover(homeCover.src, homeTitle.innerText, homeTagline1.innerText, homeTagline2.innerText)
 // }
 
-function hideElements() {
-  mainCover.classList.add('hidden');
+function hideElements(hideElementsArray) {
+  for (var i = 0; i < hideElementsArray.length; i++) {
+    hideElementsArray[i].classList.add('hidden')
+  }
 }
 
-function showElements(parentElement) {
-  parentElement.classList.remove('.hidden');
+function showElements(showElementsArray) {
+  for (var i = 0; i < showElementsArray.length; i++) {
+    showElementsArray[i].classList.remove('hidden')
+  }
 }
 
 function test() {
