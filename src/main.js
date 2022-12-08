e2510172eea7d911a477729c226ea3653bbec966
Winston Calhoun <superdevant@gmail.com>
2022-12-07 20:25:32 -0600

    Add: working save cover button

--- src/main.js
+++ src/main.js
@@ -11,52 +11,28 @@ var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 
-var randomImageIndex = getRandomIndex(covers); // number between 0-27
-var randomTitleIndex = getRandomIndex(titles);
-var randomTag1Index = getRandomIndex(descriptors);
-var randomTag2Index = getRandomIndex(descriptors);
-
-var currentCover  = new Cover(covers[randomImageIndex], titles[randomTitleIndex], descriptors[randomTag1Index], descriptors[randomTag2Index]);
-
+var currentCover  = createRandomPoster()
 
 // Add your event listeners here 👇
 
-document.addEventListener('DOMContentLoaded', createRandomPoster);
 randomCoverBtn.addEventListener('click', createRandomPoster);
-
+document.addEventListener('DOMContentLoaded', createRandomPoster);
 
 // Create your event handlers and other functions here 👇
 
-
-
+function createRandomPoster() {
+  var randomImageIndex = getRandomIndex(covers);
+  var randomTitleIndex = getRandomIndex(titles);
+  var randomTag1Index = getRandomIndex(descriptors);
+  var randomTag2Index = getRandomIndex(descriptors);
+    var currentCover = new Cover(covers[randomImageIndex], titles[randomTitleIndex], descriptors[randomTag1Index], descriptors[randomTag2Index]);
+    coverImage.src = currentCover.cover;
+    coverTitle.innerText = currentCover.title;
+    coverTag1.innerText = currentCover.tagline1;
+    coverTag2.innerText = currentCover.tagline2;
+}
 
 // We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
-
-function createRandomPoster() {
-  coverImage.src = currentCover.cover
-  coverTitle.innerText = currentCover.title
-  coverTag1.innerText = currentCover.tagline1
-  coverTag2.innerText = currentCover.tagline2
-}
-
-
-
-
-
-
-
-// **Iteration 0 **
-// When the page loads, we should see a cover with a randomly selected image, title, and tagline which includes two random descriptors
-// -- We know we need an event listener to generate a random [moviePoster] as soon as the page loads
-// -- We know we need three functions(?)
-// -- We know we will need to interpolate the tagline1 and tagline2 properties
-// Every time the user clicks the Show New Random Cover button, a new random cover is created
-// -- We know we need an event listener to listen for when a user clicks the appropriate button - this click will call all of the random functions (image, title, tagline(s))
-//    hint: you may need to update the value of the provided currentCover variable
-//    hint: use that Cover class!
-// Every time the user clicks the Show New Random Cover button, the random cover is displayed
-// -- We need to push the randomly generated movieCover to the <main cover> class
-//    hint: you may need to create a function that displays information on the DOM
