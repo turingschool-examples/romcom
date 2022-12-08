efc8a6171aca5fe639d2476dc9d2644a013c6ffe
Travis Parks <travisparks0788@gmail.com>
2022-12-07 20:40:10 -0500

    feat newly selected queries

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 // Create variables targetting the relevant DOM elements here 👇
+var coverImgSrc = document.querySelector('.cover-image')
+var title = document.querySelector('.cover-title')
+var tagline = document.querySelector('.tagline')
+var tagline = document.querySelector('.tagline')
+var tagline = document.querySelector('.tagline')
+var priceTag = document.querySelector('.price-tag')
 
+var randomButtonCover = document.querySelector('.random-cover-button')
+var saveButtonCover = document.querySelector('.save-cover-button')
+var viewSavedCover = document.querySelector('.view-saved-button')
+var makeNewCover = document.querySelector('.make-new-button')
 
 // We've provided a few variables below
 var savedCovers = [
@@ -10,10 +20,39 @@ var currentCover;
 // Add your event listeners here 👇
 
 
+// var image = document.querySelector('#image');
+
+// image.addEventListener('click', function() {
+//     image.src = 'https://dirask.com/static/bucket/1633375165831-yjQ7G6WQeL--image.png';
+// });
+
+
 // Create your event handlers and other functions here 👇
 
 
 // We've provided one function to get you started
-function getRandomIndex(array) {
-  return Math.floor(Math.random() * array.length);
+
+// function getRandomIndex(array) {
+//   return Math.floor(Math.random() * array.length);
+// }
+
+function getRandomIndex(savedCovers) {
+  return Math.floor(Math.random() * savedCovers.length);
+}
+
+function createBook(){
+title.innerText = function getRandomIndex(title) {
+  return (title(math.floor(math.random() * title.length)))
+}
+
+cover.src = function getRandomIndex(covers) {
+  return ()
+}
+
+
+
+
+
+
+
 }
