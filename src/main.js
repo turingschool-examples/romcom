c239364faf8dc3d0cfd0620d36722e1ea1b7a24a
Jeff Sahim <jeffreysahim@gmail.com>
2022-12-07 22:20:23 -0500

    feat: Add functionality to Random Cover button

--- src/main.js
+++ src/main.js
@@ -5,26 +5,41 @@ var titleLocation = document.querySelector(".cover-title")
 var firstTagLocation = document.querySelector(".tagline-1")
 var secondTagLocation = document.querySelector(".tagline-2")
 
+var randomCoverButton = document.querySelector(".random-cover-button")
+
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
+
 var currentCover;
 
-var randomCoversIndex = getRandomIndex(covers);
-var randomTitlesIndex = getRandomIndex(titles);
-var randomDescriptorsIndex1 = getRandomIndex(descriptors);
-var randomDescriptorsIndex2 = getRandomIndex(descriptors);
-
-var randomCoverImage = covers[randomCoversIndex];
-var randomTitleText = titles[randomTitlesIndex];
-var randomDescriptor1 = descriptors[randomDescriptorsIndex1];
-var randomDescriptor2 = descriptors[randomDescriptorsIndex2];
 
 // Add your event listeners here 👇
 
+window.addEventListener("load", getRandomCover)
+randomCoverButton.addEventListener("click", getRandomCover)
 
 // Create your event handlers and other functions here 👇
+
+function getRandomCover(){
+  var randomCoversIndex = getRandomIndex(covers);
+  var randomTitlesIndex = getRandomIndex(titles);
+  var randomDescriptorsIndex1 = getRandomIndex(descriptors);
+  var randomDescriptorsIndex2 = getRandomIndex(descriptors);
+
+  var randomCoverImage = covers[randomCoversIndex]
+  var randomCoverTitle = titles[randomTitlesIndex]
+  var randomCoverDescriptor1 = descriptors[randomDescriptorsIndex1]
+  var randomCoverDescriptor2 = descriptors[randomDescriptorsIndex2]
+
+  coverLocation.src = randomCoverImage
+  titleLocation.innerText = randomCoverTitle
+  firstTagLocation.innerText = randomCoverDescriptor1
+  secondTagLocation.innerText = randomCoverDescriptor2
+
+  currentCover = new Cover(randomCoverImage, randomCoverTitle, randomCoverDescriptor1, randomCoverDescriptor2)
+}
 
 
 // We've provided one function to get you started
@@ -32,7 +47,4 @@ function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
 
-coverLocation.src = randomCoverImage
-titleLocation.innerText = randomTitleText
-firstTagLocation.innerText = randomDescriptor1
-secondTagLocation.innerText = randomDescriptor2
+
