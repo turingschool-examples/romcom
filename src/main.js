864b6d46faee824320c0bfde9942136c503bf737
Kelli Watkins <watkinskellil@gmail.com>
2022-12-08 16:49:18 -0700

    Re-format file

--- src/main.js
+++ src/main.js
@@ -3,108 +3,18 @@ var cover = document.querySelector('.cover-image')
 var taglineOne = document.querySelector('.tagline-1')
 var taglineTwo = document.querySelector('.tagline-2')
 var randomCoverButton = document.querySelector('.random-cover-button');
-var covers = [
-  './assets/bluebrocade.jpg',
-  './assets/dance.jpg', 
-  './assets/embrace.png',
-  './assets/fire.png',
-  './assets/frock.png',
-  './assets/glorious.jpg',
-  './assets/golden.jpg',
-  './assets/maskedmeeting.jpg',
-  './assets/masquerade.jpg',
-  './assets/moonlitmeadow.jpg',
-  './assets/office.png',
-  './assets/picnic.jpg',
-  './assets/pirate.jpg',
-  './assets/prairie.jpg',
-  './assets/redrenaissance.jpg',
-  './assets/regency.jpg',
-  './assets/ribbons.jpg',
-  './assets/roses.jpg',
-  './assets/ruffles.jpg',
-  './assets/scroll.jpg',
-  './assets/shine.png',
-  './assets/smolder.png',
-  './assets/snow.jpg',
-  './assets/sparkles.jpg',
-  './assets/stripes.png',
-  './assets/wildwest.jpg',
-  './assets/windswept.jpg',
-];
-var titles = [
-  "Passionate Moonlight",
-  "Love's Misery",
-  "Roses and Flame",
-  "Innocent Roses",
-  "Silk and Sense",
-  "Hearts Aflame",
-  "Fiery Passion",
-  "Stolen Hearts",
-  "Secrets and Silk",
-  "Dreams of Fire",
-  "Lovers and Enemies",
-  "Passion's Embrace",
-  "Harbinger by Moonlight",
-  "Rouge Red",
-  "Moonlit Mysteries",
-  "Crimson Roses",
-  "Destiny's Fires",
-  "Proposals and Passion",
-  "Silk Wedding",
-  "Masked Seduction",
-  "Crimson Masquerade",
-  "Stolen Rubies",
-  "Emerald Eyes",
-  "Sapphire Skies",
-  "Opal Passions"
-];
-var descriptors = [
-  "passion",
-  "glory",
-  "romance",
-  "woe",
-  "sorrow",
-  "pain",
-  "ardor",
-  "devotion",
-  "excitement",
-  "fervor",
-  "rage",
-  "spirit",
-  "zeal",
-  "ecstasy",
-  "fire",
-  "storms",
-  "tempests",
-  "rapture",
-  "ire",
-  "jealousy",
-  "exhilaration",
-  "bliss",
-  "enchantment",
-  "paradise",
-  "calamity",
-  "disaster",
-  "heartache",
-  "misfortune",
-  "agony",
-  "curses",
-  "blessings",
-  "melancholy"
-];
+
+window.addEventListener('load', createBook)
+randomCoverButton.addEventListener('click', createBook)
+
 function createBook() {
-  console.log("shirt")
+  currentCover = new Cover(cover.src, title.innerText, descriptor1.innerText, descriptor2.innerText)
   title.innerText = titles[getRandomIndex(titles)]
   cover.src = covers[getRandomIndex(covers)]
   taglineOne.innerText = descriptors[getRandomIndex(descriptors)]
   taglineTwo.innerText = descriptors[getRandomIndex(descriptors)]
-  currentCover = new Cover(cover.src, title.innerText, descriptor1.innerText, descriptor2.innerText)
  }
 
-
-
-// Create variables targetting the relevant DOM elements here 👇
 
 
 // We've provided a few variables below
@@ -115,8 +25,7 @@ var currentCover;
 
 
 // Add your event listeners here 👇
-window.addEventListener('load', createBook)
-randomCoverButton.addEventListener('click', createBook)
+
 // Create your event handlers and other functions here 👇
 
 
