b0d384d0c9aab53402ed1100ca12e1fe4d99f289
Sophie LaBelle <sophie.r.labelle@gmail.com>
2022-12-07 16:10:23 -0800

    Add new event listener and new function

--- src/main.js
+++ src/main.js
@@ -14,22 +14,31 @@ console.log(homeCover)
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-var currentCover = new Cover(covers[getRandomIndex(covers)], titles[getRandomIndex(titles)], descriptors[getRandomIndex(descriptors)], descriptors[getRandomIndex(descriptors)])
-console.log(currentCover)
+// var currentCover = new Cover(covers[getRandomIndex(covers)], titles[getRandomIndex(titles)], descriptors[getRandomIndex(descriptors)], descriptors[getRandomIndex(descriptors)])
+// console.log(currentCover)
+
+var currentCover = new Cover(homeCover.src, homeTitle.innerText, homeTagline1.innerText, homeTagline2.innerText) 
 
 
 // Add your event listeners here 👇
+document.querySelector('.random-cover-button').addEventListener("click", generateNewCover)
+
 document.querySelector('.random-cover-button').addEventListener("click", showCurrentCover)
+
 
 // Create your event handlers and other functions here 👇
 
 
 // show the cover we generated
-function showCurrentCover() {
-  homeCover.innerHTML = covers[getRandomIndex(covers)]
-  homeTitle.innerHTML = titles[getRandomIndex(titles)]
-  homeTagline1.innerHTML = descriptors[getRandomIndex(descriptors)]
+function generateNewCover() {
+  homeCover.src = covers[getRandomIndex(covers)]
+  homeTitle.innerText = titles[getRandomIndex(titles)]
+  homeTagline1.innerText = descriptors[getRandomIndex(descriptors)]
   homeTagline2.innerHTML = descriptors[getRandomIndex(descriptors)]
+}
+
+function showCurrentCover() {
+  currentCover = new Cover(homeCover.src, homeTitle.innerText, homeTagline1.innerText, homeTagline2.innerText)
 }
 
 // We've provided one function to get you started
