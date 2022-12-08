2a205542af2a125b40aa949c9977b853d5790900
Shane Misra <shane.misra@gmail.com>
2022-12-07 17:05:17 -0700

    Generate random cover when loading, including text

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 // Create variables targetting the relevant DOM elements here 👇
 var coverImage = document.querySelector('.cover-image')
-
+var coverTitle = document.querySelector('.cover-title')
+var descriptor1 = document.querySelector('.tagline-1')
+var descriptor2 = document.querySelector('.tagline-2')
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
@@ -20,6 +22,9 @@ function getRandomIndex(array) {
 
 function getRandomCover() {
   coverImage.src = covers[getRandomIndex(covers)]
+  coverTitle.innerText = titles[getRandomIndex(titles)]
+  descriptor1.innerText = descriptors[getRandomIndex(descriptors)]
+  descriptor2.innerText = descriptors[getRandomIndex(descriptors)]
 }
 
 //psuedocode here:
