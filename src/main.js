2772716129fe3456741f12249f55f1d101b1e3e1
Joshua Martin <ursa8minority@gmail.com>
2022-12-07 19:07:35 -0700

    feat new functions added

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
+var savedCovers = [
+  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
+];
+var currentCover = 
+
 // Create variables targetting the relevant DOM elements here 👇
   
-  var coverImgSrc = document.querySelector('.cover-image');
-  var title = document.querySelector('cover-title');
-  var tagLine = document.querySelector('tagline-1');
+  var cover = document.querySelector('.cover-image');
+  var titles = document.querySelector('cover-title');
+  var tagLine1 = document.querySelector('tagline-1');
   var priceTag = document.querySelector('price-tag') 
   
   var randomCoverButton = document.querySelector('random-cover-button');
@@ -10,11 +15,6 @@
   var makeNewCover = document.querySelector('make-new-button')
   var tagLine2 = document.querySelector('tagline-2')
 
-// We've provided a few variables below
-var savedCovers = [
-  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
-];
-var currentCover = 
 
 //Add your event listeners here 👇
 
@@ -29,7 +29,17 @@ image.addEventListener ('click', function() {
 
 // We've provided one function to get you started
 function createBook() {
-  title.innerText = function getRandomIndex(titles) {
-    return (titles[Math.floor(Math.random() * titles.length)])  
-}
+  titles.innerHTML = function getRandomIndex(titles) {
+    return (titles[Math.floor(Math.random() * titles.length)]);  
   }
+cover.src = function getRandomIndex(covers) {
+  return (covers[Math.floor(Math.random() * covers.length)]);
+  }
+tagLine1.innerHTML = function getRandomIndex(tagLine1) {
+  return (tagLine1[Math.floor(Math.random() * tagLine1.length)]);
+}
+tagLine2.innerHTML = function getRandomIndex(tagLine2) {
+  return (tagLine1[Math.floor(Math.random() * tagLine2.length)]);
+}
+currentCover = new Cover(cover.src, titles.innerHTML, descriptor1.innerHTML, descriptor2.innerHTML) )
+}
