644176db065cc8c326ed7e4334aa40b2d545f9bc
Joe Fogiato <joe.fogiato@gmail.com>
2022-12-08 17:56:57 -0500

    Refactor: Iteration 0 generate/push random cover function

--- src/main.js
+++ src/main.js
@@ -3,36 +3,49 @@ var coverImage = document.querySelector('.cover-image');
 var coverTitle = document.querySelector('.cover-title');
 var coverTag1 = document.querySelector('.tagline-1');
 var coverTag2 = document.querySelector('.tagline-2');
+
+// Button variables 👇
 var randomCoverBtn = document.querySelector('.random-cover-button');
 
 
-// We've provided a few variables below
+// We've provided a few variables below 👇
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 
-var currentCover  =
+var currentCover;
 
 // Add your event listeners here 👇
 
-randomCoverBtn.addEventListener('click', createRandomPoster);
-document.addEventListener('DOMContentLoaded', createRandomPoster);
+randomCoverBtn.addEventListener('click', function() {
+  generateRandomPoster()
+  pushRandomCover()
+});
+document.addEventListener('DOMContentLoaded', function() {
+  generateRandomPoster()
+  pushRandomCover()
+});
 
 // Create your event handlers and other functions here 👇
 
-function createRandomPoster() {
+function generateRandomPoster(){
   var randomImageIndex = getRandomIndex(covers);
   var randomTitleIndex = getRandomIndex(titles);
   var randomTag1Index = getRandomIndex(descriptors);
   var randomTag2Index = getRandomIndex(descriptors);
   currentCover = new Cover(covers[randomImageIndex], titles[randomTitleIndex], descriptors[randomTag1Index], descriptors[randomTag2Index]);
+};
+
+function pushRandomCover() {
   coverImage.src = currentCover.cover;
   coverTitle.innerText = currentCover.title;
   coverTag1.innerText = currentCover.tagline1;
   coverTag2.innerText = currentCover.tagline2;
-}
+};
 
-// We've provided one function to get you started
+
+
+// We've provided one function to get you started 👇
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
