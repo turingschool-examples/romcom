279b29f75f993cbf1159a4a53c32d9032a7d4c7b
Chrissy Cooper <cooper.christine.r@gmail.com>
2022-12-07 22:19:45 -0600

    Add descriptor querySelectors for random descriptors on refresh

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ var randomCover = covers[randomCoverIndex];
 var randomTitleIndex = getRandomIndex(titles);
 var randomTitle = titles[randomTitleIndex];
 // console.log(new Cover(randomCover, randomTitle, randomDescriptor1, randomDescriptor2))
+
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
@@ -20,8 +21,7 @@ var currentCover;
 //on click - create a new random instance of the cover class
 
 var button = document.querySelector('.random-cover-button');
-button.addEventListener('click', createNewCover())
-// functionName would be a function we write that would
+button.addEventListener('', createNewCover())         //this is not working as we expect, it's running on refresh
 
 
 // Create your event handlers and other functions here 👇
@@ -31,9 +31,17 @@ function createNewCover() {
 
 // var cover = document.querySelector(".cover-image");
 // console.log(cover.innerHTML)
+
 var myTitle = document.querySelector(".cover-title")
 myTitle.innerText = currentCover.title
-console.log(myTitle.innerText)
+
+var myTagLine1 = document.querySelector(".tagline-1")
+myTagLine1.innerText = currentCover.tagline1
+console.log(myTagLine1.innerText)
+
+var myTagLine2 = document.querySelector(".tagline-2")
+myTagLine2.innerText = currentCover.tagline2
+console.log(myTagLine2.innerText)
 
 
 // We've provided one function to get you started
