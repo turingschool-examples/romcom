daa0602da1b366e6724784b6fd9fd94d1d394864
Shane Misra <shane.misra@gmail.com>
2022-12-07 18:05:48 -0700

    Implement form button

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ var coverTitle = document.querySelector('.cover-title')
 var descriptor1 = document.querySelector('.tagline-1')
 var descriptor2 = document.querySelector('.tagline-2')
 var randButton = document.querySelector('.random-cover-button')
+var makeOwnCoverButton = document.querySelector('.make-new-button')
+var coverData = document.querySelector('.home-view')
+var formData = document.querySelector('.form-view')
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
@@ -13,6 +16,7 @@ var currentCover = new Cover(coverImage, coverTitle, descriptor1, descriptor2)
 // Add your event listeners here 👇
 window.addEventListener('load', getRandomCover)
 randButton.addEventListener('click', getRandomCover)
+makeOwnCoverButton.addEventListener('click', switchToForm)
 
 // Create your event handlers and other functions here 👇
 
@@ -27,6 +31,10 @@ function getRandomCover() {
   coverTitle.innerText = titles[getRandomIndex(titles)]
   descriptor1.innerText = descriptors[getRandomIndex(descriptors)]
   descriptor2.innerText = descriptors[getRandomIndex(descriptors)]
+}
+function switchToForm() {
+  coverData.classList.add('hidden')
+  formData.classList.remove('hidden')
 }
 
 //psuedocode here:
