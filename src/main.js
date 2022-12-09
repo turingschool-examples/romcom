ce1526a4a0df8e36df81e71f8c90595c809ae813
Tom Doder <tdoder89@gmail.com>
2022-12-08 21:12:56 -0600

    feat work on iteration 0

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
+var currentCover;
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-var currentCover;
+
 
 
 
 // Create variables targetting the relevant DOM elements here 👇
-var coverImgSrc = document.querySelector('.cover-image')
+var cover = document.querySelector('.cover-image')
 var title = document.querySelector('.cover-title')
 var taglineOne = document.querySelector('.tagline-1')
 var taglineTwo = document.querySelector('.tagline-2')
@@ -20,9 +21,15 @@ var makeNewCover = document.querySelector('.make-new-button')
 // We've provided a few variables below
 
 
+
+title.innerText = titles[getRandomIndex(titles)]
+cover.src = covers[getRandomIndex(covers)]
+taglineOne.innerText = covers[getRandomIndex(descriptors)]
+taglineTwo.innerText = covers[getRandomIndex(descriptors)]
+//titles[0]
 // Add your event listeners here 👇
 
-
+// window.addEventListener("load", (event)
 // var image = document.querySelector('#image');
 
 // image.addEventListener('click', function() {
@@ -35,29 +42,28 @@ var makeNewCover = document.querySelector('.make-new-button')
 
 // We've provided one function to get you started
 
-// function getRandomIndex(array) {
-//   return Math.floor(Math.random() * array.length);
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+
+// function getRandomTitles(titles) {
+//   return (Math.floor(Math.random() * titles.length))
 // }
 
+// function getRandomCovers(covers) {
+//   return (Math.floor(Math.random() * covers.length))
+// }
 
+// function getRandomDescriptors(descriptors) {
+//   return (Math.floor(Math.random() * descriptors.length))
+// }
+
+// function getRandomIndex(descriptors) {
+//   return (descriptors(math.floor(math.random() * discriptors.length)))
+// }
 function createBook(){
-title.innerHTML = function getRandomIndex(titles) {
-  return (titles(math.floor(math.random() * titles.length)))
-}
-
-cover.src = function getRandomIndex(covers) {
-  return (covers(math.floor(math.random() * covers.length)))
-}
-
-taglineOne.innerHTML = function getRandomIndex(descriptors) {
-  return (descriptors(math.floor(math.random() * discriptors.length)))
-}
-
-  taglineTwo.innerHTML = function getRandomIndex(descriptors) {
-    return (descriptors(math.floor(math.random() * discriptors.length)))
-}
-
-
+  currentCover = new Cover(cover.src, title.innerText, descriptor1.innerText, descriptor2.innerText)
 }
 
 
