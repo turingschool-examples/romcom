d3ac0949d8e0548394f08cc951620a70b253cae9
Kass Daniello <zertroz@gmail.com>
2022-12-08 22:02:29 -0500

    feat: make saved covers and form button work

--- src/main.js
+++ src/main.js
@@ -8,7 +8,9 @@ var coverImage = document.querySelector(".cover-image")
 var coverTitle = document.querySelector(".cover-title")
 var tag1 = document.querySelector(".tagline-1")
 var tag2 = document.querySelector(".tagline-2")
-
+var viewForm = document.querySelector(".form-view")
+var viewHome = document.querySelector(".home-view")
+var viewSaved = document.querySelector(".saved-view")
 
 // We've provided a few variables below
 var savedCovers = [
@@ -20,7 +22,9 @@ var currentCover;
 
 window.addEventListener('load', createRandom)
 randomButton.addEventListener('click', createRandom)
-makeCoverButton.addEventListener('click', makeCover)
+makeCoverButton.addEventListener('click', makeCoverToggle)
+viewSavedButton.addEventListener('click', savedCoversToggle)
+homeButton.addEventListener('click', homeButtonToggle)
 
 // Create your event handlers and other functions here 👇
 
@@ -31,10 +35,25 @@ function createRandom(){
  tag2.innerText = descriptors[getRandomIndex(descriptors)]
 }
 
-function makeCover() {
+function makeCoverToggle() {
   saveCoverButton.classList.toggle("hidden")
   randomButton.classList.toggle("hidden")
   homeButton.classList.toggle("hidden")
+  viewHome.classList.toggle("hidden")
+  viewForm.classList.toggle("hidden")
+}
+
+function savedCoversToggle() {
+  saveCoverButton.classList.toggle("hidden")
+  randomButton.classList.toggle("hidden")
+  homeButton.classList.toggle("hidden")
+  viewHome.classList.toggle("hidden")
+  viewSaved.classList.toggle('hidden')
+}
+
+function homeButtonToggle() {
+    makeCoverToggle()
+    savedCoversToggle()
 }
 
 // We've provided one function to get you started
