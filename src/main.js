0dc6eaafce7b0d670ec1d1c608358cede3db14f7
Dustin Gouner <dustingouner20@gmail.com>
2022-12-08 19:50:52 -0700

    Feature load random cover

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 // Create variables targetting the relevant DOM elements here 👇
+
+
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 
-var currentCover = new Cover(cover.src, titles.innerHTML, tagLine1.innerHTML, tagLine2.innerHTML)
+var currentCover;
 
 
 
-var cover = document.querySelector('.cover-image')
-var title = document.querySelector('.cover-title')
+var coverImage = document.querySelector('.cover-image')
+var coverTitle = document.querySelector('.cover-title')
+var tagline = document.querySelector('.tagline')
 var taglineOne = document.querySelector('.tagline-1')
 var taglineTwo = document.querySelector('.tagline-2')
 var priceTag = document.querySelector('.price-tag')
@@ -21,10 +24,10 @@ var makeNewCover = document.querySelector('.view-saved-button')
 var makenewCover = document.querySelector('.make-new-button')
 
 // We've provided a few variables below
-var savedCovers = [
-  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
-];
-var currentCover;
+// var savedCovers = [
+//   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
+// ];
+// var currentCover;
 
 // var cover above is to test branch
 // Add your event listeners here 👇
@@ -33,31 +36,30 @@ var currentCover;
 
 // Create your event handlers and other functions here 👇
 
+window.addEventListener('load', displayRandomCover);
 
 // We've provided one function to get you started
 
-function createBook() {
-  title.innerHTML = function getRandomIndex(titles) {
-    return (titles(Math.floor(Math.random() * titles.length)))
-  }
-  cover.src = function getRandomIndex(covers) {
-  return (covers(Math.floor(Math.random() * covers.length)))
-  }
-  taglineOne.innerHTML = function getRandomIndex(descriptors){
-    return (descriptors(Math.floor(Math.random() * descriptors.length)))
-  }
-  taglineTwo.innerHTML = function getRandomIndex(descriptors){
-    return (descriptors(Math.floor(Math.random() * descriptors.length)))
-  }
-}
-
-
-
-
-function getRandomIndex(titles) {
+function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
 
-// original
+function displayRandomCover() {
+  console.log('hey this function is firing')
+  makeCover();
+  coverImage.src = currentCover.cover;
+  coverTitle.innerText = currentCover.title;
+  taglineOne.innerText = currentCover.tagline1;
+  taglineTwo.innerText = currentCover.tagline2
+
+}
+
+function makeCover() {
+  currentCover = new Cover(covers[getRandomIndex(covers)], titles[getRandomIndex(titles)], descriptors[getRandomIndex(descriptors)], descriptors[getRandomIndex(descriptors)]);
+}
+
+
+
+
 
 
