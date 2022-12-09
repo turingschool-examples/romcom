ca38fa71e6796747e9c7c3a6ee2ca3f52737415b
Christopher Baum <christopherabaum@gmail.com>
2022-12-08 18:00:06 -0600

    Refactor all main.js code

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-// Create variables targetting the relevant DOM elements here 👇
-var homeCover = document.querySelector('.cover-image')
+var homeImage = document.querySelector('.cover-image')
 var homeTitle = document.querySelector('.cover-title')
 var homeTagline1 = document.querySelector('.tagline-1')
 var homeTagline2 = document.querySelector('.tagline-2')
-var mainCover = document.querySelector('.main-cover')
+var homeMainCover = document.querySelector('.main-cover')
+var homeCover = new Cover(homeImage.src, homeTitle.innerText, homeTagline1.innerText, homeTagline2.innerText) 
 
 
 var homeButton = document.querySelector('.home-button')
@@ -12,72 +12,65 @@ var saveCoverButton = document.querySelector('.save-cover-button')
 var viewSavedButton = document.querySelector('.view-saved-button')
 var makeCoverButton = document.querySelector('.make-new-button')
 
+
 var savedCoversSection = document.querySelector('.saved-covers-section')
-savedCoversSection.innerHTML = `<h2> ${'Windswept Hearts'} </h2>`
+        savedCoversSection.innerHTML = "<h2> Windswept Hearts </h2>"
 
 
 var userForm = document.querySelector('.form-view')
-var whatToShow = [homeButton, userForm]
-var whatToHide = [mainCover, randomButton, saveCoverButton]
-var showSavedCovers = [homeButton, savedCovers]
 
-// We've provided a few variables below-------------------------------
+
+var whatToShow = [homeButton, userForm]
+var whatToHide = [homeMainCover, randomButton, saveCoverButton]
+
+
+var savedCoverPageElements = [homeButton, savedCovers]
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 
 
-// var currentCover = new Cover(covers[getRandomIndex(covers)], titles[getRandomIndex(titles)], descriptors[getRandomIndex(descriptors)], descriptors[getRandomIndex(descriptors)])
-// console.log(currentCover)
-
-var currentCover = new Cover(homeCover.src, homeTitle.innerText, homeTagline1.innerText, homeTagline2.innerText) 
-
-
 // Add your event listeners here -----------------------------------👇
 window.addEventListener('load', generateNewCover)
-// window.addEventListener('load', showCurrentCover)
-
-document.querySelector('.random-cover-button').addEventListener("click", generateNewCover)
+randomButton.addEventListener("click", generateNewCover)
 
 // --------------- Make new cover Button------------------------- 
-// Can probably use button var names instead of query selectors
 // for make your own
-document.querySelector('.make-new-button').addEventListener("click", hideElements.bind(null, whatToHide))
+makeCoverButton.addEventListener("click", activateCoverButton)
 
-document.querySelector('.make-new-button').addEventListener("click", showElements.bind(null, whatToShow))
 
 // for saved cover
-document.querySelector('.view-saved-button').addEventListener("click", hideElements.bind(null, whatToHide))
-
-document.querySelector('.view-saved-button').addEventListener("click", showElements.bind(null, showSavedCovers))
-
-// document.querySelector('.random-cover-button').addEventListener("click", showCurrentCover)
+viewSavedButton.addEventListener("click", activateViewSavedButton)
 
 
 // Create your event handlers and other functions here------------- 👇
-
-
-// show the cover we generated
 function generateNewCover() {
-  homeCover.src = covers[getRandomIndex(covers)]
+  homeImage.src = covers[getRandomIndex(covers)]
   homeTitle.innerText = titles[getRandomIndex(titles)]
   homeTagline1.innerText = descriptors[getRandomIndex(descriptors)]
   homeTagline2.innerText = descriptors[getRandomIndex(descriptors)]
-  currentCover = new Cover(homeCover.src, homeTitle.innerText, homeTagline1.innerText, homeTagline2.innerText)
+  homeCover = new Cover(homeImage.src, homeTitle.innerText, homeTagline1.innerText, homeTagline2.innerText)
 }
-// function showCurrentCover() {
-//   currentCover = new Cover(homeCover.src, homeTitle.innerText, homeTagline1.innerText, homeTagline2.innerText)
-// }
 
-function hideElements(hideElementsArray) {
-  for (var i = 0; i < hideElementsArray.length; i++) {
-    hideElementsArray[i].classList.add('hidden')
+function activateCoverButton() {
+  hideElements(whatToHide)
+  showElements(whatToShow)
+}
+
+function activateViewSavedButton() {
+  hideElements(whatToHide)
+  showElements(savedCoverPageElements)
+}
+
+function hideElements(elementsToHide) {
+  for (var i = 0; i < elementsToHide.length; i++) {
+    elementsToHide[i].classList.add('hidden')
   }
 }
 
-function showElements(showElementsArray) {
-  for (var i = 0; i < showElementsArray.length; i++) {
-    showElementsArray[i].classList.remove('hidden')
+function showElements(elementsToShow) {
+  for (var i = 0; i < elementsToShow.length; i++) {
+    elementsToShow[i].classList.remove('hidden')
   }
 }
 
@@ -88,5 +81,3 @@ function getRandomIndex(array) {
 function test() {
   console.log('Is this working?')
 }
-
-
