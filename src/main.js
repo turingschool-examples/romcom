2ebba2160ad99d9113521857b139815e83614e58
Trey Marshall <marshall.trey92@yahoo.com>
2022-12-08 21:10:05 -0600

    Complete Iteration 2

--- src/main.js
+++ src/main.js
@@ -6,20 +6,26 @@ var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 var currentCover; // make new object instance every created poster (for savin')
-var homeButton = document.querySelector('.home-button')
+var homeButton = document.querySelector('.home-button');
 var randomCoverButton = document.querySelector('.random-cover-button');
 var saveCoverButton = document.querySelector('.save-cover-button');
-var viewSavedCoverButton = document.querySelector('.view-saved-button')
+var viewSavedCoverButton = document.querySelector('.view-saved-button');
 var yourOwnCoverButton = document.querySelector('.make-new-button');
+var makeMyBookButton = document.querySelector('.create-new-book-button');
 
 var newRandomCover = document.querySelector('.cover-image');
 var newRandomTitle = document.querySelector('.cover-title');
 var newRandomTagOne = document.querySelector('.tagline-1');
 var newRandomTagTwo = document.querySelector('.tagline-2');
 
-var homeView = document.querySelector('.home-view')
-var formView = document.querySelector('.form-view')
-var savedView = document.querySelector('.saved-view')
+var userCover = document.querySelector('.user-cover');
+var userTitle = document.querySelector('.user-title');
+var descriptorOne = document.querySelector('.user-desc1');
+var descriptorTwo = document.querySelector('.user-desc2');
+
+var homeView = document.querySelector('.home-view');
+var formView = document.querySelector('.form-view');
+var savedView = document.querySelector('.saved-view');
 
 
 // Add your event listeners here 👇
@@ -31,7 +37,10 @@ randomCoverButton.addEventListener('click', getRandomCover);
 
 yourOwnCoverButton.addEventListener('click', changeViewToForm);
 
-viewSavedCoverButton.addEventListener('click', viewSavedCovers)
+viewSavedCoverButton.addEventListener('click', viewSavedCovers);
+
+makeMyBookButton.addEventListener('click', makeMyBook);
+
 
 // Create your event handlers and other functions here 👇
 // We've provided one function to get you started
@@ -39,14 +48,30 @@ function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 };
 
-function viewSavedCovers(){
+function makeMyBook(event) {
+  var userBook = new Cover(userCover.value, userTitle.value, descriptorOne.value, descriptorTwo.value)
+  event.preventDefault()
+  covers.push(userCover.value)
+  titles.push(userTitle.value)
+  descriptors.push(descriptorOne.value)
+  descriptors.push(descriptorTwo.value)
+  homeView.classList.remove('hidden')
+  formView.classList.add('hidden')
+  // savedCovers.push(userBook)
+  newRandomCover.src = userBook.cover
+  newRandomTitle.innerText = userBook.title
+  newRandomTagOne.innerText = userBook.tagline1
+  newRandomTagTwo.innerText = userBook.tagline2
+  };
+
+function viewSavedCovers() {
   savedView.classList.remove('hidden')
   formView.classList.add('hidden')
   homeView.classList.add('hidden')
   randomCoverButton.classList.add('hidden')
-  saveCoverButton.classList.add('hidden') 
+  saveCoverButton.classList.add('hidden')
   homeButton.classList.remove('hidden')
-}
+};
 
 function viewHome() {
   homeButton.classList.add('hidden')
@@ -56,19 +81,19 @@ function viewHome() {
   saveCoverButton.classList.remove('hidden')
 };
 
-function getRandomCover(){
-newRandomCover.src = covers[getRandomIndex(covers)];
-newRandomTitle.innerText = titles[getRandomIndex(titles)];
-newRandomTagOne.innerText = descriptors[getRandomIndex(descriptors)];
-newRandomTagTwo.innerText = descriptors[getRandomIndex(descriptors)];
+function getRandomCover() {
+newRandomCover.src = covers[getRandomIndex(covers)]
+newRandomTitle.innerText = titles[getRandomIndex(titles)]
+newRandomTagOne.innerText = descriptors[getRandomIndex(descriptors)]
+newRandomTagTwo.innerText = descriptors[getRandomIndex(descriptors)]
 };
 
-function changeViewToForm(){
+function changeViewToForm() {
   homeButton.classList.remove('hidden')
   formView.classList.remove('hidden')
   homeView.classList.add('hidden')
   randomCoverButton.classList.add('hidden')
   saveCoverButton.classList.add('hidden')
-
 };
+
 
