de2a4fd98e8c501b54c7669a05f77c3f3c3056b0
Amber Shipley <ashipley2@me.com>
2022-12-09 16:22:11 -0800

    feat: Show saved covers on saved covers view

--- src/main.js
+++ src/main.js
@@ -18,6 +18,7 @@ var coverInput = document.querySelector('.user-cover');
 var titleInput = document.querySelector('.user-title');
 var descriptor1Input = document.querySelector('.user-desc1');
 var descriptor2Input = document.querySelector('.user-desc2');
+var savedCoversSection = document.querySelector('.saved-covers-section');
 
 // We've provided a few variables below
 var savedCovers = [
@@ -38,8 +39,17 @@ saveCoverButton.addEventListener('click', handleSaveCoverButton);
 function handleSaveCoverButton() {
   if (!savedCovers.includes(currentCover)) {
     savedCovers.push(currentCover);
+    savedCoversSection.innerHTML = ''
+    for (var i = 0; i < savedCovers.length; i++) {
+      savedCoversSection.innerHTML += `<section class="mini-cover">
+      <img class="cover-image" src=${savedCovers[i].cover}>
+      <h2 class="cover-title">${savedCovers[i].title}</h2>
+      <h3 class="tagline">A tale of <span class="tagline-1">${savedCovers[i].tagline1}</span> and <span class="tagline-2">${savedCovers[i].tagline2}</span></h3>
+      <img class="price-tag" src="./assets/price.png">
+      <img class="overlay" src="./assets/overlay.png">
+    </section>`
+    }
   }
-  //when view saved cover button is clicked, the saved covers array will show
 }
 
 function handlePageLoad() {
