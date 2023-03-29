0840b4af0497218182411311ee04098063d82e9e
Fernando Robles <fernandorobles97@yahoo.com>
2023-03-28 23:53:31 -0500

    add random cover function

--- src/main.js
+++ src/main.js
@@ -1,14 +1,35 @@
 // Create variables targetting the relevant DOM elements here 👇
-var newCover;
+var titlesIndex = getRandomIndex(titles)
+var taglineIndex = getRandomIndex(descriptors)
+var taglineIndex2 = getRandomIndex(descriptors)
+var coverIndex = getRandomIndex(covers)
+var title = document.querySelector('h2');
+var tagline = document.querySelector('h3');
+var mainCover = document.querySelector('img')
+var randomCoverButton = document.querySelector('.random-cover-button')
 
 // We've provided a few variables below
 var savedCovers = [
   createCover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-var currentCover;
+var currentCover = {
+  id: Date.now(),
+  coverImg: covers[coverIndex],
+  title: titles[titlesIndex],
+  tagline1: descriptors[taglineIndex],
+  tagline2: descriptors[taglineIndex2]
+}
 
-// Add your event listeners here 👇
+function randomCover() {
+  title.innerHTML = currentCover.title;
+  mainCover.src = currentCover.coverImg;
+  tagline.innerHTML = `A tale of ${currentCover.tagline1} and ${currentCover.tagline2}`
+}
 
+randomCover(currentCover);
+
+// Add your event listeners here 
+randomCoverButton.addEventListener("click", randomCover)
 
 // Create your event handlers and other functions here 👇
 
@@ -28,3 +49,4 @@ function createCover(imgSrc, title, descriptor1, descriptor2) {
   }
   return cover
 }
+
