589b3980730d51da43521cd8c6be421cadfe82e8
Denzel Turambi <dturambi26@gmail.com>
2023-03-29 17:42:42 -0600

    Complete iteration 0

--- src/main.js
+++ src/main.js
@@ -2,11 +2,104 @@
 var coverImage = document.querySelector('.cover-image');
 var coverTitle = document.querySelector('.cover-title');
 var tagline = document.querySelector('.tagline');
+var taglineOne = document.querySelector('.tagline-1');
+var taglineTwo = document.querySelector('.tagline-2');
 var priceTag = document.querySelector('.price-tag');
 var overlay = document.querySelector('.overlay');
 var randomButton = document.querySelector('.random-cover-button')
 
-var {covers, titles, descriptors} = require('data.js');
+// var {covers, titles, descriptors} = require('./src/data');
+// data goes here:
+var covers = [
+  './assets/bluebrocade.jpg',
+  './assets/dance.jpg', 
+  './assets/embrace.png',
+  './assets/fire.png',
+  './assets/frock.png',
+  './assets/glorious.jpg',
+  './assets/golden.jpg',
+  './assets/maskedmeeting.jpg',
+  './assets/masquerade.jpg',
+  './assets/moonlitmeadow.jpg',
+  './assets/office.png',
+  './assets/picnic.jpg',
+  './assets/pirate.jpg',
+  './assets/prairie.jpg',
+  './assets/redrenaissance.jpg',
+  './assets/regency.jpg',
+  './assets/ribbons.jpg',
+  './assets/roses.jpg',
+  './assets/ruffles.jpg',
+  './assets/scroll.jpg',
+  './assets/shine.png',
+  './assets/smolder.png',
+  './assets/snow.jpg',
+  './assets/sparkles.jpg',
+  './assets/stripes.png',
+  './assets/wildwest.jpg',
+  './assets/windswept.jpg',
+];
+var titles = [
+  "Passionate Moonlight",
+  "Love's Misery",
+  "Roses and Flame",
+  "Innocent Roses",
+  "Silk and Sense",
+  "Hearts Aflame",
+  "Fiery Passion",
+  "Stolen Hearts",
+  "Secrets and Silk",
+  "Dreams of Fire",
+  "Lovers and Enemies",
+  "Passion's Embrace",
+  "Harbinger by Moonlight",
+  "Rouge Red",
+  "Moonlit Mysteries",
+  "Crimson Roses",
+  "Destiny's Fires",
+  "Proposals and Passion",
+  "Silk Wedding",
+  "Masked Seduction",
+  "Crimson Masquerade",
+  "Stolen Rubies",
+  "Emerald Eyes",
+  "Sapphire Skies",
+  "Opal Passions"
+];
+var descriptors = [
+  "passion",
+  "glory",
+  "romance",
+  "woe",
+  "sorrow",
+  "pain",
+  "ardor",
+  "devotion",
+  "excitement",
+  "fervor",
+  "rage",
+  "spirit",
+  "zeal",
+  "ecstasy",
+  "fire",
+  "storms",
+  "tempests",
+  "rapture",
+  "ire",
+  "jealousy",
+  "exhilaration",
+  "bliss",
+  "enchantment",
+  "paradise",
+  "calamity",
+  "disaster",
+  "heartache",
+  "misfortune",
+  "agony",
+  "curses",
+  "blessings",
+  "melancholy"
+];
 
 // We've provided a few variables below
 var savedCovers = [
@@ -20,8 +113,11 @@ randomButton.addEventListener('click', loadRandomPoster);
 
 // Create your event handlers and other functions here 👇
 function loadRandomPoster() {
-   return coverImage.src = covers[getRandomIndex(covers)]
-}
+   return coverImage.src = covers[getRandomIndex(covers)], 
+    coverTitle.innerText = titles[getRandomIndex(titles)],
+    taglineOne.innerText = descriptors[getRandomIndex(descriptors)],
+    taglineTwo.innerText = descriptors[getRandomIndex(descriptors)]
+};
 
 // We've provided two functions to get you started
 function getRandomIndex(array) {
