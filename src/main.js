a0b4734a05e6d3a3f7c5b11302ce2e0d80fa2c56
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-03-29 22:04:06 -0600

    Outline iteration 1 and leave pseudocode to follow

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-// // Create variables targetting the relevant DOM elements here 👇
+// Create variables targetting the relevant DOM elements here 👇
 var randomTitle = document.querySelector('.cover-title');
 
 var randomDescriptor1 = document.querySelector('.tagline-1');
@@ -9,19 +9,37 @@ var image = document.querySelector('.cover-image');
 
 var randomCoverButton = document.querySelector('.random-cover-button');
 
-// // We've provided a few variables below
+var homeButton = document.querySelector('.home-button');
+
+var makeCoverButton = document.querySelector('.make-new-button');
+
+var saveButton = document.querySelector('.save-cover-button');
+
+var veiwSaveButton = document.querySelector('.view-saved-button');
+
+// We've provided a few variables below
 var savedCovers = [
   createCover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 
 var currentCover;
 
-// // Add your event listeners here 👇
+// Add your event listeners here 👇
 randomCoverButton.addEventListener('click', displayNewCover);
+
+homeButton.addEventListener('click',)
+
+makeCoverButton.addEventListener('click',)
+
+saveButton.addEventListener('click',)
+
+veiwSaveButton.addEventListener('click',)
+
+//might need to add more eventListners for each button click to invoke different functions
 
 // /* do we create a new function that uses getRandomIndex function as callback?*/
 
-// // Create your event handlers and other functions here 👇
+// Create your event handlers and other functions here 👇
 function displayNewCover() {
   randomTitle.innerText = titles[getRandomIndex(titles)];
   randomDescriptor1.innerText = descriptors[getRandomIndex(descriptors)];
@@ -29,7 +47,9 @@ function displayNewCover() {
   image.src = covers[getRandomIndex(covers)];
 };
 
-// // We've provided two functions to get you started
+//add functions here to be called to hide and show buttons and pages - might have multiple event listeners for one button click to hide multiple buttons and pull up things to keep code DRY
+
+// We've provided two functions to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 };
