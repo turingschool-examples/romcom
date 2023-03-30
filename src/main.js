5d0e6ac182b7c4b86c7811ec5eedb5e32887bb58
Parvin Sattorova <nanajon66@gmail.com>
2023-03-30 14:24:10 -0600

    Create variables targetting the relevant DOM elements. Edit savedCovers var

--- src/main.js
+++ src/main.js
@@ -1,22 +1,60 @@
-// Create variables targetting the relevant DOM elements here 👇
-
 
 // We've provided a few variables below
-var savedCovers = [
-  createCover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
-];
-var currentCover;
+// var savedCovers = [
+//   createCover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
+// ];
+// var currentCover;
+
+// Create variables targetting the relevant DOM elements here 👇
+
+// querySelector variables.
+
+// Buttons. Controls. Elements.
+var homeButton = document.querySelector(".home-button");
+var randomCoverButton = document.querySelector(".random-cover-button");
+var saveCoverButton = document.querySelector(".save-cover-button");
+var viewSavedCoversButton = document.querySelector(".view-saved-button");
+var makeNewCoverButton = document.querySelector(".make-new-button");
+var makeMyBookButton = document.querySelector('.create-new-book-button');
+
+// View elements.
+var homeView = document.querySelector(".home-view");
+var formView = document.querySelector(".form-view");
+var savedView = document.querySelector(".saved-view");
+var savedCoverSection = document.querySelector(".saved-covers-section");
+
+// Cover elements.
+var coverImage = document.querySelector(".cover-image");
+var coverTitle = document.querySelector(".cover-title");
+var tagline1 = document.querySelector(".tagline-1");
+var tagline2 = document.querySelector(".tagline-2");
+
+// Form elements.
+var userInputCover = document.querySelector(".user-cover");
+var userInputTitle = document.querySelector(".user-title");
+var userInputDesc1 = document.querySelector(".user-desc1");
+var userInputDesc2 = document.querySelector(".user-desc2");
+var createNewBookButton = document.querySelector(".create-new-book-button");
 
 // Add your event listeners here 👇
 
 
 // Create your event handlers and other functions here 👇
 
+// Variables
 
-// We've provided two functions to get you started
+var savedCovers = [
+  createCover(covers[getRandomIndex(cover)], titles[getRandomIndex(title)], 
+  descriptors[getRandomIndex(descriptors)], 
+  descriptors[getRandomIndex(descriptors)])
+];
+var currentCover;
+
+// We've provided two functions to get you started 
+
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
-}
+};
 
 function createCover(imgSrc, title, descriptor1, descriptor2) {
   var cover = {
@@ -25,6 +63,10 @@ function createCover(imgSrc, title, descriptor1, descriptor2) {
     title: title,
     tagline1: descriptor1,
     tagline2: descriptor2
-  }
+  };
   return cover
-}
+};
+
+  
+  
+  
