2522ae03fe16c1e3446849ad40fd7d14c65205a7
Caitlin Cradick <caitlincradick@gmail.com>
2023-03-30 15:21:52 -0700

    Add refactored code for previous functions for displaying different pages and wrapping up iteration 1

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ var viewSavedButton = document.querySelector('.view-saved-button');
 var coverForm = document.querySelector('.form-view');
 var homeView = document.querySelector('.home-view');
 var saveView = document.querySelector('.saved-view');
+var viewPages = document.querySelectorAll('.view');
 
 // We've provided a few variables below
 var savedCovers = [
@@ -22,8 +23,15 @@ var currentCover;
 // Add your event listeners here 👇
 window.addEventListener('load', createRandomCover);
 randomCoverButton.addEventListener('click', createRandomCover);
-makeCoverButton.addEventListener('click', showForm);
-viewSavedButton.addEventListener('click', saveCover);
+makeCoverButton.addEventListener('click', function() {
+  togglePage(coverForm);
+});
+viewSavedButton.addEventListener('click', function() {
+  togglePage(saveView);
+});
+homeButton.addEventListener('click', function() {
+  togglePage(homeView);
+});
 
 // Create your event handlers and other functions here 👇
 function createRandomCover() {
@@ -33,28 +41,30 @@ function createRandomCover() {
   tagline2.innerText = descriptors[getRandomIndex(descriptors)];
 }
 
-function showForm() {
-  homeView.classList.add('hidden');
-  coverForm.classList.remove('hidden');
-
-  if (coverForm) {
-    randomCoverButton.classList.add('hidden');
-    viewSavedButton.classList.add('hidden');
-    homeButton.classList.remove('hidden');
+function togglePage(page) {
+  for(var i = 0; i < viewPages.length; i++) {
+    viewPages[i].classList.add('hidden');
   }
-}
+  page.classList.remove('hidden');
 
-function saveCover() {
-  homeView.classList.add('hidden');
-  saveView.classList.remove('hidden');
-  
-  if (saveView) {
+  if (!homeView.classList.contains('hidden')) {
+    homeButton.classList.add('hidden');
+    randomCoverButton.classList.remove('hidden');
+    saveCoverButton.classList.remove('hidden');
+    viewSavedButton.classList.remove('hidden');
+  }
+
+  if (!saveView.classList.contains('hidden')) {
     saveCoverButton.classList.add('hidden');
     randomCoverButton.classList.add('hidden');
     homeButton.classList.remove('hidden');
   }
 
-
+  if (!coverForm.classList.contains('hidden')) {
+    randomCoverButton.classList.add('hidden');
+    viewSavedButton.classList.add('hidden');
+    homeButton.classList.remove('hidden');
+  }
 }
 
 // We've provided two functions to get you started
