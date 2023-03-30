9b5374484998ec06e87acb7e294ed8c6664f6f6b
Alec Livaditis <aleclivaditis@gmail.com>
2023-03-30 18:08:50 -0400

    Add home button visibility feature

--- src/main.js
+++ src/main.js
@@ -7,25 +7,31 @@ var priceTag = document.querySelector('.price-tag');
 var overlay = document.querySelector('.overlay');
 var randomButton = document.querySelector('.random-cover-button');
 var makeYourOwnCoverButton = document.querySelector('.make-new-button');
-var viewForm = document.querySelector('.form-view');
+var formView = document.querySelector('.form-view');
 var homeView = document.querySelector('.home-view');
 var randomCoverButton = document.querySelector('.random-cover-button');
 var saveCoverButton = document.querySelector('.save-cover-button');
+var homeButton = document.querySelector('.home-button');
+var savedView = document.querySelector('.saved-view');
+var savedButton = document.querySelector('.view-saved-button');
 
 // We've provided a few variables below
 var savedCovers = [
   createCover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-var currentCover;
+
+var currentCover = {};
 
 // Add your event listeners here 👇
 window.addEventListener('load', loadRandomPoster);
 randomButton.addEventListener('click', loadRandomPoster);
 makeYourOwnCoverButton.addEventListener('click', showForm);
+savedButton.addEventListener('click', showSavedPosters);
+homeButton.addEventListener('click', showHome);
 
 // Create your event handlers and other functions here 👇
 function loadRandomPoster() {
-   return coverImage.src = covers[getRandomIndex(covers)], 
+    coverImage.src = covers[getRandomIndex(covers)], 
     coverTitle.innerText = titles[getRandomIndex(titles)],
     taglineOne.innerText = descriptors[getRandomIndex(descriptors)],
     taglineTwo.innerText = descriptors[getRandomIndex(descriptors)]
@@ -35,9 +41,26 @@ function showForm() {
   homeView.classList.add('hidden');
   randomCoverButton.classList.add('hidden');
   saveCoverButton.classList.add('hidden');
-  viewForm.classList.remove('hidden');
-  // show save
-  return viewForm, homeView;
+  formView.classList.remove('hidden');
+  homeButton.classList.remove('hidden');
+  return formView, homeView;
+}
+
+function showSavedPosters() {
+  savedView.classList.remove('hidden');
+  homeView.classList.add('hidden');  
+  saveCoverButton.classList.add('hidden');  
+  randomCoverButton.classList.add('hidden');  
+  homeButton.classList.remove('hidden');  
+}
+
+function showHome() {
+  homeView.classList.remove('hidden')
+  formView.classList.add('hidden')
+  savedView.classList.add('hidden')
+  randomCoverButton.classList.remove('hidden');
+  saveCoverButton.classList.remove('hidden');
+  homeButton.classList.add('hidden')
 }
 
 // We've provided two functions to get you started
