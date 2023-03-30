2498cba9b3683787c8ecd000549e1b9c358eca35
Marisa Wyatt <risawyatt22@gmail.com>
2023-03-30 16:05:00 -0600

    Write the makeCustomCoverForms function

--- src/main.js
+++ src/main.js
@@ -4,11 +4,14 @@ var coverTitle = document.querySelector('.cover-title');
 var tagline1 = document.querySelector('.tagline-1');
 var tagline2 = document.querySelector('.tagline-2');
 var coverImage = document.querySelector('.cover-image');
-var randomTitle = titles[getRandomIndex(titles)];
-var randomTagline1 = descriptors[getRandomIndex(descriptors)];
-var randomTagline2 = descriptors[getRandomIndex(descriptors)];
-var randomImage = covers[getRandomIndex(covers)];
-
+var makeCoverButton = document.querySelector('.make-new-button');
+var homeButton = document.querySelector('.home-button');
+var saveCoverButton = document.querySelector('.save-cover-button');
+var viewSavedButton = document.querySelector('.view-saved-button');
+var homeView = document.querySelector('.home-view');
+var formView = document.querySelector('.form-view');
+var savedView = document.querySelector('.saved-view');
+var saveCoverButton = document.querySelector('.save-cover-button')
 
 // We've provided a few variables below
 var savedCovers = [
@@ -19,7 +22,7 @@ var currentCover;
 // Add your event listeners here 👇
 randomButton.addEventListener('click', newRandomBook)
 window.addEventListener('DOMContentLoaded', newRandomBook)
-
+makeCoverButton.addEventListener('click', makeCustomCoverForms)
 // Create your event handlers and other functions here 👇
 function newRandomBook() {
   coverTitle.innerText = titles[getRandomIndex(titles)];
@@ -28,7 +31,15 @@ function newRandomBook() {
   coverImage.src = covers[getRandomIndex(covers)];
 }
 
+function makeCustomCoverForms() {
+  homeButton.classList.remove('hidden')
+  homeView.classList.add('hidden')
+  formView.classList.remove('hidden')
+  randomButton.classList.add('hidden')
+  saveCoverButton.classList.add('hidden')
+}
 // We've provided two functions to get you started
+
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
