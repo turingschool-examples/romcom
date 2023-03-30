f2937319ffcb5ca6306f721cfb78aa02aa745a99
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-03-29 20:07:52 -0600

    Refactor code and start working on event listener

--- src/main.js
+++ src/main.js
@@ -1,28 +1,30 @@
 // Create variables targetting the relevant DOM elements here 👇
+var imgSrc = document.querySelector('.cover-image');
 var randomTitle = document.querySelector('.cover-title')
-
-var randomDescriptor = document.querySelector('.tagline-1', `tagline-2`);
-
-var image = document.querySelector('.cover-image');
+var descriptor1 = document.querySelector('.tagline-1');
+var descriptor2 = document.querySelector(`.tagline-2`);
 
 // We've provided a few variables below
-var savedCovers = [
-  createCover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
-];
-var currentCover;
+// var savedCovers = [
+//   createCover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
+// ];
+var currentCover = createCover((imgSrc, randomTitle, descriptor1, descriptor2))
 
 // Add your event listeners here 👇
+var randomCover = document.querySelector('.random-cover-button')
 
+randomCover.addEventListener('click', createCover)
 
 /* do we create a new function that uses getRandomIndex function as callback?*/
-
 
 // Create your event handlers and other functions here 👇
 randomTitle.innerText = titles[getRandomIndex(titles)]
 
-randomDescriptor.innerText = descriptors[getRandomIndex(descriptors)];
+descriptor1.innerText = descriptors[getRandomIndex(descriptors)];
 
-image.src = covers[getRandomIndex(covers)];
+descriptor2.innerText = descriptors[getRandomIndex(descriptors)];
+
+imgSrc.src = covers[getRandomIndex(covers)];
 
 // We've provided two functions to get you started
 function getRandomIndex(array) {
