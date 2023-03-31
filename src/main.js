962c68444b5f344c0b5207f29803bb7f64b873ef
“Saki <sakisandra.c@gmail.com>
2023-03-31 09:17:17 -0600

    Add an id to images when rendered

--- src/main.js
+++ src/main.js
@@ -143,7 +143,7 @@ function renderCovers() {
   for (var i=0; i < savedCovers.length; i++){
     savedCoversSection.innerHTML += `
       <section class="mini-cover">
-        <img class="mini-cover" src="${savedCovers[i].coverImg}">
+        <img class="mini-cover" id =${savedCovers[i].id} src="${savedCovers[i].coverImg}">
         <h2 class="cover-title">${savedCovers[i].title}</h2>
         <h3 class="tagline">A tale of <span class="tagline-1">${savedCovers[i].tagline1}</span> and <span class="tagline-2">${savedCovers[i].tagline2}</span>
         </h3>
