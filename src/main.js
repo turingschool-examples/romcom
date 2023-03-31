a40e195e818f26e7c4afff152a4202c423a79c32
Calli Herrmann <calliherrmann@gmail.com>
2023-03-30 21:49:26 -0400

    Add missing variable

--- src/main.js
+++ src/main.js
@@ -19,6 +19,7 @@ var savedCovers = [
   createCover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 var currentCover;
+var currentView;
 
 // Add your event listeners here 👇
 showNewRandomCoverButton.addEventListener('click', getRandomCover);
