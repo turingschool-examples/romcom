e758d17e9d7d64504acab972fb3e2419ed3716e7
Victor Sriqui <victorsriqui@gmail.com>
2023-04-01 18:33:26 -0600

    Add a little extra functionality, some refactoring, some organization, some stylistic edits.

--- src/main.js
+++ src/main.js
@@ -1,54 +1,89 @@
-// ***Variables*** //
-// ***Button Vars*** //
-var randomCoverButton = document.querySelector('.random-cover-button');
-var saveCoverButton = document.querySelector('.save-cover-button');
-var viewSavedButton = document.querySelector('.view-saved-button');
-var makeNewButton = document.querySelector('.make-new-button');
-var goHomeButton = document.querySelector('.home-button');
-var makeBookButton = document.querySelector('.create-new-book-button');
-// ** Page vars ** //
-var viewForm = document.querySelector('.form-view');
-var viewHome = document.querySelector('.home-view');
-var viewSaved = document.querySelector('.saved-view');
-var saveCover = document.querySelector(".saved-covers-section");
-// ** Cover Vars ** //
-var coverImage = document.querySelector('.cover-image');
-var coverTitle = document.querySelector('.cover-title');
-var tagline1 = document.querySelector('.tagline-1');
-var tagline2 = document.querySelector('.tagline-2');
-// ** Form Vars ** //
-var customCover = document.querySelector('.user-cover');
-var customTitle = document.querySelector('.user-title');
-var customDescriptor1 = document.querySelector('.user-desc1');
-var customDescriptor2 = document.querySelector('.user-desc2');
-// ** Project Vars ** //
-var savedCovers = [
-  createCover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
-];
+// Variables //
+// Button Variables //
+var randomCoverButton = document.querySelector(".random-cover-button");
+var saveCoverButton = document.querySelector(".save-cover-button");
+var viewSavedButton = document.querySelector(".view-saved-button");
+var makeNewButton = document.querySelector(".make-new-button");
+var goHomeButton = document.querySelector(".home-button");
+var createBookButton = document.querySelector(".create-new-book-button");
+
+// Page vars //
+var viewForm = document.querySelector(".form-view");
+var viewHome = document.querySelector(".home-view");
+var viewSaved = document.querySelector(".saved-view");
+var savedCoversSection = document.querySelector(".saved-covers-section");
+
+// Cover Vars //
+var coverImage = document.querySelector(".cover-image");
+var coverTitle = document.querySelector(".cover-title");
+var tagline1 = document.querySelector(".tagline-1");
+var tagline2 = document.querySelector(".tagline-2");
+
+// Form Vars //
+var userCover = document.querySelector('#cover');
+var userTitle = document.querySelector('.user-title');
+var userDescriptor1 = document.querySelector('.user-desc1');
+var userDescriptor2 = document.querySelector('.user-desc2');
+
+// Vars for Cover Object and Array of Cover Objects //
+var savedCovers = [];
 var currentCover;
-// ***Event Listeners*** //
-// ***Refreshing the Page*** //
-onload = handleRandomCoverButtonClicked
-// ***Click Input Event Listeners*** //
-randomCoverButton.onclick = handleRandomCoverButtonClicked;
-saveCoverButton.onclick = handleSaveCoverButtonClicked;
-viewSavedButton.onclick = handleViewSavedButtonClicked;
-makeNewButton.onclick = handleNewButtonClicked;
-goHomeButton.onclick = handleHomeButtonClicked;
 
-makeBookButton.onclick = (function(event) {
+// Event Listeners //
+// Loads New Random Cover and Page Loading //
+onload = newRandomCover;
+
+// Event Listeners For Button Functionality For Changing Views //
+randomCoverButton.onclick = newRandomCover;
+saveCoverButton.onclick = saveNewCover;
+viewSavedButton.onclick = viewSavedCovers;
+makeNewButton.onclick = makeNewBook;
+goHomeButton.onclick = goHome;
+// Event Listener For Making A Custom Book Button //
+createBookButton.onclick = function(event) {
   event.preventDefault();
-  generateCover();
-  loadPicture();
-  loadTitle();
-  loadDescriptor1();
-  loadDescriptor2();
-  setTimeout(clearForm, 1000)
-})
+  goHome();
+  createCustomBook();
+  saveToArrays();
+  setTimeout(clearInputs, 1000)  
+}
 
-// ***Event Handlers and Functions*** //
-// ***Functions To Ease Readability Of Code*** //
+// Event Listener for Double Clicking Saved Covers on Saved Covers Page //
+savedCoversSection.ondblclick = function(event) {
+  deleteCover(event)
+}
 
+// Event Handlers and Various Functions //
+// Function For Creating Cover Object //
+function createCover(imgSrc, title, descriptor1, descriptor2) {
+  var cover = {
+    id: Date.now(),
+    cover: imgSrc,
+    title: title,
+    tagline1: descriptor1,
+    tagline2: descriptor2
+  }
+
+  return cover
+}
+
+// Randomizer For a Number to Be Used For Index Positions in an Array //
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length)
+}
+// Function For Randomizing Front Page Cover Data Fields //
+function newRandomCover() {
+  currentCover = createCover(
+    covers[getRandomIndex(covers)],
+    titles[getRandomIndex(titles)],
+    descriptors[getRandomIndex(descriptors)],
+    descriptors[getRandomIndex(descriptors)],
+  );
+
+  displayCover()
+}
+
+// Functions for Readability of Button Handling //
 function hide(element) {
   element.classList.add("hidden")
 }
@@ -57,120 +92,125 @@ function show(element) {
   element.classList.remove("hidden")
 }
 
-function handleRandomCoverButtonClicked() {
-  coverImage.src = covers[getRandomIndex(covers)];
-  coverTitle.innerText = titles[getRandomIndex(titles)];
-  tagline1.innerText = descriptors[getRandomIndex(descriptors)];
-  tagline2.innerText = descriptors[getRandomIndex(descriptors)]  
+// Button Functions for Button Handling //
+function makeNewBook() {
+  hide(randomCoverButton);
+  hide(saveCoverButton);
+  show(goHomeButton);
+  show(viewForm);
+  hide(viewHome)
 }
 
+function viewSavedCovers() {
+  hide(randomCoverButton);
+  hide(saveCoverButton);
+  show(goHomeButton);
+  show(viewSaved);
+  hide(viewForm);
+  hide(viewHome)
+}
 
-function handleHomeButtonClicked(){
-  show(makeNewButton);
+function goHome() {
   show(randomCoverButton);
   show(saveCoverButton);
   hide(goHomeButton);
-  show(viewSavedButton);
   hide(viewSaved);
   hide(viewForm);
-  show(viewHome);
-  hide(saveCover)
-};
-
-
-
-function handleSaveCoverButtonClicked(){
-  show(makeNewButton);
-  show(randomCoverButton);
-  show(saveCoverButton);
-  show(goHomeButton);
-  show(viewSavedButton);
-  hide(viewSaved);
-  hide(viewForm);
-  hide(viewHome);
-  show(saveCover)
-};
-
-function handleNewButtonClicked() {
-  show(makeNewButton);
-  hide(randomCoverButton);
-  hide(saveCoverButton);
-  show(goHomeButton);
-  show(viewSavedButton);
-  hide(viewSaved);
-  show(viewForm);
-  hide(viewHome);
-  hide(saveCover)
+  show(viewHome)
 }
 
-function handleViewSavedButtonClicked() {
-  show(makeNewButton);
-  hide(randomCoverButton);
-  hide(saveCoverButton);
-  show(goHomeButton);
-  show(viewSavedButton);
-  show(viewSaved);
-  hide(viewForm);
-  hide(viewHome);
-  hide(saveCover)
-} 
-  
+// Function for Creating a Cover Object //
+function createCustomBook() {  
+  currentCover = createCover(
+  userCover.value,
+  userTitle.value,
+  userDescriptor1.value,
+  userDescriptor2.value,
+  );
 
-
-function generateCover() {
-  coverImage.src = customCover.value;
-  coverTitle.innerText = customTitle.value;
-  tagline1.innerText = customDescriptor1.value;
-  tagline2.innerText = customDescriptor2.value;
-  handleHomeButtonClicked();
+  displayCover(); 
 }
 
-function loadPicture() {
-  if (covers.indexOf(customCover.value) === -1) {
-   return covers.push(customCover.value)
+// Function For Cover Preview //
+function displayCover() {
+  coverImage.src = currentCover.cover;
+  coverTitle.innerText = currentCover.title;
+  tagline1.innerText = currentCover.tagline1;
+  tagline2.innerText = currentCover.tagline2
+}
+
+// Functions for Saving to Data Arrays For Availability For Random Cover Handling //
+function saveToArrays() {
+  loadPictureIf();
+  loadTitleIf();
+  loadDescriptor1If();
+  loadDescriptor2If()
+}
+
+function loadPictureIf() {
+  if (covers.indexOf(userCover.cover) === -1) {
+    covers.push(userCover.value)
   }
 }
 
-function loadTitle() {
-  if (titles.indexOf(customTitle.value) === -1) {
-   return titles.push(customTitle.value)
+function loadTitleIf() {
+  if (titles.indexOf(userTitle.value) === -1) {
+    titles.push(userTitle.value)
   } 
 }
 
-function loadDescriptor1() {
-  if (descriptors.indexOf(customDescriptor1.value) === -1){
-   return descriptors.push(customDescriptor1.value)
+function loadDescriptor1If() {
+  if (descriptors.indexOf(userDescriptor1.value) === -1){
+    descriptors.push(userDescriptor1.value)
   }
 }
 
-function loadDescriptor2() {
-  if (descriptors.indexOf(customDescriptor2.value) === -1) {
-   return descriptors.push(customDescriptor2.value)
+function loadDescriptor2If() {
+  if (descriptors.indexOf(userDescriptor2.value) === -1) {
+    descriptors.push(userDescriptor2.value)
   }
 }
 
-
-function clearForm() {
-  customCover.value = "";
-  customTitle.value = "";
-  customDescriptor1.value = "";
-  customDescriptor2.value = "";
+// Function for Clearing Data Fields of Make Your Own Cover //
+function clearInputs() {
+  userCover.value = "";
+  userTitle.value = "";
+  userDescriptor1.value = "";
+  userDescriptor2.value = "";
 }
 
-
-
-
-function getRandomIndex(array) {
-  return Math.floor(Math.random() * array.length)
-}
-
-function createCover(imgSrc, title, descriptor1, descriptor2) {
-  var cover = {
-    id: Date.now(),
-    coverImg: imgSrc,
-    title: title,
-    tagline1: descriptor1,
-    tagline2: descriptor2
+// Function For Saving Custom Or Random Cover Objects To Saved Cover Arrays // 
+function saveNewCover() {
+  if (!savedCovers.includes(currentCover)) {
+    savedCovers.push(currentCover);
   }
-  return cover
+
+  displaySavedCovers();
+}
+
+// Function For Displaying Saved Cover Array Objects On Saved Covers Page in Miniature //
+function displaySavedCovers() {
+  savedCoversSection.innerHTML = '';
+  for (i = 0; i < savedCovers.length; i++) {
+    savedCoversSection.innerHTML +=
+    `
+    <section class ="mini-cover" id="${savedCovers[i].id}">
+      <img class="cover-image" src=${savedCovers[i].cover}>
+      <h2 class="cover-title">${savedCovers[i].title}</h2>
+      <h3 class="tagline">A tale of <span class="tagline-1">${savedCovers[i].tagline1}</span> and <span class="tagline-2">${savedCovers[i].tagline2}</span></h3>
+    </section>
+    `
+  }
+}
+
+// Function For Handling Deletion //
+function deleteCover(event) { 
+  for (i = 0; i < savedCovers.length; i++) {
+    
+    if(event.target.closest('section').id === savedCovers[i].id.toString()){
+      savedCovers.splice(i, 1);    
+    }
+  }
+  
+  displaySavedCovers()
 }
