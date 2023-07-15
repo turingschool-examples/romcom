6af1262ac1c0f63805fab0d9760a545cf896c123
Zen McMillan <zenmcmillan@gmail.com>
2023-07-14 20:33:55 -0600

    Create event listener for form, create a function that takes the form values and assigns it to the currentCover variable and creates a function to add the currentCover to the savedCovers array

--- src/main.js
+++ src/main.js
@@ -10,12 +10,19 @@ var imageOverLay = document.querySelector('.overlay');
 var showRandomCoverButton = document.querySelector('.random-cover-button');
 var makeOwnCoverButton = document.querySelector('.make-new-button');
 var homePage = document.querySelector('.home-view');
-var form = document.querySelector('.form-view');
+var formView = document.querySelector('.form-view');
 var section = document.querySelector('section');
 var saveCoverButton = document.querySelector('.save-cover-button');
 var homeButton = document.querySelector('.home-button');
 var savedCoversButton = document.querySelector('.view-saved-button');
 var savedCoversPage = document.querySelector('saved-covers-section');
+
+var form = document.querySelector('form')
+var makeMyBookButton = document.querySelector('.create-new-book-button');
+var coverInput = document.querySelector('#cover');
+var titleInput = document.querySelector('#title')
+var descriptor1Input = document.querySelector('#descriptor1');
+var descriptor2Input = document.querySelector('#descriptor2');
 // queries over all dom elements(?) we need to change
 
 // We've provided a few variables below
@@ -41,24 +48,44 @@ makeOwnCoverButton.addEventListener('click', makeYourOwnCover)
 savedCoversButton.addEventListener('click', viewSavedCovers)
 
 homeButton.addEventListener('click', viewHomePage)
-  
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault()
+  createBook()
+})
+
 
 // Create your event handlers and other functions here 👇
+
+function createBook() {
+  currentCover.coverImg = coverInput.value;
+  currentCover.title = titleInput.value;
+  currentCover.theTagLine1 = titleInput.value;
+  currentCover.theTagLine2 = descriptor2Input.value;
+}
+
+function updateSavedCoversArray() {
+  for (var i = 0; i < savedCovers.length; i++) {
+    if (savedCovers[i].id != currentCover.id) {
+      savedCovers.push(currentCover)
+    }
+  }
+}
+
 
 function makeYourOwnCover () {
   homePage.classList.add('hidden')
   saveCoverButton.classList.add('hidden') 
   showRandomCoverButton.classList.add('hidden') 
-  form.classList.add('hidden')
-  form.classList.remove('hidden')
+  formView.classList.remove('hidden')
   homeButton.classList.remove('hidden')
-  
 }
 
 function viewSavedCovers() {
   homePage.classList.add('hidden') 
   showRandomCoverButton.classList.add('hidden') 
   saveCoverButton.classList.add('hidden')
+  formView.classList.add('hidden')
   homeButton.classList.remove('hidden')
 }
 
