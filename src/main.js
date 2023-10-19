1cb078102f5d39765b4d4fd3f8a79e957ebd52c2
Jeremy Pearson <jeremy.pearson08@gmail.com>
2023-10-19 17:22:09 -0600

    Added saved/home/form view

--- src/main.js
+++ src/main.js
@@ -14,14 +14,16 @@ var savedCovers = [
   createCover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 var currentCover = createRandomCover();
+
 // Add your event listeners here :point_down:
 randomBtn.addEventListener('click', createRandomCover);
 newBtn.addEventListener('click', makeNewBtn);
 viewBtn.addEventListener('click', viewSavedCovers);
 homeBtn.addEventListener('click', viewHome)
 // saveBtn.addEventListener('click', saveCover);
-// newBookBtn.addEventListener('click', makeNewBookBtn);
+newBookBtn.addEventListener('click', createCustomCover);
 // Create your event handlers and other functions here :point_down:
+
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
@@ -71,3 +73,18 @@ function viewHome(){
   saveBtn.classList.remove('hidden');
   randomBtn.classList.remove('hidden');
 }
+
+function createCustomCover() {
+  var userCover = document.querySelector('.user-cover').value;
+  var userTitle = document.querySelector('.user-title').value;
+  var userDesc1 = document.querySelector('.user-desc1').value;
+  var userDesc2 = document.querySelector('.user-desc2').value;
+  const customCover = createCover(userCover, userTitle, userDesc1, userDesc2);
+  document.querySelector('.cover-image').src = customCover.coverImg;
+  document.querySelector('.cover-title').innerText = customCover.title;
+  document.querySelector('.tagline-2').innerText = customCover.tagline2;
+  document.querySelector('.tagline-1').innerText = customCover.tagline1;
+  viewHome();
+}
+
+var customCover = createCover(userCover, userTitle, userDesc1, userDesc2);
