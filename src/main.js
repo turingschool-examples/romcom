cc4d70537b9a3f46f669efec1651dd0df63148fb
Florenzo Bauer <florenzobauer@gmail.com>
2023-10-18 22:55:32 -0600

    Making full functions-some errors

--- src/main.js
+++ src/main.js
@@ -1,49 +1,50 @@
 // Create variables targetting the relevant DOM elements here 👇
 
-
 var homeView = document.querySelector('.home-view');
 var formView = document.querySelector('.form-view');
-var savedView = document.querySelector('.saved-view');
-var randomView = document.querySelector('.random-cover-view');
+var savedView = document.querySelector('.saved-view')
+
+
 var homeButton = document.querySelector('.home-button');
 var saveVButton = document.querySelector('.save-cover-button');
 var viewSButton = document.querySelector('.view-saved-button');
 var randomButton = document.querySelector('.random-cover-button');
+var makeOwnButton = document.querySelector('.make-new-button');
 
 // We've provided a few variables below
+
 var savedCovers = [
   createCover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 var currentCover;
-
 // Add your event listeners here 👇
-
-savedVButton.addEventListener('click', savedLButton);
-homeButton.addEventListener('click', homeLButton);
-// element.querySelector(`.home-button`).addEventListener(`click`, homeButton());
-// element.querySelector(`.saved-cover-button`).addEventListener(`click`, saveCoverButton());
+homeButton.addEventListener("click", homeF("click"));
+saveVButton.addEventListener("click", savedF());
+makeOwnButton.addEventListener("click", makeF());
 // Create your event handlers and other functions here 👇
-function homeLButton(){
- //q: why is this not working?
- // a:
+function homeF(){
+  homeButton.classList.add("hidden");
+  formView.classList.add("hidden");
+  savedView.classList.add("hidden");
 
-  add(homeView);
-  remove(savedView);
-  remove(randomView);
-  remove(formView);
-  remove(homeButton);
+  homeView.classList.remove("hidden");
 }
-function savedLButton(){
-  
-    add(savedView)
-    remove(homeView);
-    remove(homeButton);
-    remove(saveVButton);
-    remove(randomButton);
-  
+function makeF(){
+  homeView.classList.add("hidden");
+  savedView.classList.add("hidden");
+  formView.classList.remove("hidden");
+  homeButton.classList.remove("hidden");
 }
-homeLButton();
-savedLButton(); 
+function savedF(){
+  homeView.classList.add("hidden");
+  formView.classList.add("hidden");
+
+  savedView.classList.remove("hidden");
+  homeButton.classList.remove("hidden");
+}
+
+
+
 // We've provided two functions to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
@@ -59,3 +60,4 @@ function createCover(imgSrc, title, descriptor1, descriptor2) {
   }
   return cover
 }
+
