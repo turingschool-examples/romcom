556603f8a7a5001c69e5457f4f3459337079d489
Cam Johnson <cameron.n.johnson15@gmail.com>
2023-10-19 15:47:10 -0600

    feat: Add functionality to View Covers and Home buttons

--- src/main.js
+++ src/main.js
@@ -1,30 +1,37 @@
 // Create variables targetting the relevant DOM elements here 👇
-var randButton = document.querySelector('.random-cover-button');
-var imageCover = document.querySelector('.cover-image');
-var bookTitle = document.querySelector('.cover-title');
-var tagline1 = document.querySelector('.tagline-1');
-var tagline2 = document.querySelector('.tagline-2');
+var randButton = document.querySelector(".random-cover-button");
+var imageCover = document.querySelector(".cover-image");
+var bookTitle = document.querySelector(".cover-title");
+var tagline1 = document.querySelector(".tagline-1");
+var tagline2 = document.querySelector(".tagline-2");
+var viewCoversBtn = document.querySelector(".view-saved-button");
+var homePage = document.querySelector(".home-view");
+var savedCoversView = document.querySelector(".saved-view");
+var saveCoverBtn = document.querySelector(".save-cover-button");
+var homeBtn = document.querySelector(".home-button");
 
 // We've provided a few variables below
-var savedCovers = [
-  createCover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
-];
-var currentCover;
+// var savedCovers = [
+//   createCover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
+// ];
+// var currentCover;
 
 // Add your event listeners here 👇
-randButton.addEventListener('click', generateNewCover);
+randButton.addEventListener("click", generateNewCover);
+viewCoversBtn.addEventListener("click", viewSavedCovers);
+homeBtn.addEventListener("click", returnToHome);
 
 // Create your event handlers and other functions here 👇
 function generateNewTitle() {
   var index = getRandomIndex(titles);
-  
+
   bookTitle.innerText = titles[index];
 }
 
 function generateNewTagline() {
   var index1 = getRandomIndex(titles);
   var index2 = getRandomIndex(titles);
-  
+
   if (index2 === index1) {
     if (index1 === descriptors.length - 1) {
       index2 = 0;
@@ -32,7 +39,7 @@ function generateNewTagline() {
       index2 = index1 + 1;
     }
   }
-  
+
   tagline1.innerText = descriptors[index1];
   tagline2.innerText = descriptors[index2];
 }
@@ -45,7 +52,23 @@ function generateNewCover() {
   generateNewTagline();
 }
 
-// Starting conditions 
+function viewSavedCovers() {
+  homePage.classList.add("hidden");
+  savedCoversView.classList.remove("hidden");
+  randButton.classList.add("hidden");
+  saveCoverBtn.classList.add("hidden");
+  homeBtn.classList.remove("hidden");
+}
+
+function returnToHome() {
+  savedCoversView.classList.add("hidden");
+  homePage.classList.remove("hidden");
+  homeBtn.classList.add("hidden");
+  randButton.classList.remove("hidden");
+  saveCoverBtn.classList.remove("hidden");
+}
+
+// Starting conditions
 generateNewCover();
 generateNewTitle();
 generateNewTagline();
@@ -61,7 +84,7 @@ function createCover(imgSrc, title, descriptor1, descriptor2) {
     coverImg: imgSrc,
     title: title,
     tagline1: descriptor1,
-    tagline2: descriptor2
-  }
-  return cover
+    tagline2: descriptor2,
+  };
+  return cover;
 }
