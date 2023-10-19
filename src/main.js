0b0546a293062660b81f4b3ea99c52836c9c54f2
Dana Zack <danaezack@gmail.com>
2023-10-19 08:37:26 -0600

    Update currentCover variable

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ var newBookBtn = document.querySelector('.create-new-book-button');
 var savedCovers = [
   createCover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-var currentCover;
+var currentCover = createRandomCover();
 
 
 
