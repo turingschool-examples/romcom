5dd1fc358a883a32c9b7bb08433b3c7898b0c226
Jamie Francisco <0jamiefrancisco@gmail.com>
2023-10-20 13:03:57 -0700

    Add all functionality to switch views

--- src/main.js
+++ src/main.js
@@ -4,30 +4,67 @@ var coverTitle = document.querySelector('.cover-title');
 var coverTagline1 = document.querySelector('.tagline-1');
 var coverTagline2 = document.querySelector('.tagline-2');
 var randomCoverButton = document.querySelector('.random-cover-button')
-
+var saveCoverButton = document.querySelector('.save-cover-button');
+var viewSavedButton = document.querySelector('.view-saved-button');
+var makeCoverButton = document.querySelector('.make-new-button');
+var homeButton = document.querySelector('.home-button');
+var homeView = document.querySelector('.home-view');
+var formView = document.querySelector('.form-view');
+var savedView = document.querySelector('.saved-view');
 
 // We've provided a few variables below
 var savedCovers = [
   createCover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 var currentCover;
-
 // Add your event listeners here 👇
+window.addEventListener('load', generateRandomCover);
 randomCoverButton.addEventListener('click', generateRandomCover);
-window.addEventListener('load', generateRandomCover)
+makeCoverButton.addEventListener('click', showFormView);
+viewSavedButton.addEventListener('click', showSavedView);
+homeButton.addEventListener('click', showHomeView);
 
 // Create your event handlers and other functions here 👇
 function generateRandomCover() {
-  var imgSrc = covers[getRandomIndex(covers)]
+  var imgSrc = covers[getRandomIndex(covers)];
   var title = titles[getRandomIndex(titles)];
   var descriptor1 = descriptors[getRandomIndex(descriptors)];
   var descriptor2 = descriptors[getRandomIndex(descriptors)];
-
   coverImage.src = imgSrc;
   coverTitle.innerText = title;
   coverTagline1.innerText = descriptor1;
   coverTagline2.innerText = descriptor2;
 }
+
+function show(element) {
+  element.classList.remove('hidden');
+}
+
+function hide(element) {
+  element.classList.add('hidden');
+}
+
+function showFormView() {
+ hide(homeView);
+ hide(saveCoverButton);
+ hide(randomCoverButton);
+ show(formView);
+ show(homeButton);
+}
+
+ function showSavedView() {
+  hide(formView);
+  show(savedView);
+ }
+
+ function showHomeView() {
+  hide(formView);
+  show(homeView);
+  show(saveCoverButton);
+  show(randomCoverButton);
+  hide(homeButton);
+  generateRandomCover();
+ }
 
 // We've provided two functions to get you started
 function getRandomIndex(array) {
