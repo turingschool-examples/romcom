1f275d87429848dc4ff1f2bf450e455ca368b34f
Gwyneth Patrick <gwynethpatrick235@gmail.com>
2024-02-07 18:29:47 -0700

    Will remove the console logs from the data file, will generate a random cover on landing, and will add functionality to the random cover button

--- src/main.js
+++ src/main.js
@@ -1,30 +1,35 @@
 // Create variables targetting the relevant DOM elements here 👇
-cover = createCover('./assets/fire.png', 'title', 'title', 'title')
 
 // We've provided a few variables below
 var savedCovers = [
   createCover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 var currentCover = document.querySelector('.main-cover');
-console.log(currentCover.innerHTML)
-currentCover.innerHTML = 
-`<img class="cover-image" src="${cover.coverImg}">
-<h2 class="cover-title">${cover.title}</h2>
-<h3 class="tagline">A tale of <span class="tagline-1">${cover.tagline1}</span> and <span class="tagline-2">${cover.tagline2}</span></h3>
-<img class="price-tag" src="./assets/price.png">
-<img class="overlay" src="./assets/overlay.png">`
+
+
+
+// Add your event listeners here 👇
+
+document.querySelector('.random-cover-button').addEventListener('click', createRandomCover)
 
 // Create your event handlers and other functions here 👇
 
 function createRandomCover() {
-  newCover = []
-  newCover[0] = getRandomIndex()
+  newCover = createCover(covers[getRandomIndex(covers)],titles[getRandomIndex(titles)],descriptors[getRandomIndex(descriptors)],descriptors[getRandomIndex(descriptors)])
+  currentCover.innerHTML = 
+  `<img class="cover-image" src="${newCover.coverImg}">
+  <h2 class="cover-title">${newCover.title}</h2>
+  <h3 class="tagline">A tale of <span class="tagline-1">${newCover.tagline1}</span> and <span class="tagline-2">${newCover.tagline2}</span></h3>
+  <img class="price-tag" src="./assets/price.png">
+  <img class="overlay" src="./assets/overlay.png">`
+  
 
   console.log(coverSeed)
   //createCover(coverSeed[0])
 
   
 }
+
 
 
 function randomCoverButton(){
@@ -49,3 +54,5 @@ function createCover(imgSrc, title, descriptor1, descriptor2) {
   }
   return cover
 }
+
+createRandomCover()
