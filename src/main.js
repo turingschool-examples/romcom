54d00bb4cdaf9eb8a594effb754ece8ce9161932
laurel Bonal <laurelbonal@gmail.com>
2024-02-09 08:27:37 -0700

    Debug iteration 1

--- src/main.js
+++ src/main.js
@@ -6,26 +6,17 @@ var imageTitle = document.querySelector('.poster-title')
 var coverTitle = document.querySelector('.cover-title')
 var tag1 = document.querySelector('.tagline-1')
 var tag2 = document.querySelector('.tagline-2')
+
+
 var coverButton = document.querySelector('.random-cover-button')
+var makeNewButton = document.querySelector('.make-new-button')
+var homeButton = document.querySelector('.home-button')
+var saveCoverButton = document.querySelector('.save-cover-button')
+var viewSavedButton = document.querySelector('.view-saved-button')
 
-
-var randomCoverIndex = getRandomIndex(covers)
-var randomTitleIndex = getRandomIndex(titles)
-var randomTags1Index = getRandomIndex(descriptors)
-var randomTags2Index = getRandomIndex(descriptors)
-
-var chosenRandomTitle = titles[randomTitleIndex]
-var chosenRandomCoverPath = covers[randomCoverIndex]
-var chosenRandomTag1 = descriptors[randomTags1Index]
-var chosenRandomTag2 = descriptors[randomTags2Index]
-
-
-
-
-
-
-
-
+var formView = document.querySelector('.form-view');
+var homeView = document.querySelector('.home-view');
+var savedView = document.querySelector('.saved-view')
 
 // We've provided a few variables below
 var savedCovers = [
@@ -35,26 +26,13 @@ var currentCover;
 
 // Add your event listeners here 👇
 
+coverButton.addEventListener('click', createRandomCover)
+makeNewButton.addEventListener('click', reactMakeNew)
+homeButton.addEventListener('click', reactHomeButton)
+viewSavedButton.addEventListener('click', reactViewSaved)
+
 
 // Create your event handlers and other functions here 👇
-
-coverButton.addEventListener('click', createRandomCover)
-
-// We've provided two functions to get you started
-function getRandomIndex(array) {
-  return Math.floor(Math.random() * array.length);
-}
-
-function createCover(imgSrc, title, descriptor1, descriptor2) {
-  var cover = {
-    id: Date.now(),
-    coverImg: imgSrc,
-    title: title,
-    tagline1: descriptor1,
-    tagline2: descriptor2
-  }
-  return cover
-}
 
 function createRandomCover() {
   var randomCoverIndex = getRandomIndex(covers)
@@ -74,3 +52,44 @@ function createRandomCover() {
 
   currentCover = createCover(chosenRandomCoverPath, chosenRandomTitle, chosenRandomTag1, chosenRandomTag2);
 }
+
+function reactMakeNew(){
+  homeView.classList.add('hidden');
+  formView.classList.remove('hidden');
+  coverButton.classList.add('hidden');
+  saveCoverButton.classList.add('hidden');
+  homeButton.classList.remove('hidden')
+}
+
+function reactHomeButton(){
+  homeView.classList.remove('hidden');
+  formView.classList.add('hidden');
+  coverButton.classList.remove('hidden');
+  saveCoverButton.classList.remove('hidden');
+  homeButton.classList.add('hidden');
+}
+
+function reactViewSaved(){
+  homeView.classList.add('hidden');
+  savedView.classList.remove('hidden');
+  coverButton.classList.add('hidden');
+  saveCoverButton.classList.add('hidden');
+  homeButton.classList.remove('hidden');
+}
+
+// We've provided two functions to get you started
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+function createCover(imgSrc, title, descriptor1, descriptor2) {
+  var cover = {
+    id: Date.now(),
+    coverImg: imgSrc,
+    title: title,
+    tagline1: descriptor1,
+    tagline2: descriptor2
+  }
+  return cover
+}
+
