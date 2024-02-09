67a1e8d85fc42254920d542509b3e59699f80956
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-02-08 17:28:18 -0800

    Change views enabled
    The code now shows different views when the buttons are hit. The nav bar
    on the top still does not properly reflect the buttons needed.
    
    Co-authored by: Ben <benwehrend@gmail.com>
    Co-authored by: Brandon <brandonjamesdoza@gmail.com>

--- src/main.js
+++ src/main.js
@@ -3,7 +3,15 @@ var coverImage = document.querySelector('.cover-image');
 var coverTitle = document.querySelector('.cover-title');
 var coverTagLine1 = document.querySelector('.tagline-1');
 var coverTagLine2 = document.querySelector('.tagline-2');
+
 var buttonRandomCover = document.querySelector('.random-cover-button');
+var buttonHome = document.querySelector('.home-button')
+var buttonMakeOwn = document.querySelector('.make-new-button')
+var buttonViewSaved = document.querySelector('.view-saved-button')
+
+var viewHome = document.querySelector('.home-view');
+var viewMakeOwn = document.querySelector('.form-view');
+var viewViewSaved = document.querySelector('.saved-view');
 
 // We've provided a few variables below
 
@@ -15,6 +23,9 @@ var currentCover;
 
 // Add your event listeners here 👇
 buttonRandomCover.addEventListener('click', init);
+buttonHome.addEventListener('click', setHome);
+buttonMakeOwn.addEventListener('click', setMake);
+buttonViewSaved.addEventListener('click', setSaved);
 
 // Create your event handlers and other functions here 👇
 function init() {
@@ -28,6 +39,53 @@ function updateCover() {
   coverTagLine1.innerText = currentCover.tagline1;
   coverTagLine2.innerText = currentCover.tagline2;
 };
+
+function randomCover() {
+  return createCover(
+    getRandomIndex(covers),
+    getRandomIndex(titles),
+    getRandomIndex(descriptors),
+    getRandomIndex(descriptors)
+    );
+}
+
+// "home"
+// only show the home section
+// show: Show new random cover, save cover should be visible
+// hide: home
+function setHome() {
+  viewHome.classList.remove('hidden');
+  viewMakeOwn.classList.add('hidden');
+  viewViewSaved.classList.add('hidden');
+
+  buttonHome.classList.add('hidden');
+}
+
+// "make your own cover"
+// HOME, VIEW SAVED COVERS, MAKE YOUR OWN COVER
+// in the top row show: home
+// in top row HIDE: show new random cover, save cover
+// see form, homepage view, save cover
+function setMake() {
+  viewMakeOwn.classList.remove('hidden');
+  viewHome.classList.add('hidden');
+  viewViewSaved.classList.add('hidden');
+
+  buttonHome.classList.remove('hidden');
+}
+
+// "view saved covers"
+// HOME, VIEW SAVED COVERS, MAKE YOUR OWN COVER
+// in the top row SHOW: home
+// in the top row HIDE: show new random cover, save cover
+// show the saved covers section, hide homepage, hide the make your cover view
+function setSaved() {
+  viewViewSaved.classList.remove('hidden');
+  viewMakeOwn.classList.add('hidden');
+  viewHome.classList.add('hidden');
+
+  buttonHome.classList.remove('hidden');
+}
 
 // We've provided two functions to get you started
 
@@ -50,30 +108,4 @@ function createCover(imgSrc, title, descriptor1, descriptor2) {
   return cover
 };
 
-function randomCover() {
-  return createCover(
-    getRandomIndex(covers),
-    getRandomIndex(titles),
-    getRandomIndex(descriptors),
-    getRandomIndex(descriptors)
-    );
-}
-
 init();
-
-// "make your own over"
-// HOME, VIEW SAVED COVERS, MAKE YOUR OWN COVER
-// in the top row show: home
-// in top row HIDE: show new random cover, save cover
-// see form, homepage view, save cover
-
-// "view saved covers"
-// HOME, VIEW SAVED COVERS, MAKE YOUR OWN COVER
-// in the top row SHOW: home
-// in the top row HIDE: show new random cover, save cover
-// show the saved covers section, hide homepage, hide the make your cover view
-
-// "home"
-// only show the home section
-// show: Show new random cover, save cover should be visible
-// hide: home
