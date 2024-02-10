80a5125bba632e998728a7ee161a4bf60c921a7d
David Swatzell <thatsstrikethree@gmail.com>
2024-02-10 14:50:27 -0600

    allow posters to be saved and displayed Co-authored-by: Kyle B <kylemboomer@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,69 +1,72 @@
 // Create variables targetting the relevant DOM elements here 👇
-var coverImage = document.querySelector('.cover-image')
-var coverTitle = document.querySelector('.cover-title')
-var coverTag1 = document.querySelector('.tagline-1')
-var coverTag2 = document.querySelector('.tagline-2')
-var randCoverButton = document.querySelector('.random-cover-button')
-var makeCoverButton = document.querySelector('.make-new-button')
-var viewSavedCovers = document.querySelector('.view-saved-button')
-var homeButton = document.querySelector('.home-button')
-var saveCoversButton = document.querySelector('.saved-cover-button')
-var coverUrl = document.querySelector('.user-cover')
-var titleInputBox = document.querySelector('.user-title')
-var descriptorInputBox1 = document.querySelector('.user-desc1')
-var descriptorInputBox2 = document.querySelector('.user-desc2')
-var formView = document.querySelector('.form-view');
-var newBookButton = document.querySelector('.create-new-book-button')
+var coverImage = document.querySelector(".cover-image");
+var coverTitle = document.querySelector(".cover-title");
+var coverTag1 = document.querySelector(".tagline-1");
+var coverTag2 = document.querySelector(".tagline-2");
+var randCoverButton = document.querySelector(".random-cover-button");
+var makeCoverButton = document.querySelector(".make-new-button");
+var viewSavedCovers = document.querySelector(".view-saved-button");
+var homeButton = document.querySelector(".home-button");
+var saveCoversButton = document.querySelector(".save-cover-button");
+var coverUrl = document.querySelector(".user-cover");
+var titleInputBox = document.querySelector(".user-title");
+var descriptorInputBox1 = document.querySelector(".user-desc1");
+var descriptorInputBox2 = document.querySelector(".user-desc2");
+var formView = document.querySelector(".form-view");
+var newBookButton = document.querySelector(".create-new-book-button");
+var savedCoversSection = document.querySelector(".saved-covers-section")
 
 // We've provided a few variables below
 var savedCovers = [];
 var currentCover;
 
 // Add your event listeners here 👇
-document.addEventListener('DOMContentLoaded', getRandomNovel);
-randCoverButton.addEventListener('click', getRandomNovel);
-makeCoverButton.addEventListener('click', makeCover)
-viewSavedCovers.addEventListener('click', showSavedCovers)
-homeButton.addEventListener('click', goHome)
-newBookButton.addEventListener('click',makeANovel)
+document.addEventListener("DOMContentLoaded", getRandomNovel);
+randCoverButton.addEventListener("click", getRandomNovel);
+makeCoverButton.addEventListener("click", makeCover);
+homeButton.addEventListener("click", goHome);
+newBookButton.addEventListener("click", makeANovel);
+saveCoversButton.addEventListener('click', saveCurrentCover);
+viewSavedCovers.addEventListener('click', function() {
+  showSavedCovers();
+  displaySavedCovers();
+});
+
 
 // Create your event handlers and other functions here 👇
-function makeCover(){
-  document.querySelector('.form-view').classList.remove('hidden')
-  document.querySelector('.home-view').classList.add('hidden')
-  document.querySelector('.random-cover-button').classList.add('hidden')
-  document.querySelector('.save-cover-button').classList.add('hidden')
-  document.querySelector('.home-button').classList.remove('hidden')
-  document.querySelector('.make-new-button').classList.remove('hidden')
-  document.querySelector('.view-saved-button').classList.remove('hidden')
+function makeCover() {
+  document.querySelector(".form-view").classList.remove("hidden");
+  document.querySelector(".home-view").classList.add("hidden");
+  document.querySelector(".random-cover-button").classList.add("hidden");
+  document.querySelector(".save-cover-button").classList.add("hidden");
+  document.querySelector(".home-button").classList.remove("hidden");
+  document.querySelector(".make-new-button").classList.remove("hidden");
+  document.querySelector(".view-saved-button").classList.remove("hidden");
 }
 
 function showSavedCovers() {
-  document.querySelector('.saved-view').classList.remove('hidden')
-  document.querySelector('.random-cover-button').classList.add('hidden')
-  document.querySelector('.home-button').classList.remove('hidden')
-  document.querySelector('.home-view').classList.add('hidden')
-  document.querySelector('.save-cover-button').classList.add('hidden')
-  document.querySelector('.view-saved-button').classList.remove('hidden')
-  document.querySelector('.form-view').classList.add('hidden')
+  document.querySelector(".saved-view").classList.remove("hidden");
+  document.querySelector(".random-cover-button").classList.add("hidden");
+  document.querySelector(".home-button").classList.remove("hidden");
+  document.querySelector(".home-view").classList.add("hidden");
+  document.querySelector(".save-cover-button").classList.add("hidden");
+  document.querySelector(".view-saved-button").classList.remove("hidden");
+  document.querySelector(".form-view").classList.add("hidden");
 }
 
 function goHome() {
-  document.querySelector('.home-view').classList.remove('hidden')
-  document.querySelector('.form-view').classList.add('hidden')
-  document.querySelector('.home-button').classList.add('hidden')
-  document.querySelector('.save-cover-button').classList.remove('hidden')
-  document.querySelector('.random-cover-button').classList.remove('hidden')
-  document.querySelector('.view-saved-button').classList.remove('hidden')
-  document.querySelector('.make-new-button').classList.remove('hidden')
+  document.querySelector(".home-view").classList.remove("hidden");
+  document.querySelector(".form-view").classList.add("hidden");
+  document.querySelector(".home-button").classList.add("hidden");
+  document.querySelector(".save-cover-button").classList.remove("hidden");
+  document.querySelector(".random-cover-button").classList.remove("hidden");
+  document.querySelector(".view-saved-button").classList.remove("hidden");
+  document.querySelector(".make-new-button").classList.remove("hidden");
 }
 function changeFormView() {
-  document.querySelector('.form-view').classList.toggle('hidden');
-  document.querySelector('.home-view').classList.toggle('hidden');
+  document.querySelector(".form-view").classList.toggle("hidden");
+  document.querySelector(".home-view").classList.toggle("hidden");
 }
-
-
-
 
 // We've provided two functions to get you started
 function getRandomIndex(array) {
@@ -76,12 +79,10 @@ function createCover(imgSrc, title, descriptor1, descriptor2) {
     coverImg: imgSrc,
     title: title,
     tagline1: descriptor1,
-    tagline2: descriptor2
-  }
-  return cover
+    tagline2: descriptor2,
+  };
+  return cover;
 }
-
-
 
 function getRandomNovel() {
   var randomCoverIndex = getRandomIndex(covers);
@@ -106,16 +107,43 @@ function getRandomNovel() {
   return currentCover;
 }
 
-function makeANovel (event){
-  if (event) event.preventDefault()
+function makeANovel(event) {
+  if (event) event.preventDefault();
   var userNovelDesign = createCover(coverUrl, title, descriptor1, descriptor2);
-    coverImage.setAttribute('src', userNovelDesign.coverImg.value);
-    coverTitle.innerText = userNovelDesign.title.value;
-    coverTag1.innerText = userNovelDesign.tagline1.value;
-    coverTag2.innerText = userNovelDesign.tagline2.value;
-    covers.push(userNovelDesign.coverImg);
-    titles.push(userNovelDesign.title);
-    descriptors.push([userNovelDesign.tagline1, userNovelDesign.tagline2]);
-    changeFormView()
+  coverImage.setAttribute("src", userNovelDesign.coverImg.value);
+  coverTitle.innerText = userNovelDesign.title.value;
+  coverTag1.innerText = userNovelDesign.tagline1.value;
+  coverTag2.innerText = userNovelDesign.tagline2.value;
+  covers.push(userNovelDesign.coverImg);
+  titles.push(userNovelDesign.title);
+  descriptors.push([userNovelDesign.tagline1, userNovelDesign.tagline2]);
+  changeFormView();
 }
 
+function saveCurrentCover() {
+  var newCover = {
+    coverImg: coverImage.src,
+    title: coverTitle.innerText,
+    tagline1: coverTag1.innerText,
+    tagline2: coverTag2.innerText
+  };
+  var isDuplicate = savedCovers.some(function(savedCover) {
+    return savedCover.coverImg === newCover.coverImg;
+  });
+  if (!isDuplicate) {
+    savedCovers.push(newCover);
+  }
+}
+
+function displaySavedCovers() {
+ savedCovers.forEach(function(savedCover) {
+    var savedCoverElement = document.createElement('div');
+    savedCoverElement.classList.add('mini-cover');
+    savedCoverElement.innerHTML = `
+      <img class="cover-image" src="${savedCover.coverImg}" alt="Saved Cover">
+      <h4 class="cover-title">${savedCover.title}</h4>
+      <h5 class="tagline">A tale of ${savedCover.tagline1} and ${savedCover.tagline2}</h5>
+    `;
+    savedCoversSection.appendChild(savedCoverElement);
+  });
+}
